69b772a15261efc57cf7d125d017c40dd963a2a8
Monika Wenger <wenger@fortiss.org>
2017-12-29 15:48:59 +0100

    add attribute tab to application editor

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/AttributeSection.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/AttributeSection.java
@@ -7,25 +7,25 @@
  *
  * SPDX-License-Identifier: EPL-2.0 3
  *******************************************************************************/
-package org.eclipse.fordiac.ide.systemconfiguration.properties;
+package org.eclipse.fordiac.ide.application.properties;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.fordiac.ide.application.editparts.FBNetworkEditPart;
 import org.eclipse.fordiac.ide.gef.properties.AbstractAttributeSection;
-import org.eclipse.fordiac.ide.model.libraryElement.Device;
-import org.eclipse.fordiac.ide.systemconfiguration.editparts.DeviceEditPart;
+import org.eclipse.fordiac.ide.model.libraryElement.Application;
 
 public class AttributeSection extends AbstractAttributeSection {
-	protected Device getInputType(Object input) {
-		if(input instanceof DeviceEditPart){
-			return ((DeviceEditPart) input).getModel();
+	protected Application getInputType(Object input) {
+		if(input instanceof FBNetworkEditPart){
+			return ((FBNetworkEditPart) input).getModel().getApplication();
 		}
 		return null;
 	}
 
 	@Override
 	protected EObject getType() {
-		if(type instanceof Device){
-			return (Device) type;
+		if(type instanceof Application){
+			return (Application) type;
 		}
 		return null;
 	}
