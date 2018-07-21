465e2a253c8b59dd1c3c3463b7f0ea27cf4d34c9
Monika Wenger <wenger@fortiss.org>
2018-07-21 19:10:00 +0200

    remove incomplete create connection section of interface elements

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/InterfaceElementSection.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/InterfaceElementSection.java
@@ -14,32 +14,18 @@ package org.eclipse.fordiac.ide.application.properties;
 
 import org.eclipse.emf.edit.ui.celleditor.AdapterFactoryTreeEditor;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
-import org.eclipse.fordiac.ide.model.commands.create.AbstractConnectionCreateCommand;
-import org.eclipse.fordiac.ide.model.commands.create.AdapterConnectionCreateCommand;
-import org.eclipse.fordiac.ide.model.commands.create.DataConnectionCreateCommand;
-import org.eclipse.fordiac.ide.model.commands.create.EventConnectionCreateCommand;
 import org.eclipse.fordiac.ide.model.commands.delete.DeleteConnectionCommand;
-import org.eclipse.fordiac.ide.model.libraryElement.AdapterDeclaration;
-import org.eclipse.fordiac.ide.model.libraryElement.CompositeFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.Connection;
-import org.eclipse.fordiac.ide.model.libraryElement.Event;
-import org.eclipse.fordiac.ide.model.libraryElement.FBNetwork;
-import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
-import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.ui.ISharedImages;
@@ -48,124 +34,17 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
 public class InterfaceElementSection extends org.eclipse.fordiac.ide.gef.properties.InterfaceElementSection{
 	private TreeViewer connectionsTree;
-	private Button newConnection;
 	private Button delConnection;
 	private Group group;
-	private Combo fbCombo;
-	private Combo interfaceCombo;
-	private CLabel fbLabel;
-	private CLabel interfaceElementLabel;
 	
 	public void createControls(final Composite parent, final TabbedPropertySheetPage tabbedPropertySheetPage) {
 		createSuperControls = false;
 		super.createControls(parent, tabbedPropertySheetPage);
-		parent.setLayout(new GridLayout(3, true));
+		parent.setLayout(new GridLayout(2, true));
 		parent.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
 		createConnectionDisplaySection(parent);
-		createConnectionSection(parent);
-	}
-	
-	private void createConnectionSection(Composite parent) {
-		Group group = getWidgetFactory().createGroup(parent, "Create Connection");
-		group.setLayout(new GridLayout(2, false));
-		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
-		fbLabel = getWidgetFactory().createCLabel(group, ""); //$NON-NLS-1$
-		fbCombo = new Combo(group, SWT.SINGLE | SWT.READ_ONLY);
-		fbCombo.addListener( SWT.Selection, event -> fillInterfaceCombo(fbCombo.getText()));
-		interfaceElementLabel = getWidgetFactory().createCLabel(group, ""); //$NON-NLS-1$
-		interfaceCombo = new Combo(group, SWT.SINGLE | SWT.READ_ONLY);
-		interfaceCombo.addSelectionListener(new SelectionListener() {
-			@Override
-			public void widgetSelected(final SelectionEvent e) {
-//				getInterfaceelement
-			}
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-			}
-		});
-		
-		newConnection = getWidgetFactory().createButton(group, "", SWT.PUSH); //$NON-NLS-1$
-		newConnection.setImage(PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_ADD));
-		delConnection.setLayoutData(new  GridData(SWT.LEFT, SWT.BOTTOM, false, true));
-		newConnection.setToolTipText("new Connection");	
-		newConnection.addListener( SWT.Selection, event -> {
-				AbstractConnectionCreateCommand cmd = null;
-				if(getType() instanceof Event){
-					cmd = new EventConnectionCreateCommand(getFBNetwork());
-				}else if(getType() instanceof AdapterDeclaration){
-					cmd = new AdapterConnectionCreateCommand(getFBNetwork());
-				}else if(getType() instanceof VarDeclaration){
-					cmd = new DataConnectionCreateCommand(getFBNetwork());
-				}
-				if(null != cmd){
-					cmd.setDestination(getSelectedFB().getInterfaceElement(interfaceCombo.getText()));
-					cmd.setSource(getType());
-					executeCommand(cmd);
-					connectionsTree.refresh();
-				}
-			});
 	}
 
-	private FBNetwork getFBNetwork() {
-		if(null != getType().getFBNetworkElement()){
-			return getType().getFBNetworkElement().getFbNetwork();
-		}
-		if(getType().eContainer().eContainer() instanceof CompositeFBType){
-			return ((CompositeFBType)getType().eContainer().eContainer()).getFBNetwork();
-		}
-		return null;
-	}
-	
-	private FBNetworkElement getSelectedFB() {
-		if(null != getFBNetwork()){
-			return getFBNetwork().getFBNamed(fbCombo.getText());
-		}
-		return null;
-	}
-	
-	private void fillFbCombo() {
-		fbCombo.removeAll();
-		if(null != getFBNetwork()){
-			getFBNetwork().getNetworkElements().forEach(element -> fbCombo.add(element.getName()));			
-		}
-//		fbCombo.remove(getType().getFBNetworkElement().getName());
-	}
-
-	private void fillInterfaceCombo(String fbName) {
-		interfaceCombo.removeAll();		
-		FBNetworkElement element = getSelectedFB();
-		if(element.getName().equals(fbName)){
-			if(getType().isIsInput() && null != getType().getFBNetworkElement()){
-				if(getType() instanceof Event){
-					element.getInterface().getEventOutputs().forEach(port -> interfaceCombo.add(port.getName()));
-					return;
-				}
-				if(getType() instanceof AdapterDeclaration){
-					element.getInterface().getPlugs().forEach(port -> interfaceCombo.add(port.getName()));
-					return;
-				}
-				if(getType() instanceof VarDeclaration){
-					element.getInterface().getOutputVars().forEach(port -> interfaceCombo.add(port.getName()));
-					return;
-				}
-			}else{
-				if(getType() instanceof Event){
-					element.getInterface().getEventInputs().forEach(port -> interfaceCombo.add(port.getName()));
-					return;
-				}
-				if(getType() instanceof AdapterDeclaration){
-					element.getInterface().getSockets().forEach(port -> interfaceCombo.add(port.getName()));
-					return;
-				}
-				if(getType() instanceof VarDeclaration){
-					element.getInterface().getInputVars().forEach(port -> interfaceCombo.add(port.getName()));
-					return;
-				}
-			}
-		}
-	}
-	
 	private void createConnectionDisplaySection(Composite parent) {
 		group = getWidgetFactory().createGroup(parent, "Connections");
 		group.setLayout(new GridLayout(2, false));
@@ -201,15 +80,10 @@ public class InterfaceElementSection extends org.eclipse.fordiac.ide.gef.propert
 		if(null != type) {
 			if(getType().isIsInput()){
 				group.setText("In-Connections");
-				fbLabel.setText("Source FB:");
-				interfaceElementLabel.setText("Source Interface:"); 
 			}else{
 				group.setText("Out-Connections");
-				fbLabel.setText("Target FB:");
-				interfaceElementLabel.setText("Target Interface:"); 
 			}
 			connectionsTree.setInput(getType());
-			fillFbCombo();
 		}
 		commandStack = commandStackBuffer;
 	}
