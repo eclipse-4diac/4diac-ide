112dc094e3a5534f78217bcfd72918281c390cbb
peda1337 <peter.gsellmann@gmail.com>
2018-10-04 12:31:59 +0200

    [539557] Added basic framework for SimpleFB Editor
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=539557
    
    Change-Id: I2bceff2da53cd5f987bc8c4045d377b4b4e55166
    Signed-off-by: peda1337 <peter.gsellmann@gmail.com>

---
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.simplefb/src/org/eclipse/fordiac/ide/fbtypeeditor/simplefb/SimpleFBEditor.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2018 TU Wien/ACIN
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Peter Gsellmann 
+ *   - initial implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.fbtypeeditor.simplefb;
+
+public class SimpleFBEditor {
+	
+}
