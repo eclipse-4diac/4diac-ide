2768f0ca09a2fd1a135357021565d41b7720b06c
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2019-07-02 11:43:09 +0200

    [548854] closing multiple projects at the same time
    when closing multiple projects, all are deleted within same dialogue
    also did some code clean-up
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=548854
    Change-Id: I548f066cfd7ccdd8a86a9b2f79e27cf03c4ee75a
    
    Change-Id: I548f066cfd7ccdd8a86a9b2f79e27cf03c4ee75a
    Signed-off-by: Bianca Wiesmayr <bianca.wiesmayr@jku.at>

--- plugins/org.eclipse.fordiac.ide.systemmanagement.ui/src/org/eclipse/fordiac/ide/systemmanagement/ui/handlers/DeleteSystem.java
+++ plugins/org.eclipse.fordiac.ide.systemmanagement.ui/src/org/eclipse/fordiac/ide/systemmanagement/ui/handlers/DeleteSystem.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2008 - 2016 Profactor Gmbh, TU Wien ACIN, fortiss GmbH
+ *               2019 Johannes Kepler University Linz
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,8 +10,13 @@
  * Contributors:
  *   Gerhard Ebenhofer, Alois Zoitl, Monika Wenger
  *     - initial API and implementation and/or initial documentation
+ *   Alois Zoitl, Bianca Wiesmayr, Virendra Ashiwal
+ *     - delete multiple projects in single dialogue
  *******************************************************************************/
 package org.eclipse.fordiac.ide.systemmanagement.ui.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -18,11 +24,9 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.fordiac.ide.model.libraryElement.AutomationSystem;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.jface.window.IShellProvider;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.actions.DeleteResourceAction;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -31,25 +35,28 @@ public class DeleteSystem extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
-		if(selection instanceof TreeSelection) {			
-			for (Object selElement : ((TreeSelection)selection).toList()) {
-				if(selElement instanceof AutomationSystem){
-					IProject project = ((AutomationSystem)selElement).getProject();
-					runDeleteAction(project);					
-				}
-			}
-		}
+		List<IProject> projectSelection = getSelectedProjects(selection);
+		runDeleteAction(projectSelection);
 		return null;
 	}
 
-	private static void runDeleteAction(IProject project) {
-		DeleteResourceAction action = new DeleteResourceAction(new IShellProvider() {			
-			@Override
-			public Shell getShell() {
-				return Display.getDefault().getActiveShell();
-			}
-		});
-		action.selectionChanged(new StructuredSelection(project));
+	private static void runDeleteAction(List<IProject> projectSelection) {
+		DeleteResourceAction action = new DeleteResourceAction(() -> Display.getDefault().getActiveShell());
+		action.selectionChanged(new StructuredSelection(projectSelection));
 		action.run();
+	}
+
+	private static List<IProject> getSelectedProjects(ISelection selection) {
+		List<IProject> projectSelection = new ArrayList<>();
+		if (selection instanceof IStructuredSelection) {
+			for (Object element : ((StructuredSelection) selection).toList()) {
+				if (element instanceof AutomationSystem) {
+					projectSelection.add(((AutomationSystem) element).getProject());
+				} else if (element instanceof IProject) {
+					projectSelection.add((IProject) element);
+				}
+			}
+		}
+		return projectSelection;
 	}
 }
