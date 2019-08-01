1f5014e0efe8aa6933c07e4cfa16a3088a7372ba
Alois Zoitl <alois.zoitl@gmx.at>
2019-08-01 22:18:45 +0200

    [549728] Moved Linewidth getter to display thread
    In order to remove invalid thread access the linewidth getter is moved
    to the display thread.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=549728
    Change-Id: I44daa6dd4931e8af0afa93bcf33df469e48fb7fd
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/CoordinateConverter.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/CoordinateConverter.java
@@ -13,48 +13,68 @@
 package org.eclipse.fordiac.ide.model;
 
 import org.eclipse.draw2d.FigureUtilities;
+import org.eclipse.swt.widgets.Display;
 
-
-/** Helper class for transforming between the coordination resolution between IEC 61499-2 Annex B.1 
- * definitions to the resolution used in 4diac IDE editors.
+/**
+ * Helper class for transforming between the coordination resolution between IEC
+ * 61499-2 Annex B.1 definitions to the resolution used in 4diac IDE editors.
  *
  */
 public enum CoordinateConverter {
 	INSTANCE;
-	
-	/** The scaler transformation value for coordinate conversion.
+
+	/**
+	 * The scaler transformation value for coordinate conversion.
 	 * 
 	 * In general it the lineHeigth / 100 as defined in IEC 61499-2 Annex B.1
 	 */
 	private double transformationScale;
-	
-	private CoordinateConverter() {
-		double lineHeight = FigureUtilities.getFontMetrics(null).getHeight();  //null means to use the default font, needs update when we get setable fonts
-		transformationScale = lineHeight / 100.0; 
+
+	private static class LineHeightRunnable implements Runnable {
+		private double lineHeight;
+
+		public double getLineHeight() {
+			return lineHeight;
+		}
+
+		@Override
+		public void run() {
+			// null means to use the default font, needs update when we get setable fonts
+			lineHeight = FigureUtilities.getFontMetrics(null).getHeight();
+		}
 	}
-	
-	
-	/** Take the string representing the value from the 61499-2 XML file to an 4diac IDE internal coordinate
+
+	private CoordinateConverter() {
+		LineHeightRunnable lineHeight = new LineHeightRunnable();
+
+		Display.getDefault().syncExec(lineHeight); 
+		transformationScale = lineHeight.getLineHeight() / 100.0;
+	}
+
+	/**
+	 * Take the string representing the value from the 61499-2 XML file to an 4diac
+	 * IDE internal coordinate
 	 * 
-	 * @param value	string representation of a coordinate value
-	 * @return 		4diac IDE internal coordinate value
+	 * @param value string representation of a coordinate value
+	 * @return 4diac IDE internal coordinate value
 	 */
 	public int convertFrom1499XML(final String value) {
 		double parsedValue = 0;
-		if ((null != value) && (0 != value.length())){
-			parsedValue = Double.parseDouble(value);					
+		if ((null != value) && (0 != value.length())) {
+			parsedValue = Double.parseDouble(value);
 		}
-		return (int)(parsedValue * transformationScale);
+		return (int) (parsedValue * transformationScale);
 	}
-	
-	/** Take an 4diac IDE internal coordinate and transform it to a string representation suitable for an IEC 61499-2 XML
+
+	/**
+	 * Take an 4diac IDE internal coordinate and transform it to a string
+	 * representation suitable for an IEC 61499-2 XML
 	 * 
-	 * @param value	4diac IDE internal coordinate
-	 * @return 		string representation of the value in IEC 61499-2 format
+	 * @param value 4diac IDE internal coordinate
+	 * @return string representation of the value in IEC 61499-2 format
 	 */
 	public String convertTo1499XML(final int value) {
 		return Double.toString(value / transformationScale);
 	}
-
 
 }
