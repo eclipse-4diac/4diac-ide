f36fb31ac2139282d6bf8e2a5e64547c1fef3712
Alois Zoitl <alois.zoitl@gmx.at>
2019-08-14 13:51:12 +0200

    [550064] Added more checks for selected objects in new app wizard
    The new application wizard now correctly selects the according system
    for more selected elements (system configuration, typelib, fbnetworks),
    so that less often a system has to be selected when creating a new
    application.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=550064
    Change-Id: Ia813dc3e70688ab9f6518f1e0867f782066ce5e8
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/navigator/TypeLibRootElement.java
+++ plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/navigator/TypeLibRootElement.java
@@ -17,26 +17,25 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.fordiac.ide.model.libraryElement.AutomationSystem;
 
-class TypeLibRootElement implements IAdaptable{
+public class TypeLibRootElement implements IAdaptable {
 
 	private AutomationSystem system;
-	
+
 	TypeLibRootElement(AutomationSystem system) {
 		this.system = system;
 	}
-	
-	public AutomationSystem getSystem(){
+
+	public AutomationSystem getSystem() {
 		return system;
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public Object getAdapter(Class adapter) {
-		if((adapter == IResource.class) || (adapter == IProject.class)){
+		if ((adapter == IResource.class) || (adapter == IProject.class)) {
 			return getSystem().getProject();
 		}
 		return null;
 	}
 
-	
 }
