9ee425fdd425be2aa1b8c3cb14205c84209545a8
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2019-08-30 14:48:07 +0200

    [546964] cleanup for StateSection after editing
    Extracted code specific for Transition and Action table to the
    respective classes for code clean-up after the new change
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=546964
    
    Change-Id: I10feaba11fd91f47a38ded71c4f9917192a8b7db
    Signed-off-by: Bianca Wiesmayr <bianca.wiesmayr@jku.at>

---
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/widgets/ActionEditingComposite.java
@@ -0,0 +1,230 @@
+/*******************************************************************************
+ * Copyright (c) 2015 - 2017 fortiss GmbH
+ * 				 2019 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Monika Wenger, Alois Zoitl
+ *     - initial API and implementation and/or initial documentation
+ *   Alois Zoitl - Harmonized look and feel, added multi line selection
+ *   Virendra Ashiwal, Bianca Wiesmayr
+ *     - added "[none]" as showing text in ECC->State->Property when no
+ *       algorithm is selected
+ *     - change TransitionViewer to table and make it editable
+ *   Alois Zoitl - extracted helper for ComboCellEditors that unfold on activation
+ *   Bianca Wiesmayr - externalized code from StateSection and cleanup
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.fbtypeeditor.ecc.widgets;
+
+import java.util.List;
+
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeActionOrderCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeAlgorithmCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeOutputCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.CreateECActionCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.DeleteECActionCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.contentprovider.ActionContentProvider;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.contentprovider.ECCContentAndLabelProvider;
+import org.eclipse.fordiac.ide.model.libraryElement.Algorithm;
+import org.eclipse.fordiac.ide.model.libraryElement.BasicFBType;
+import org.eclipse.fordiac.ide.model.libraryElement.ECAction;
+import org.eclipse.fordiac.ide.model.libraryElement.ECState;
+import org.eclipse.fordiac.ide.model.libraryElement.Event;
+import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
+import org.eclipse.fordiac.ide.ui.widget.AddDeleteReorderListWidget;
+import org.eclipse.fordiac.ide.ui.widget.ComboBoxWidgetFactory;
+import org.eclipse.fordiac.ide.ui.widget.CommandExecutor;
+import org.eclipse.fordiac.ide.ui.widget.TableWidgetFactory;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.CommandStack;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ICellModifier;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
+
+public class ActionEditingComposite {
+	private static class ActionListLabelProvider extends LabelProvider implements ITableLabelProvider {
+		@Override
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+
+		@Override
+		public String getColumnText(Object element, int columnIndex) {
+			if (element instanceof ECAction) {
+				switch (columnIndex) {
+				case ACTION_COLUMN_ALGORITHM:
+					return (null != ((ECAction) element).getAlgorithm()) ? ((ECAction) element).getAlgorithm().getName()
+							: ECCContentAndLabelProvider.EMPTY_FIELD;
+				case ACTION_COLUMN_EVENT:
+					return (null != ((ECAction) element).getOutput()) ? ((ECAction) element).getOutput().getName()
+							: ECCContentAndLabelProvider.EMPTY_FIELD;
+				default:
+					break;
+				}
+			}
+			return ECCContentAndLabelProvider.EMPTY_FIELD;
+		}
+	}
+
+	private class ActionViewerCellModifier implements ICellModifier {
+		@Override
+		public boolean canModify(final Object element, final String property) {
+			return true;
+		}
+
+		@Override
+		public Object getValue(final Object element, final String property) {
+			ECAction selectedAction = (ECAction) element;
+			switch (property) {
+			case ACTION_ALGORITHM:
+				List<Algorithm> algorithms = ECCContentAndLabelProvider.getAlgorithms(getBasicFBType());
+				return Integer.valueOf(
+						(null != selectedAction.getAlgorithm()) ? algorithms.indexOf(selectedAction.getAlgorithm())
+								: algorithms.size());
+			case ACTION_EVENT:
+				List<String> events = ECCContentAndLabelProvider.getOutputEventNames(getBasicFBType());
+				return Integer.valueOf(
+						(null != selectedAction.getOutput()) ? events.indexOf(selectedAction.getOutput().getName())
+								: events.size());
+			default:
+				return ""; //$NON-NLS-1$
+			}
+		}
+
+		@Override
+		public void modify(final Object element, final String property, final Object value) {
+			TableItem tableItem = (TableItem) element;
+			ECAction selectedAction = (ECAction) tableItem.getData();
+			int selected = ((Integer) value).intValue();
+			Command cmd = null;
+
+			switch (property) {
+			case ACTION_ALGORITHM:
+				List<Algorithm> algorithms = ECCContentAndLabelProvider.getAlgorithms(getBasicFBType());
+				Algorithm alg = (selected < algorithms.size()) ? algorithms.get(selected) : null;
+				cmd = new ChangeAlgorithmCommand(selectedAction, alg);
+				break;
+			case ACTION_EVENT:
+				List<Event> events = ECCContentAndLabelProvider.getOutputEvents(getBasicFBType());
+				Event ev = ((0 <= selected) && (selected < events.size())) ? events.get(selected) : null;
+				cmd = new ChangeOutputCommand(selectedAction, ev);
+				break;
+			default:
+				break;
+			}
+			if ((null != cmd) && (null != commandStack)) {
+				commandStack.execute(cmd);
+				refresh();
+			}
+		}
+
+	}
+
+	private static final String ACTION_ALGORITHM = "Algorithm"; //$NON-NLS-1$
+	private static final String ACTION_EVENT = "Event"; //$NON-NLS-1$
+	private static final int ACTION_COLUMN_ALGORITHM = 0;
+	private static final int ACTION_COLUMN_EVENT = 1;
+
+	private TableViewer actionViewer;
+	private AddDeleteReorderListWidget actionMgmButtons;
+	private TabbedPropertySheetWidgetFactory actionWidgetFactory;
+	private Group actionGroup;
+
+	private CommandStack commandStack;
+	private CommandExecutor commandExecutor; // the parent section
+	private ECState type;
+
+	public ActionEditingComposite(Composite parent, TabbedPropertySheetWidgetFactory actionWidgetFactory,
+			CommandExecutor commandExecutor) {
+		this.actionWidgetFactory = actionWidgetFactory;
+		this.commandExecutor = commandExecutor;
+		actionGroup = actionWidgetFactory.createGroup(parent, "Actions");
+		commandStack = null;
+		type = null;
+		createGroupLayout();
+
+	}
+
+	private void createGroupLayout() {
+		actionGroup.setLayout(new GridLayout(2, false));
+		actionGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		actionMgmButtons = new AddDeleteReorderListWidget();
+		actionMgmButtons.createControls(actionGroup, actionWidgetFactory);
+
+		actionViewer = TableWidgetFactory.createTableViewer(actionGroup);
+		configureActionTableLayout(actionViewer.getTable());
+		actionViewer.setContentProvider(new ActionContentProvider());
+		actionViewer.setLabelProvider(new ActionListLabelProvider());
+
+		actionMgmButtons.bindToTableViewer(actionViewer, commandExecutor,
+				ref -> new CreateECActionCommand(LibraryElementFactory.eINSTANCE.createECAction(), type),
+				ref -> new DeleteECActionCommand((ECAction) ref),
+				ref -> new ChangeActionOrderCommand(type, (ECAction) ref, true),
+				ref -> new ChangeActionOrderCommand(type, (ECAction) ref, false));
+	}
+
+	private void configureActionTableLayout(final Table table) {
+		new TableColumn(table, SWT.LEFT).setText("Algorithm");
+		new TableColumn(table, SWT.LEFT).setText("Event");
+
+		TableLayout tabLayout = new TableLayout();
+		tabLayout.addColumnData(new ColumnWeightData(1, 50));
+		tabLayout.addColumnData(new ColumnWeightData(2, 50));
+
+		table.setLayout(tabLayout);
+		actionViewer.setColumnProperties(new String[] { ACTION_ALGORITHM, ACTION_EVENT });
+	}
+
+	private CellEditor[] createActionViewerCellEditors(Table table) {
+		BasicFBType fbType = getBasicFBType();
+		return new CellEditor[] {
+				ComboBoxWidgetFactory.createComboBoxCellEditor(table,
+						ECCContentAndLabelProvider.getAlgorithmNames(fbType).toArray(new String[0]), SWT.READ_ONLY),
+				ComboBoxWidgetFactory.createComboBoxCellEditor(table,
+						ECCContentAndLabelProvider.getOutputEventNames(fbType).toArray(new String[0]), SWT.READ_ONLY) };
+	}
+
+	private BasicFBType getBasicFBType() {
+		return type.getECC().getBasicFBType();
+	}
+
+	public void refresh() {
+		CommandStack commandStackBuffer = commandStack;
+		commandStack = null;
+		if (null != type) {
+			actionViewer.setInput(type);
+		}
+		commandStack = commandStackBuffer;
+	}
+
+	public void setTypeAndCommandStack(ECState type, CommandStack commandStack) {
+		this.type = type;
+		this.commandStack = commandStack;
+
+		// if the selected state is the start state disable adding actions
+		actionMgmButtons.setCreateButtonEnablement(!type.isStartState());
+
+		// have to do that here, because now we have a valid type
+		actionViewer.setCellEditors(createActionViewerCellEditors(actionViewer.getTable()));
+		actionViewer.setCellModifier(new ActionViewerCellModifier());
+	}
+}
---
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/widgets/TransitionEditingComposite.java
@@ -0,0 +1,279 @@
+/*******************************************************************************
+ * Copyright (c) 2015 - 2017 fortiss GmbH
+ * 				 2019 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Monika Wenger, Alois Zoitl
+ *     - initial API and implementation and/or initial documentation
+ *   Alois Zoitl - Harmonized look and feel, added multi line selection
+ *   Virendra Ashiwal, Bianca Wiesmayr
+ *     - added "[none]" as showing text in ECC->State->Property when no
+ *       algorithm is selected
+ *     - change TransitionViewer to table and make it editable
+ *   Alois Zoitl - extracted helper for ComboCellEditors that unfold on activation
+ *   Bianca Wiesmayr - externalized code from StateSection and cleanup
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.fbtypeeditor.ecc.widgets;
+
+import java.util.List;
+
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeConditionEventCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeConditionExpressionCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeECTransitionCommentCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeTransitionDestinationCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeTransitionPriorityCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.CreateTransitionCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.DeleteTransitionCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.contentprovider.ECCContentAndLabelProvider;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.contentprovider.StateContentProvider;
+import org.eclipse.fordiac.ide.model.libraryElement.BasicFBType;
+import org.eclipse.fordiac.ide.model.libraryElement.ECState;
+import org.eclipse.fordiac.ide.model.libraryElement.ECTransition;
+import org.eclipse.fordiac.ide.ui.widget.AddDeleteReorderListWidget;
+import org.eclipse.fordiac.ide.ui.widget.ComboBoxWidgetFactory;
+import org.eclipse.fordiac.ide.ui.widget.CommandExecutor;
+import org.eclipse.fordiac.ide.ui.widget.TableWidgetFactory;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.CommandStack;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ICellModifier;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
+
+public class TransitionEditingComposite {
+	private class TransitionListLabelProvider extends LabelProvider implements ITableLabelProvider {
+
+		@Override
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+
+		@Override
+		public String getColumnText(Object element, int columnIndex) {
+			if (element instanceof ECTransition) {
+				switch (columnIndex) {
+				case TRANSITION_COLUMN_PRIORITY:
+					return Integer.toString(type.getOutTransitions().indexOf(element) + 1);
+
+				case TRANSITION_COLUMN_DESTINATION:
+					if (null != ((ECTransition) element).getDestination()) {
+						return ((ECTransition) element).getDestination().getName();
+					}
+					break;
+
+				case TRANSITION_COLUMN_EVENT:
+					if (null != ((ECTransition) element).getConditionEvent()) {
+						return ((ECTransition) element).getConditionEvent().getName();
+					}
+					break;
+
+				case TRANSITION_COLUMN_CONDITION:
+					return ((ECTransition) element).getConditionExpression();
+
+				case TRANSITION_COLUMN_COMMENT:
+					return ((ECTransition) element).getComment();
+
+				default:
+					break;
+				}
+			}
+			return ECCContentAndLabelProvider.EMPTY_FIELD;
+		}
+	}
+
+	private class TransitionViewerCellModifier implements ICellModifier {
+		@Override
+		public boolean canModify(final Object element, final String property) {
+			return !property.equals(TRANSITION_PRIORITY); // modify all other columns
+		}
+
+		@Override
+		public Object getValue(final Object element, final String property) {
+			ECTransition selectedTransition = (ECTransition) element;
+			switch (property) {
+			case TRANSITION_DESTINATION: // int for Combobox
+				List<String> dest = ECCContentAndLabelProvider.getStateNames(getBasicFBType());
+				return Integer.valueOf((null != selectedTransition.getDestination())
+						? dest.indexOf(selectedTransition.getDestination().getName())
+						: dest.size());
+			case TRANSITION_EVENT:
+				List<String> events = ECCContentAndLabelProvider.getTransitionConditionEventNames(getBasicFBType());
+				return Integer.valueOf((null != selectedTransition.getConditionEvent())
+						? events.indexOf(selectedTransition.getConditionEvent().getName())
+						: events.size());
+			case TRANSITION_CONDITION: // String for TextCellEditor
+				return selectedTransition.getConditionExpression();
+			case TRANSITION_COMMENT:
+				return selectedTransition.getComment();
+			default:
+				return ""; //$NON-NLS-1$
+			}
+		}
+
+		@Override
+		public void modify(final Object element, final String property, final Object value) {
+			TableItem tableItem = (TableItem) element;
+			ECTransition selectedTransition = (ECTransition) tableItem.getData();
+
+			Command cmd = null;
+			switch (property) {
+			case TRANSITION_DESTINATION:
+				int selectedDest = ((Integer) value).intValue();
+				List<ECState> destinations = ECCContentAndLabelProvider.getStates(getBasicFBType());
+				ECState dest = ((0 <= selectedDest) && (selectedDest < destinations.size()))
+						? destinations.get(selectedDest)
+						: null;
+				cmd = new ChangeTransitionDestinationCommand(selectedTransition, dest);
+				break;
+
+			case TRANSITION_EVENT:
+				int selectedEv = ((Integer) value).intValue();
+				List<String> events = ECCContentAndLabelProvider.getTransitionConditionEventNames(getBasicFBType());
+				String ev = ((0 <= selectedEv) && (selectedEv < events.size())) ? events.get(selectedEv) : null;
+				cmd = new ChangeConditionEventCommand(selectedTransition, ev);
+				break;
+
+			case TRANSITION_COMMENT:
+				cmd = new ChangeECTransitionCommentCommand(selectedTransition, (String) value);
+				break;
+
+			case TRANSITION_CONDITION:
+				cmd = new ChangeConditionExpressionCommand(selectedTransition, (String) value);
+				break;
+
+			default:
+				break;
+			}
+			if ((null != cmd) && (null != commandStack)) {
+				commandStack.execute(cmd);
+				refresh();
+			}
+		}
+
+		private void refresh() {
+			transitionsOutViewer.setInput(type);
+		}
+
+	}
+
+	private static final String TRANSITION_PRIORITY = "TransitionPriority"; //$NON-NLS-1$
+	private static final String TRANSITION_DESTINATION = "TransitionDestination"; //$NON-NLS-1$
+	private static final String TRANSITION_EVENT = "TransitionEvent"; //$NON-NLS-1$
+	private static final String TRANSITION_CONDITION = "TransitionCondition"; //$NON-NLS-1$
+	private static final String TRANSITION_COMMENT = "TransitionComment"; //$NON-NLS-1$
+
+	private static final int TRANSITION_COLUMN_PRIORITY = 0;
+	private static final int TRANSITION_COLUMN_DESTINATION = 1;
+	private static final int TRANSITION_COLUMN_EVENT = 2;
+	private static final int TRANSITION_COLUMN_CONDITION = 3;
+	private static final int TRANSITION_COLUMN_COMMENT = 4;
+
+	private TableViewer transitionsOutViewer;
+	private TabbedPropertySheetWidgetFactory transitionWidgetFactory;
+	private Group transitionGroup;
+
+	private CommandExecutor commandExecutor; // the parent section
+	private ECState type;
+	private CommandStack commandStack;
+
+	public TransitionEditingComposite(Composite parent, TabbedPropertySheetWidgetFactory transitionWidgetFactory,
+			CommandExecutor commandExecutor) {
+		this.transitionWidgetFactory = transitionWidgetFactory;
+		this.transitionGroup = transitionWidgetFactory.createGroup(parent, "Transitions from selected state");
+		this.commandExecutor = commandExecutor;
+		type = null;
+		commandStack = null;
+		createGroupLayout();
+	}
+
+	private void createGroupLayout() {
+		transitionGroup.setLayout(new GridLayout(2, false));
+		transitionGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		AddDeleteReorderListWidget transitionMgmButtons = new AddDeleteReorderListWidget();
+		transitionMgmButtons.createControls(transitionGroup, transitionWidgetFactory);
+
+		transitionsOutViewer = TableWidgetFactory.createTableViewer(transitionGroup);
+		configureTransitionTableLayout(transitionsOutViewer.getTable());
+		transitionsOutViewer.setContentProvider(new StateContentProvider());
+		transitionsOutViewer.setLabelProvider(new TransitionListLabelProvider());
+
+		transitionMgmButtons.bindToTableViewer(transitionsOutViewer, commandExecutor,
+				ref -> new CreateTransitionCommand(type, getBasicFBType().getECC().getStart(), null),
+				ref -> new DeleteTransitionCommand((ECTransition) ref),
+				ref -> new ChangeTransitionPriorityCommand(type, (ECTransition) ref, true),
+				ref -> new ChangeTransitionPriorityCommand(type, (ECTransition) ref, false));
+	}
+
+	private void configureTransitionTableLayout(final Table table) {
+		new TableColumn(table, SWT.LEFT); // creates a table column without headline for the order numbering
+		new TableColumn(table, SWT.LEFT).setText("Destination");
+		new TableColumn(table, SWT.LEFT).setText("Event");
+		new TableColumn(table, SWT.LEFT).setText("Condition");
+		new TableColumn(table, SWT.LEFT).setText("Comment");
+
+		TableLayout tabLayout = new TableLayout();
+		tabLayout.addColumnData(new ColumnWeightData(1, 50));
+		tabLayout.addColumnData(new ColumnWeightData(2, 50));
+		tabLayout.addColumnData(new ColumnWeightData(3, 50));
+		tabLayout.addColumnData(new ColumnWeightData(4, 50));
+		tabLayout.addColumnData(new ColumnWeightData(5, 50));
+
+		table.setLayout(tabLayout);
+
+		transitionsOutViewer.setColumnProperties(new String[] { TRANSITION_PRIORITY, TRANSITION_DESTINATION,
+				TRANSITION_EVENT, TRANSITION_CONDITION, TRANSITION_COMMENT });
+	}
+
+	private CellEditor[] createTransitionViewerCellEditors(Table table) {
+		BasicFBType fbType = getBasicFBType();
+		return new CellEditor[] { null,
+				ComboBoxWidgetFactory.createComboBoxCellEditor(table,
+						ECCContentAndLabelProvider.getStateNames(fbType).toArray(new String[0]), SWT.READ_ONLY),
+				ComboBoxWidgetFactory.createComboBoxCellEditor(table,
+						ECCContentAndLabelProvider.getTransitionConditionEventNames(fbType).toArray(new String[0]),
+						SWT.READ_ONLY),
+				new TextCellEditor(table), new TextCellEditor(table) };
+	}
+
+	private BasicFBType getBasicFBType() {
+		return type.getECC().getBasicFBType();
+	}
+
+	public void refresh() {
+		CommandStack commandStackBuffer = commandStack;
+		commandStack = null;
+		if (null != type) {
+			transitionsOutViewer.setInput(type);
+		}
+		commandStack = commandStackBuffer;
+	}
+
+	public void setTypeAndCommandStack(ECState type, CommandStack commandStack) {
+		this.type = type;
+		this.commandStack = commandStack;
+
+		// have to do that here, because now we have a valid type
+		transitionsOutViewer.setCellEditors(createTransitionViewerCellEditors(transitionsOutViewer.getTable()));
+		transitionsOutViewer.setCellModifier(new TransitionViewerCellModifier());
+	}
+}
