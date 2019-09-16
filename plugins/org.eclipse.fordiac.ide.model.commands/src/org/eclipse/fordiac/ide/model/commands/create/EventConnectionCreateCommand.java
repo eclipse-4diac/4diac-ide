562bce9c6dc2c89df818722c83c8219b540567fa
Alois Zoitl <alois.zoitl@gmx.at>
2019-09-16 09:57:20 +0200

    [551042] Fixed checks for connections going and coming from subapps
    With this fix now the code for checking if source and destination need
    to be swapped (i.e., connections created reverse) is harmonized between
    the connection creation and the checks if a connection is allowed. The
    more correct code from connection creation has been used as starting
    point.
    
    Furthermore some code clean-up in the LinkConstraints class where
    performed.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=551042
    Change-Id: I7a8f31d32da2f52f62005bdcf41020b19f377f59
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/EventConnectionCreateCommand.java
+++ plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/EventConnectionCreateCommand.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2009, 2016, 2017 Profactor GmbH, fortiss GmbH
- * 
+ * 				 2019 Johannes Kepler University Linz
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -9,7 +10,8 @@
  *
  * Contributors:
  *   Gerhard Ebenhofer, Alois Zoitl
- *   - initial API and implementation and/or initial documentation
+ *     - initial API and implementation and/or initial documentation
+ *   Alois Zoitl - reworked and harmonized source/target checking 551042
  *******************************************************************************/
 package org.eclipse.fordiac.ide.model.commands.create;
 
@@ -20,12 +22,12 @@ import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
 
 /**
  * The Class EventConnectionCreateCommand.
- * 
+ *
  * @author Gerhard Ebenhofer, gerhard.ebenhofer@profactor.at
  */
 public class EventConnectionCreateCommand extends AbstractConnectionCreateCommand {
 
-	public EventConnectionCreateCommand(FBNetwork parent){
+	public EventConnectionCreateCommand(FBNetwork parent) {
 		super(parent);
 	}
 
@@ -36,46 +38,43 @@ public class EventConnectionCreateCommand extends AbstractConnectionCreateComman
 
 	@Override
 	public boolean canExecute() {
-		if (getSource() == null || getDestination() == null) {
+		if (!super.canExecute()) {
 			return false;
 		}
-		if(getSource() == getDestination()){
+
+		if (duplicateConnection()) {
 			return false;
 		}
-		if (!(getSource() instanceof Event)) {
-			return false;
-		}
-		if (!(getDestination() instanceof Event)) {
-			return false;
-		}
-		
-		if(duplicateConnection()){
-			return false;
-		}
-		
-		return LinkConstraints.canExistEventConnection((Event) getSource(), (Event)getDestination());
+
+		return LinkConstraints.canExistEventConnection((Event) getSource(), (Event) getDestination());
 	}
-	
+
 	private boolean duplicateConnection() {
 		for (Connection con : getSource().getInputConnections()) {
-			//as we are maybe creating a reverse connection we need to check both 
-			if((con.getSource() == getDestination()) || (con.getDestination() == getDestination())){
+			// as we are maybe creating a reverse connection we need to check both
+			if ((con.getSource() == getDestination()) || (con.getDestination() == getDestination())) {
 				return true;
 			}
 		}
 		for (Connection con : getSource().getOutputConnections()) {
-			//as we are maybe creating a reverse connection we need to check both
-			if((con.getSource() == getDestination()) || (con.getDestination() == getDestination())){
+			// as we are maybe creating a reverse connection we need to check both
+			if ((con.getSource() == getDestination()) || (con.getDestination() == getDestination())) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 
 	@Override
 	protected AbstractConnectionCreateCommand createMirroredConnectionCommand(FBNetwork fbNetwork) {
 		return new EventConnectionCreateCommand(fbNetwork);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	protected Class getInterfaceType() {
+		return Event.class;
 	}
 
 }
