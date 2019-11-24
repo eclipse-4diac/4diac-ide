3fb774c1535c5af55db8dee47d5ffa4837b7d354
Alois Zoitl <alois.zoitl@gmx.at>
2019-11-24 23:07:16 +0100

    [553405] First implementation of a go to parent handler
    With this new handler you can easily navigate to the parent an
    subapplication.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=553405
    Change-Id: Ib0846378e06380b9bcb143c3f2328686876b2137
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

---
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/GotoParentHandler.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Alois Zoitl - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.application.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.fordiac.ide.application.editors.ApplicationEditorInput;
+import org.eclipse.fordiac.ide.application.editors.FBNetworkEditor;
+import org.eclipse.fordiac.ide.application.editors.SubAppNetworkEditor;
+import org.eclipse.fordiac.ide.application.editors.SubApplicationEditorInput;
+import org.eclipse.fordiac.ide.model.libraryElement.Application;
+import org.eclipse.fordiac.ide.model.libraryElement.SubApp;
+import org.eclipse.fordiac.ide.ui.editors.EditorUtils;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.ISources;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class GotoParentHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		SubAppNetworkEditor editor = (SubAppNetworkEditor) HandlerUtil.getActiveEditor(event);
+
+		EObject model = editor.getModel().eContainer().eContainer().eContainer();
+		EditorUtils.openEditor(getEditorInput(model), getEditorId(model));
+
+		return Status.OK_STATUS;
+	}
+
+	@Override
+	public void setEnabled(Object evaluationContext) {
+		Object selection = HandlerUtil.getVariable(evaluationContext, ISources.ACTIVE_EDITOR_ID_NAME);
+		setBaseEnabled(SubAppNetworkEditor.class.getName().equals(selection));
+	}
+
+	private static IEditorInput getEditorInput(EObject model) {
+		if (model instanceof SubApp) {
+			return new SubApplicationEditorInput((SubApp) model);
+		}
+		if (model instanceof Application) {
+			return new ApplicationEditorInput((Application) model);
+		}
+		return null;
+	}
+
+	private static String getEditorId(EObject model) {
+		if (model instanceof SubApp) {
+			return SubAppNetworkEditor.class.getName();
+		}
+		if (model instanceof Application) {
+			return FBNetworkEditor.class.getName();
+		}
+		return null;
+	}
+
+}
