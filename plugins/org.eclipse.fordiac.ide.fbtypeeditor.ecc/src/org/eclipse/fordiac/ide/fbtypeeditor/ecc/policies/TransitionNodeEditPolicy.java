1d9ed3afb199a10409b4045c96e664d1b8bbd974
Alois Zoitl <alois.zoitl@gmx.at>
2019-12-19 22:08:58 +0100

    [558467] Reworked reconnect transition
    With this fix now if a reconnect transition is resulting in the same
    source and target no changes to the model are performed.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=558467
    Change-Id: I467b10f60b857dee9abf2b7283e92a69dfd2ec9c
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/policies/TransitionNodeEditPolicy.java
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/policies/TransitionNodeEditPolicy.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2011, 2013 Profactor GmbH, fortiss GmbH
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -17,6 +17,7 @@ import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.CreateTransitionCommand;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ReconnectTransitionCommand;
 import org.eclipse.fordiac.ide.model.libraryElement.ECState;
+import org.eclipse.fordiac.ide.model.libraryElement.ECTransition;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy;
 import org.eclipse.gef.requests.CreateConnectionRequest;
@@ -27,21 +28,15 @@ import org.eclipse.gef.requests.ReconnectRequest;
  */
 public class TransitionNodeEditPolicy extends GraphicalNodeEditPolicy {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#getConnectionCompleteCommand(org.eclipse.gef.requests.CreateConnectionRequest)
-	 */
 	@Override
-	protected Command getConnectionCompleteCommand(
-			final CreateConnectionRequest request) {
+	protected Command getConnectionCompleteCommand(final CreateConnectionRequest request) {
 		if (request.getStartCommand() instanceof CreateTransitionCommand) {
 			CreateTransitionCommand command = (CreateTransitionCommand) request.getStartCommand();
 			if (getHost().getModel() instanceof ECState) {
 
 				Point destination = request.getLocation().getCopy();
 				getHostFigure().translateToRelative(destination);
-				
+
 				command.setDestinationLocation(destination);
 				command.setDestination((ECState) getHost().getModel());
 				return command;
@@ -51,20 +46,14 @@ public class TransitionNodeEditPolicy extends GraphicalNodeEditPolicy {
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#getConnectionCreateCommand(org.eclipse.gef.requests.CreateConnectionRequest)
-	 */
 	@Override
-	protected Command getConnectionCreateCommand(
-			final CreateConnectionRequest request) {
+	protected Command getConnectionCreateCommand(final CreateConnectionRequest request) {
 
 		CreateTransitionCommand cmd = new CreateTransitionCommand();
 		if (getHost().getModel() instanceof ECState) {
 			Point source = request.getLocation().getCopy();
 			getHostFigure().translateToRelative(source);
-			
+
 			cmd.setSource((ECState) getHost().getModel());
 			cmd.setSourceLocation(source);
 			cmd.setViewer(getHost().getViewer());
@@ -73,24 +62,20 @@ public class TransitionNodeEditPolicy extends GraphicalNodeEditPolicy {
 		return cmd;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#getReconnectSourceCommand(org.eclipse.gef.requests.ReconnectRequest)
-	 */
 	@Override
 	protected Command getReconnectSourceCommand(final ReconnectRequest request) {
-		return new ReconnectTransitionCommand(request);
+		ECTransition transition = (ECTransition) request.getConnectionEditPart().getModel();
+		// check if the source has changed
+		return (transition.getSource().equals(request.getTarget().getModel())) ? null
+				: new ReconnectTransitionCommand(request);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy#getReconnectTargetCommand(org.eclipse.gef.requests.ReconnectRequest)
-	 */
 	@Override
 	protected Command getReconnectTargetCommand(final ReconnectRequest request) {
-		return new ReconnectTransitionCommand(request);
+		ECTransition transition = (ECTransition) request.getConnectionEditPart().getModel();
+		// check if the source has changed
+		return (transition.getDestination().equals(request.getTarget().getModel())) ? null
+				: new ReconnectTransitionCommand(request);
 	}
 
 }
