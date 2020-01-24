00cef9ef67d564fee441f4334f27913d35f666ac
Alois Zoitl <alois.zoitl@gmx.at>
2020-01-24 16:38:39 +0100

    [559297] Implemented output and input streams with ByteBuffer
    With this change the XML serialisation is performed on own streams
    without synchronized access method and no reallocation and copying. This
    allows to save even bigger systems (tested up to 1GB)
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=559297
    Change-Id: I25582df3a0e34a1254f646ba3fa5fe8acc30e582
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/dataexport/CommonElementExporter.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/dataexport/CommonElementExporter.java
@@ -17,10 +17,14 @@
  ********************************************************************************/
 package org.eclipse.fordiac.ide.model.dataexport;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -45,11 +49,95 @@ import org.eclipse.fordiac.ide.model.libraryElement.VersionInfo;
 
 abstract class CommonElementExporter {
 
+	private static class ByteBufferInputStream extends InputStream {
+
+		private final Iterator<ByteBuffer> bufferIterator;
+		private ByteBuffer currentDataBuffer;
+
+		public ByteBufferInputStream(List<ByteBuffer> dataBuffers) {
+			dataBuffers.forEach(ByteBuffer::flip);
+			bufferIterator = dataBuffers.iterator();
+			currentDataBuffer = bufferIterator.next();
+		}
+
+		@Override
+		public int read() throws IOException {
+			if (currentDataBuffer.hasRemaining()) {
+				return currentDataBuffer.get() & 0xFF;
+			} else if (bufferIterator.hasNext()) {
+				currentDataBuffer = bufferIterator.next();
+				return currentDataBuffer.get() & 0xFF;
+			}
+			return -1;
+		}
+
+		@Override
+		public int read(byte[] b) throws IOException {
+			if (currentDataBuffer.hasRemaining()) {
+				return super.read(b);
+			} else if (bufferIterator.hasNext()) {
+				currentDataBuffer = bufferIterator.next();
+				return super.read(b);
+			}
+			return -1;
+		}
+
+		@Override
+		public int read(byte[] b, int off, int len) throws IOException {
+			int readLen = Math.min(available(), len);
+			currentDataBuffer.get(b, off, readLen);
+			return readLen;
+		}
+
+		@Override
+		public int available() throws IOException {
+			return currentDataBuffer.remaining();
+		}
+
+	}
+
+	private static class ByteBufferOutputStream extends OutputStream {
+
+		private static final int SINGLE_DATA_BUFFER_CAPACITY = 500 * 1024 * 1024; //
+		private final List<ByteBuffer> dataBuffers = new ArrayList<>(5); // give it an initial capacity of 5 to reduce
+																			// reallocation
+		private ByteBuffer currentDataBuffer;
+
+		public ByteBufferOutputStream() {
+			addNewDataBuffer();
+		}
+
+		public List<ByteBuffer> getDataBuffers() {
+			return dataBuffers;
+		}
+
+		private void addNewDataBuffer() {
+			currentDataBuffer = ByteBuffer.allocateDirect(SINGLE_DATA_BUFFER_CAPACITY);
+			dataBuffers.add(currentDataBuffer);
+		}
+
+		@Override
+		public void write(int arg0) throws IOException {
+			if (!currentDataBuffer.hasRemaining()) {
+				addNewDataBuffer();
+			}
+			currentDataBuffer.put((byte) arg0);
+		}
+
+		@Override
+		public void write(byte[] b, int off, int len) throws IOException {
+			if (currentDataBuffer.remaining() < len) {
+				addNewDataBuffer();
+			}
+			currentDataBuffer.put(b, off, len);
+		}
+	}
+
 	public static final String LINE_END = "\n"; //$NON-NLS-1$
 	public static final String TAB = "\t"; //$NON-NLS-1$
 
 	private final XMLStreamWriter writer;
-	private ByteArrayOutputStream outputStream;
+	private ByteBufferOutputStream outputStream;
 
 	private int tabCount = 0;
 
@@ -99,7 +187,7 @@ abstract class CommonElementExporter {
 	private XMLStreamWriter createEventWriter() {
 		XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
 
-		outputStream = new ByteArrayOutputStream();
+		outputStream = new ByteBufferOutputStream();
 		try {
 			XMLStreamWriter newWriter = outputFactory.createXMLStreamWriter(outputStream,
 					StandardCharsets.UTF_8.name());
@@ -135,18 +223,17 @@ abstract class CommonElementExporter {
 	protected void writeToFile(IFile iFile) {
 		long startTime = System.currentTimeMillis();
 		try {
+			writer.writeCharacters(LINE_END);
 			writer.writeEndDocument();
 			writer.close();
-			outputStream.flush();
-			outputStream.close();
-			ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+			ByteBufferInputStream inputStream = new ByteBufferInputStream(outputStream.getDataBuffers());
 			if (iFile.exists()) {
 				iFile.setContents(inputStream, IResource.KEEP_HISTORY | IResource.FORCE, null);
 			} else {
 				checkAndCreateFolderHierarchy(iFile);
 				iFile.create(inputStream, IResource.KEEP_HISTORY | IResource.FORCE, null);
 			}
-		} catch (CoreException | IOException | XMLStreamException e) {
+		} catch (CoreException | XMLStreamException e) {
 			Activator.getDefault().logError(e.getMessage(), e);
 		}
 		long endTime = System.currentTimeMillis();
