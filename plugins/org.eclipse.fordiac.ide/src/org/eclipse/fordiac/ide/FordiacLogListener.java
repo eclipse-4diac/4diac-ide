3f6eff589ca8ce39093fa922d4732da3f3fdbe46
Alois Zoitl <alois.zoitl@gmx.at>
2020-06-19 11:00:00 +0200

    [562577] Changed loglistner to only show errors from 4diac IDE plugins
    As 4diac delivers now more (e.g., EGit) it makes sense that our
    log-listener is handling only 4diac IDE issues.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=562577
    Change-Id: I1e1422a72afe8b8071b270d6da49eb779774cdff
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide/src/org/eclipse/fordiac/ide/FordiacLogListener.java
+++ plugins/org.eclipse.fordiac.ide/src/org/eclipse/fordiac/ide/FordiacLogListener.java
@@ -63,9 +63,11 @@ public class FordiacLogListener implements ILogListener {
 	@Override
 	public void logging(IStatus status, String plugin) {
 
-		if ((status.getSeverity() == IStatus.ERROR) && (null != status.getException())) {
+		if ((status.getSeverity() == IStatus.ERROR) && (null != status.getException())
+				&& (status.getPlugin().contains("org.eclise.fordiac.ide"))) { //$NON-NLS-1$
 			// inform the user that an error has happened
-			// we currently only treat errors with exception as notworthy
+			// we currently only treat errors with exception and from a 4diac IDE plug-in as
+			// noteworthy
 			showErrorDialog(createStatusWithStackTrace(status));
 		}
 	}
