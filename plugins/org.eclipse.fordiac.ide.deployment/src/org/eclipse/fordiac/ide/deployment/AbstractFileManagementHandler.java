0bb3a9dc7c8397048a7ddd9e45f9f7731dc6f6d1
Ernst Blecha <ernst.blecha@jku.at>
2020-07-15 10:28:24 +0200

    [cleanup] fix missing copyright headers
    Change-Id: I82b704319d5fb3cf0aa9838042dbc739730329fb
    Signed-off-by: Ernst Blecha <ernst.blecha@jku.at>

--- plugins/org.eclipse.fordiac.ide.deployment/src/org/eclipse/fordiac/ide/deployment/AbstractFileManagementHandler.java
+++ plugins/org.eclipse.fordiac.ide.deployment/src/org/eclipse/fordiac/ide/deployment/AbstractFileManagementHandler.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2018 fortiss GmbH
+ * 				 2020 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Jose Maria Jesus Cabral Lassalle, Alois Zoitl
+ *     - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.fordiac.ide.deployment;
 
 import java.io.File;
