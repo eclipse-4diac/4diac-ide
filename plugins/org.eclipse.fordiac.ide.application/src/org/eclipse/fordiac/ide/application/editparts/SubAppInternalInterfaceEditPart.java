50b37653fe7f7a748c46e8382a19c271c007da89
Alois Zoitl <alois.zoitl@gmx.at>
2020-07-21 18:49:41 +0200

    [565404] Leave subapps by double clicking on their interface element
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=565404
    Change-Id: I559b73ae613ba33bf5e6bd1428291010136d3675
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/editparts/SubAppInternalInterfaceEditPart.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/editparts/SubAppInternalInterfaceEditPart.java
@@ -1,5 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2017 - 2018 fortiss GmbH, Johannes Kepler University
+ * Copyright (c) 2017 - 2018 fortiss GmbH
+ *               2018 - 2020 Johannes Kepler University
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -8,14 +10,27 @@
  *
  * Contributors:
  *   Alois Zoitl - initial API and implementation and/or initial documentation
+ *               - allow navigation to parent by double-clicking on subapp 
+ *                 interface element
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.editparts;
 
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.fordiac.ide.application.editors.ApplicationEditorInput;
+import org.eclipse.fordiac.ide.application.editors.FBNetworkEditor;
+import org.eclipse.fordiac.ide.application.editors.SubAppNetworkEditor;
+import org.eclipse.fordiac.ide.application.editors.SubApplicationEditorInput;
 import org.eclipse.fordiac.ide.application.policies.DeleteSubAppInterfaceElementPolicy;
 import org.eclipse.fordiac.ide.gef.draw2d.ConnectorBorder;
 import org.eclipse.fordiac.ide.gef.figures.ToolTipFigure;
+import org.eclipse.fordiac.ide.model.libraryElement.Application;
+import org.eclipse.fordiac.ide.model.libraryElement.SubApp;
+import org.eclipse.fordiac.ide.ui.editors.EditorUtils;
 import org.eclipse.gef.EditPolicy;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.RequestConstants;
+import org.eclipse.ui.IEditorInput;
 
 public class SubAppInternalInterfaceEditPart extends UntypedSubAppInterfaceElementEditPart {
 
@@ -47,6 +62,43 @@ public class SubAppInternalInterfaceEditPart extends UntypedSubAppInterfaceEleme
 	protected void refreshVisuals() {
 		getFigure().setToolTip(new ToolTipFigure(getModel()));
 		super.refreshVisuals();
+	}
+
+	@Override
+	public void performRequest(final Request request) {
+		if (request.getType() == RequestConstants.REQ_OPEN) {
+			// REQ_OPEN -> doubleclick
+			goToParent();
+		} else {
+			super.performRequest(request);
+		}
+	}
+
+	private void goToParent() {
+		EObject perentModel = getModel().getFBNetworkElement().eContainer().eContainer();
+		FBNetworkEditor newEditor = (FBNetworkEditor) EditorUtils.openEditor(getEditorInput(perentModel),
+				getEditorId(perentModel));
+		newEditor.selectElement(getModel());
+	}
+
+	private static IEditorInput getEditorInput(EObject model) {
+		if (model instanceof SubApp) {
+			return new SubApplicationEditorInput((SubApp) model);
+		}
+		if (model instanceof Application) {
+			return new ApplicationEditorInput((Application) model);
+		}
+		return null;
+	}
+
+	private static String getEditorId(EObject model) {
+		if (model instanceof SubApp) {
+			return SubAppNetworkEditor.class.getName();
+		}
+		if (model instanceof Application) {
+			return FBNetworkEditor.class.getName();
+		}
+		return null;
 	}
 
 }
