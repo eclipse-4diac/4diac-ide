003330c954336195e613e479709bd71c994f8434
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2020-07-28 17:27:53 +0200

    [565606] filter interface property sections for typed subapp
    Within the Subapp editor the property sheets for editing the interface
    are always shown. Within an application editor (clicking on Subapp from
    outside), the property sheets for editing the interfaces are only shown
    for untyped subapps.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=565606
    Change-Id: If564fbffe8526d1bcacb3f2f60fa15218c34af30
    Signed-off-by: Bianca Wiesmayr <bianca.wiesmayr@jku.at>

---
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/SubappTypeFilter.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Bianca Wiesmayr
+ *     - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.application.properties;
+
+import org.eclipse.fordiac.ide.application.editparts.SubAppForFBNetworkEditPart;
+import org.eclipse.fordiac.ide.application.editparts.UISubAppNetworkEditPart;
+import org.eclipse.jface.viewers.IFilter;
+
+public class SubappTypeFilter implements IFilter {
+
+	@Override
+	public boolean select(Object toTest) {
+		if (toTest instanceof SubAppForFBNetworkEditPart) {
+			return ((SubAppForFBNetworkEditPart) toTest).getModel().getType() == null; // only for untyped
+		}
+		if (toTest instanceof UISubAppNetworkEditPart) {
+			return true;
+		}
+		return false;
+	}
+}
