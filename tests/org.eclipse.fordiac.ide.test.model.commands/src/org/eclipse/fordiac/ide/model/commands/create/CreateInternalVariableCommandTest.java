5feb0fa5fe515196bf0a3bc0f85e956962111c68
Ernst Blecha <ernst.blecha@jku.at>
2020-10-13 09:29:32 +0200

    [563935] add test for ChangeVariableOrderCommand
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=563935
    Change-Id: I0c469059099aee95cc1031506cf8874c1cbc95da
    Signed-off-by: Ernst Blecha <ernst.blecha@jku.at>

--- tests/org.eclipse.fordiac.ide.test.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/CreateInternalVariableCommandTest.java
+++ tests/org.eclipse.fordiac.ide.test.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/CreateInternalVariableCommandTest.java
@@ -17,7 +17,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.fordiac.ide.model.FordiacKeywords;
-import org.eclipse.fordiac.ide.model.commands.testinfra.CommandTestBase;
+import org.eclipse.fordiac.ide.model.commands.change.ChangeVariableOrderCommand;
 import org.eclipse.fordiac.ide.model.commands.testinfra.CreateInternalVariableCommandTestBase;
 import org.eclipse.fordiac.ide.model.libraryElement.BaseFBType;
 import org.junit.jupiter.params.provider.Arguments;
@@ -25,6 +25,10 @@ import org.junit.jupiter.params.provider.Arguments;
 //see org.eclipse.fordiac.ide.util.ColorHelperTest.java for information on implementing tests
 
 public class CreateInternalVariableCommandTest extends CreateInternalVariableCommandTestBase {
+
+	private static final String VARIABLE1_NAME = FordiacKeywords.VARIABLE_INTERNAL;
+	private static final String VARIABLE2_NAME = "InternalVar2"; //$NON-NLS-1$
+	private static final String VARIABLE3_NAME = "INTERNALVAR1"; //$NON-NLS-1$
 
 	private static State executeCommandVar1(State state) {
 		BaseFBType baseFBType = getBaseFBType(state, assumption);
@@ -38,7 +42,7 @@ public class CreateInternalVariableCommandTest extends CreateInternalVariableCom
 	private static void verifyStateVar1(State state, State oldState, TestFunction t) {
 		BaseFBType baseFBType = getBaseFBType(state, t);
 		t.test(!baseFBType.getInternalVars().isEmpty());
-		t.test(baseFBType.getInternalVars().get(0).getName(), FordiacKeywords.VARIABLE_INTERNAL);
+		t.test(baseFBType.getInternalVars().get(0).getName(), VARIABLE1_NAME);
 		t.test(baseFBType.getInternalVars().get(0).getTypeName(), FordiacKeywords.BOOL);
 	}
 
@@ -55,7 +59,7 @@ public class CreateInternalVariableCommandTest extends CreateInternalVariableCom
 	private static void verifyStateVar2(State state, State oldState, TestFunction t) {
 		verifyStateVar1(state, oldState, t);
 		BaseFBType baseFBType = getBaseFBType(state, t);
-		t.test(baseFBType.getInternalVars().get(1).getName(), "InternalVar2"); //$NON-NLS-1$
+		t.test(baseFBType.getInternalVars().get(1).getName(), VARIABLE2_NAME);
 		t.test(baseFBType.getInternalVars().get(1).getTypeName(), FordiacKeywords.DWORD);
 	}
 
@@ -71,8 +75,24 @@ public class CreateInternalVariableCommandTest extends CreateInternalVariableCom
 	private static void verifyStateVar3(State state, State oldState, TestFunction t) {
 		verifyStateVar2(state, oldState, t);
 		BaseFBType baseFBType = getBaseFBType(state, t);
-		t.test(baseFBType.getInternalVars().get(2).getName(), "INTERNALVAR1"); //$NON-NLS-1$
+		t.test(baseFBType.getInternalVars().get(2).getName(), VARIABLE3_NAME);
 		t.test(baseFBType.getInternalVars().get(2).getTypeName(), FordiacKeywords.BOOL);
+	}
+
+	private static State executeReorder(State state, int index, boolean direction) {
+		final BaseFBType baseFBType = getBaseFBType(state, assumption);
+		state.setCommand(new ChangeVariableOrderCommand(baseFBType.getInternalVars(),
+				baseFBType.getInternalVars().get(index), direction));
+		return commandExecution(state);
+	}
+
+	private static void verifyOrder(State state, State oldState, TestFunction t, String name1, String name2,
+			String name3) {
+		final BaseFBType baseFBType = getBaseFBType(state, t);
+		t.test(baseFBType.getInternalVars().size(), 3);
+		t.test(baseFBType.getInternalVars().get(0).getName(), name1);
+		t.test(baseFBType.getInternalVars().get(1).getName(), name2);
+		t.test(baseFBType.getInternalVars().get(2).getName(), name3);
 	}
 
 	// parameter creation function
@@ -89,7 +109,23 @@ public class CreateInternalVariableCommandTest extends CreateInternalVariableCom
 				new ExecutionDescription<>("Add a third internal variable", //$NON-NLS-1$
 						CreateInternalVariableCommandTest::executeCommandVar3, //
 						CreateInternalVariableCommandTest::verifyStateVar3 //
-				) //
+				), //
+				new ExecutionDescription<>("move second algorithmn to third place", //$NON-NLS-1$
+						(State s) -> executeReorder(s, 1, false), //
+						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE1_NAME, VARIABLE3_NAME,
+								VARIABLE2_NAME)), //
+				new ExecutionDescription<>("move second algorithmn to first place", //$NON-NLS-1$
+						(State s) -> executeReorder(s, 1, true), //
+						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE3_NAME, VARIABLE1_NAME,
+								VARIABLE2_NAME)), //
+				new ExecutionDescription<>("move first algorithmn past lower bound", //$NON-NLS-1$
+						(State s) -> executeReorder(s, 0, true), //
+						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE3_NAME, VARIABLE1_NAME,
+								VARIABLE2_NAME)), //
+				new ExecutionDescription<>("move third algorithmn past upper bound", //$NON-NLS-1$
+						(State s) -> executeReorder(s, 2, false), //
+						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE3_NAME, VARIABLE1_NAME,
+								VARIABLE2_NAME)) //
 		);
 
 		return createCommands(executionDescriptions);
