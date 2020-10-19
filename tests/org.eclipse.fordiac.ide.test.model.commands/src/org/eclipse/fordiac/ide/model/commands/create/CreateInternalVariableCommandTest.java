297d37bda9dd622a1b65ad643c635e40b23d8766
Ernst Blecha <ernst.blecha@jku.at>
2020-10-19 13:48:25 +0200

    [563935] test for DeleteInternalVariableCommand
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=563935
    Change-Id: I6dd333b479174b47bea1028b9ff53dbff0da1192
    Signed-off-by: Ernst Blecha <ernst.blecha@jku.at>

--- tests/org.eclipse.fordiac.ide.test.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/CreateInternalVariableCommandTest.java
+++ tests/org.eclipse.fordiac.ide.test.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/CreateInternalVariableCommandTest.java
@@ -18,8 +18,10 @@ import java.util.List;
 
 import org.eclipse.fordiac.ide.model.FordiacKeywords;
 import org.eclipse.fordiac.ide.model.commands.change.ChangeVariableOrderCommand;
+import org.eclipse.fordiac.ide.model.commands.delete.DeleteInternalVariableCommand;
 import org.eclipse.fordiac.ide.model.commands.testinfra.CreateInternalVariableCommandTestBase;
 import org.eclipse.fordiac.ide.model.libraryElement.BaseFBType;
+import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 import org.junit.jupiter.params.provider.Arguments;
 
 //see org.eclipse.fordiac.ide.util.ColorHelperTest.java for information on implementing tests
@@ -95,6 +97,22 @@ public class CreateInternalVariableCommandTest extends CreateInternalVariableCom
 		t.test(baseFBType.getInternalVars().get(2).getName(), name3);
 	}
 
+	private static State executeDeleteVariable(State state) {
+		final BaseFBType baseFBType = getBaseFBType(state, assumption);
+		final VarDeclaration v = baseFBType.getInternalVars().get(0);
+		assumption.test(v.getName(), VARIABLE3_NAME);
+		state.setCommand(new DeleteInternalVariableCommand(baseFBType, v));
+		return commandExecution(state);
+	}
+
+	private static void verifyDelete(State state, State oldState, TestFunction t) {
+		final BaseFBType baseFBType = getBaseFBType(state, t);
+		t.test(baseFBType.getInternalVars().size(),
+				((BaseFBType) oldState.getFunctionblock().getType()).getInternalVars().size() - 1);
+		t.test(baseFBType.getInternalVars().get(0).getName(), VARIABLE1_NAME);
+		t.test(baseFBType.getInternalVars().get(1).getName(), VARIABLE2_NAME);
+	}
+
 	// parameter creation function
 	public static Collection<Arguments> data() {
 		final List<ExecutionDescription<?>> executionDescriptions = List.of( //
@@ -113,19 +131,27 @@ public class CreateInternalVariableCommandTest extends CreateInternalVariableCom
 				new ExecutionDescription<>("move second algorithmn to third place", //$NON-NLS-1$
 						(State s) -> executeReorder(s, 1, false), //
 						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE1_NAME, VARIABLE3_NAME,
-								VARIABLE2_NAME)), //
+								VARIABLE2_NAME)//
+				), //
 				new ExecutionDescription<>("move second algorithmn to first place", //$NON-NLS-1$
 						(State s) -> executeReorder(s, 1, true), //
 						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE3_NAME, VARIABLE1_NAME,
-								VARIABLE2_NAME)), //
+								VARIABLE2_NAME)//
+				), //
 				new ExecutionDescription<>("move first algorithmn past lower bound", //$NON-NLS-1$
 						(State s) -> executeReorder(s, 0, true), //
 						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE3_NAME, VARIABLE1_NAME,
-								VARIABLE2_NAME)), //
+								VARIABLE2_NAME)//
+				), //
 				new ExecutionDescription<>("move third algorithmn past upper bound", //$NON-NLS-1$
 						(State s) -> executeReorder(s, 2, false), //
 						(State s, State o, TestFunction t) -> verifyOrder(s, o, t, VARIABLE3_NAME, VARIABLE1_NAME,
-								VARIABLE2_NAME)) //
+								VARIABLE2_NAME)//
+				), //
+				new ExecutionDescription<>("delete first entry", //$NON-NLS-1$
+						CreateInternalVariableCommandTest::executeDeleteVariable, //
+						CreateInternalVariableCommandTest::verifyDelete //
+				)//
 		);
 
 		return createCommands(executionDescriptions);
