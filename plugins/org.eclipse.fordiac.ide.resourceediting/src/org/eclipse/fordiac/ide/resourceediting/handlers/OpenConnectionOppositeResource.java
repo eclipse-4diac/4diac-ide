72a880d7d5046e8c0cd77b0edab011e09be44c66
Alexander Lumplecker <alexander.lumplecker@jku.at>
2020-10-19 13:00:33 +0200

    [528922] Virtual IO tool tip fixed NullPointerException
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=528922
    Change-Id: Ic993b1c01c94b9cbc5ec04ea3a1844e8b06b5b97
    Signed-off-by: Alexander Lumplecker <alexander.lumplecker@jku.at>

--- plugins/org.eclipse.fordiac.ide.resourceediting/src/org/eclipse/fordiac/ide/resourceediting/handlers/OpenConnectionOppositeResource.java
+++ plugins/org.eclipse.fordiac.ide.resourceediting/src/org/eclipse/fordiac/ide/resourceediting/handlers/OpenConnectionOppositeResource.java
@@ -1,11 +1,19 @@
 /*******************************************************************************
  * Copyright (c) 2017 fortiss GmbH
+ * 				 2020 Primetals Technologies Germany GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
  * SPDX-License-Identifier: EPL-2.0 3
+ *
+ * Contributors:
+ *   Alois Zoitl - initial implementation and/or documentation
+ *   Alexander Lumplecker
+ *     - extraced getOppositeInterfaceelement and getConnectionOpposite
+ *       to ConnectionsHelper
+ *     - added getOppositeMappedIE
  *******************************************************************************/
 package org.eclipse.fordiac.ide.resourceediting.handlers;
 
@@ -18,8 +26,8 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.fordiac.ide.gef.AdvancedScrollingGraphicalViewer;
+import org.eclipse.fordiac.ide.model.helpers.ConnectionsHelper;
 import org.eclipse.fordiac.ide.model.libraryElement.Connection;
-import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
 import org.eclipse.fordiac.ide.model.libraryElement.Resource;
 import org.eclipse.fordiac.ide.resourceediting.editors.ResourceDiagramEditor;
@@ -35,11 +43,11 @@ public class OpenConnectionOppositeResource extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		final ISelection selection = HandlerUtil.getCurrentSelection(event);
 		if (selection instanceof IStructuredSelection) {
-			Object first = ((IStructuredSelection) selection).getFirstElement();
+			final Object first = ((IStructuredSelection) selection).getFirstElement();
 			if (first instanceof VirtualInOutputEditPart) {
-				IInterfaceElement oppositeMappedIE = getOppositeInterfaceelement((VirtualInOutputEditPart) first);
+				final IInterfaceElement oppositeMappedIE = getOppositeMappedIE((VirtualInOutputEditPart) first);
 				if (null != oppositeMappedIE) {
 					openResource(oppositeMappedIE);
 				}
@@ -50,55 +58,43 @@ public class OpenConnectionOppositeResource extends AbstractHandler {
 
 	@Override
 	public void setEnabled(Object evaluationContext) {
-		IEvaluationContext ctx = (IEvaluationContext) evaluationContext;
+		final IEvaluationContext ctx = (IEvaluationContext) evaluationContext;
 		Object obj = ctx.getDefaultVariable();
 
 		if (obj instanceof List) {
-			List<?> list = (List<?>) obj;
+			final List<?> list = (List<?>) obj;
 			if (!list.isEmpty()) {
 				obj = list.get(0);
 			}
 		}
 		if (obj instanceof VirtualInOutputEditPart) {
-			setBaseEnabled(null != getOppositeInterfaceelement((VirtualInOutputEditPart) obj));
+			setBaseEnabled(null != getOppositeMappedIE((VirtualInOutputEditPart) obj));
 		} else {
 			setBaseEnabled(false);
 		}
 	}
 
-	private static IInterfaceElement getOppositeInterfaceelement(VirtualInOutputEditPart vIOEditPart) {
-		IInterfaceElement ie = vIOEditPart.getModel().getReferencedInterfaceElement();
-		IInterfaceElement fbOppostiteIE = ie.getFBNetworkElement().getOpposite().getInterfaceElement(ie.getName());
-		if (null != fbOppostiteIE) {
-			IInterfaceElement connectionOpposite = getConnectionOpposite(fbOppostiteIE);
-			if (null != connectionOpposite && connectionOpposite.getFBNetworkElement().isMapped()) {
-				FBNetworkElement mappedOppositeElement = connectionOpposite.getFBNetworkElement().getOpposite();
-				return mappedOppositeElement.getInterfaceElement(connectionOpposite.getName());
-			}
-
-		}
-		return null;
-	}
-
-	private static IInterfaceElement getConnectionOpposite(IInterfaceElement fbOppostiteIE) {
-		EList<Connection> connections = (fbOppostiteIE.isIsInput()) ? fbOppostiteIE.getInputConnections()
-				: fbOppostiteIE.getOutputConnections();
-		if (!connections.isEmpty()) {
-			return (fbOppostiteIE.isIsInput()) ? connections.get(0).getSource() : connections.get(0).getDestination();
+	private static IInterfaceElement getOppositeMappedIE(VirtualInOutputEditPart vIOEditPart) {
+		final IInterfaceElement ie = vIOEditPart.getModel().getReferencedInterfaceElement();
+		final EList<Connection> ieConnections = ConnectionsHelper.getConnections(ie);
+		if (!ieConnections.isEmpty()) {
+			final IInterfaceElement oppositeMappedIE = ConnectionsHelper.getOppositeInterfaceElement(ie,
+					ieConnections.get(0));
+			return oppositeMappedIE;
 		}
 		return null;
 	}
 
 	private static void openResource(IInterfaceElement oppositeMappedIE) {
-		Resource res = oppositeMappedIE.getFBNetworkElement().getResource();
+		final Resource res = oppositeMappedIE.getFBNetworkElement().getResource();
 
 		if (null != res) {
-			IEditorPart editor = OpenListenerManager.openEditor(res);
+			final IEditorPart editor = OpenListenerManager.openEditor(res);
 			if (editor instanceof ResourceDiagramEditor) {
-				AdvancedScrollingGraphicalViewer viewer = ((ResourceDiagramEditor) editor).getViewer();
+				final AdvancedScrollingGraphicalViewer viewer = ((ResourceDiagramEditor) editor).getViewer();
 				if (viewer != null) {
-					Map<?, ?> map = viewer.getEditPartRegistry();
-					Object ieToSelect = map.get(oppositeMappedIE);
+					final Map<?, ?> map = viewer.getEditPartRegistry();
+					final Object ieToSelect = map.get(oppositeMappedIE);
 					if (ieToSelect instanceof EditPart) {
 						viewer.selectAndRevealEditPart((EditPart) ieToSelect);
 					}
