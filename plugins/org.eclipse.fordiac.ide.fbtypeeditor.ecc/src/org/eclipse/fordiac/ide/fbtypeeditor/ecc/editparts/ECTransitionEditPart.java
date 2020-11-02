a240e26a63a2213ded16d68a3fe3fd5312468538
Alois Zoitl <alois.zoitl@gmx.at>
2020-11-02 16:47:46 +0100

    [568443] Fixed refresh issue when deleting transitions
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=568443
    Change-Id: Ifa504ecb0f1887675b8d1cc7b0e970014311adc1
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/editparts/ECTransitionEditPart.java
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/editparts/ECTransitionEditPart.java
@@ -62,8 +62,12 @@ public class ECTransitionEditPart extends AbstractConnectionEditPart {
 		@Override
 		public void notifyChanged(Notification notification) {
 			super.notifyChanged(notification);
-			refreshTransitionTooltip();
-			refresh();
+			if (null != notification.getNewValue()) {
+				// if newValue is null we are most probably in the deletion process, no update
+				// is needed
+				refreshTransitionTooltip();
+				refresh();
+			}
 		}
 	};
 
