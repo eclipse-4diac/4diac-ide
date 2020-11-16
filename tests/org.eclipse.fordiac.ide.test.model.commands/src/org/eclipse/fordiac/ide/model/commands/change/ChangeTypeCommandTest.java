1ea574564e89270188c359dbd4f99ae06db38448
Ernst Blecha <ernst.blecha@jku.at>
2020-11-16 18:03:31 +0100

    [563935] add test for ChangeTypeCommand
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=563935
    Change-Id: Ibad200029f3f0b01be5e83b0ebdc9579a6289dc3
    Signed-off-by: Ernst Blecha <ernst.blecha@jku.at>

---
+++ tests/org.eclipse.fordiac.ide.test.model.commands/src/org/eclipse/fordiac/ide/model/commands/change/ChangeTypeCommandTest.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Primetals Technologies Germany GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Ernst Blecha
+ *     - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.model.commands.change;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.fordiac.ide.model.FordiacKeywords;
+import org.eclipse.fordiac.ide.model.commands.create.WithCreateTest;
+import org.eclipse.fordiac.ide.model.commands.testinfra.FBNetworkTestBase;
+import org.eclipse.fordiac.ide.model.libraryElement.InterfaceList;
+import org.junit.jupiter.params.provider.Arguments;
+
+public class ChangeTypeCommandTest extends FBNetworkTestBase {
+
+	private static State changeDataInputType(State state) {
+		final InterfaceList fb = state.getFunctionblock().getFBType().getInterfaceList();
+		
+		state.setCommand(new ChangeTypeCommand(fb.getInputVars().get(0), getDatatypelib().getType(FordiacKeywords.LWORD)));
+		return commandExecution(state);
+	}
+
+	private static void validateDataInputType(State state, State oldState, TestFunction t) {
+		final InterfaceList fb = state.getFunctionblock().getFBType().getInterfaceList();
+		t.test(fb.getInputVars().get(0).getTypeName(), FordiacKeywords.LWORD);
+		t.test(fb.getInputVars().get(0).getType(), getDatatypelib().getType(FordiacKeywords.LWORD));
+	}
+
+	private static void validateDataInputTypeNetworkElements(State state, State oldState, TestFunction t) {
+		final InterfaceList fb = state.getFunctionblock().getFBType().getInterfaceList();
+		final InterfaceList fb1 = state.getFbNetwork().getNetworkElements().get(0).getInterface();
+		final InterfaceList fb2 = state.getFbNetwork().getNetworkElements().get(1).getInterface();
+
+		t.test(fb1.getInputVars().get(0).getTypeName(), fb.getInputVars().get(0).getTypeName());
+		t.test(fb2.getInputVars().get(0).getTypeName(), fb.getInputVars().get(0).getTypeName());
+		t.test(fb1.getInputVars().get(0).getType(), fb.getInputVars().get(0).getType());
+		t.test(fb2.getInputVars().get(0).getType(), fb.getInputVars().get(0).getType());
+	}
+	
+	// parameter creation function
+	public static Collection<Arguments> data() {
+		final List<ExecutionDescription<?>> executionDescriptions = List.of( //
+				new ExecutionDescription<State>("Prepare Functionblocks", //$NON-NLS-1$
+						WithCreateTest::createInterfaceElements, //
+						WithCreateTest::verifyFBCreation //
+				), //
+				new ExecutionDescription<State>("Change Datatype of DataInput of FBType", //$NON-NLS-1$
+						ChangeTypeCommandTest::changeDataInputType, //
+						ChangeTypeCommandTest::validateDataInputType //
+				), //
+				new ExecutionDescription<State>("Update FBType to Network elements", //$NON-NLS-1$
+						WithCreateTest::updateNetworkElements, //
+						ChangeTypeCommandTest::validateDataInputTypeNetworkElements //
+				) //
+		);
+		return createCommands(executionDescriptions);
+	}
+
+}
