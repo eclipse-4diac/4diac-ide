0de7fe359c32d5954e4ece528d08fc7dd7e69c33
Alois Zoitl <alois.zoitl@gmx.at>
2021-03-17 18:27:42 +0100

    [567991] Added more logging for hard to trace issue in data type lib
    As reported in the bug there is in rare cases a problem with loading the
    data type lib. As it is hard to reproduce and logically not explainable
    this commits adds further logging to hopefully trace it down.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=567991
    Change-Id: Ic5fb74b333cc5d95c2dc37c51397c221c6e07d61
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/NamedElementComparator.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/NamedElementComparator.java
@@ -1,6 +1,6 @@
 /********************************************************************************
  * Copyright (c) 2016 - 2017 fortiss GmbH
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -22,10 +22,21 @@ public enum NamedElementComparator implements Comparator<INamedElement> {
 
 	INSTANCE;
 
-	private Collator col = Collator.getInstance();
+	private final Collator col = Collator.getInstance();
 
 	@Override
-	public int compare(INamedElement o1, INamedElement o2) {
+	public int compare(final INamedElement o1, final INamedElement o2) {
+		if (o1 == null) {
+			Activator.getDefault()
+					.logError("o1 was null, o2 value: " + o2 + " name: " + ((null != o2) ? o2.getName() : "Also null")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		}
+		if (o2 == null) {
+			Activator.getDefault()
+					.logError("o2 was null, o1 value: " + o1 + " name: " + ((null != o1) ? o1.getName() : "Also null")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		}
+		if (col == null) {
+			Activator.getDefault().logError("col2 was null"); //$NON-NLS-1$
+		}
 		return col.compare(o1.getName(), o2.getName());
 	}
 }
