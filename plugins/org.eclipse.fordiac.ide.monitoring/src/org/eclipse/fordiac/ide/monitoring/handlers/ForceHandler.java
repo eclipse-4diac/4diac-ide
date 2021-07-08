785d8eccab3b8e953192e77fe1fc73f08a2f7717
Alois Zoitl <alois.zoitl@gmx.at>
2021-07-08 21:28:20 +0200

    [574695] Validate literals on force value input
    With this fix only valid literal values are accepted for force inputs.
    
    bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=574695
    Change-Id: I3b96267c13524d6c6806f776735ba4d7d4d34cfa

--- plugins/org.eclipse.fordiac.ide.monitoring/src/org/eclipse/fordiac/ide/monitoring/handlers/ForceHandler.java
+++ plugins/org.eclipse.fordiac.ide.monitoring/src/org/eclipse/fordiac/ide/monitoring/handlers/ForceHandler.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016, 2018 fortiss GmbH, Johannes Kepler University
- * 
+ * Copyright (c) 2015, 2021 fortiss GmbH, Johannes Kepler University,
+ *                          Primetals Technologies Austria GmbH
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -9,7 +10,8 @@
  *
  * Contributors:
  *   Gerd Kainz, Alois Zoitl - initial API and implementation and/or initial documentation
- *   Alois Zoitl - Harmonized deployment and monitoring   
+ *   Alois Zoitl - Harmonized deployment and monitoring
+ *   			 - Added value validation for the force input dialog
  *******************************************************************************/
 package org.eclipse.fordiac.ide.monitoring.handlers;
 
@@ -17,9 +19,11 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.fordiac.ide.deployment.monitoringbase.MonitoringBaseElement;
 import org.eclipse.fordiac.ide.gef.editparts.InterfaceEditPart;
+import org.eclipse.fordiac.ide.model.data.DataType;
 import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 import org.eclipse.fordiac.ide.model.monitoring.MonitoringElement;
+import org.eclipse.fordiac.ide.model.validation.ValueValidator;
 import org.eclipse.fordiac.ide.monitoring.MonitoringManager;
 import org.eclipse.fordiac.ide.monitoring.editparts.MonitoringEditPart;
 import org.eclipse.jface.dialogs.InputDialog;
@@ -31,20 +35,21 @@ import org.eclipse.ui.handlers.HandlerUtil;
 public class ForceHandler extends AbstractMonitoringHandler {
 
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		super.execute(event);
-		StructuredSelection selection = (StructuredSelection) HandlerUtil.getCurrentSelection(event);
-		VarDeclaration variable = getVariable(selection.getFirstElement());
+		final StructuredSelection selection = (StructuredSelection) HandlerUtil.getCurrentSelection(event);
+		final VarDeclaration variable = getVariable(selection.getFirstElement());
 		if (null != variable) {
-			MonitoringManager manager = MonitoringManager.getInstance();
-			MonitoringBaseElement element = manager.getMonitoringElement(variable);
+			final MonitoringManager manager = MonitoringManager.getInstance();
+			final MonitoringBaseElement element = manager.getMonitoringElement(variable);
 			if (element instanceof MonitoringElement) {
-				MonitoringElement monitoringElement = (MonitoringElement) element;
+				final MonitoringElement monitoringElement = (MonitoringElement) element;
+				final DataType type = monitoringElement.getPort().getInterfaceElement().getType();
 
-				InputDialog input = new InputDialog(Display.getDefault().getActiveShell(), "Force Value", "Value",
+				final InputDialog input = new InputDialog(Display.getDefault().getActiveShell(), "Force Value", "Value",
 						monitoringElement.isForce() ? monitoringElement.getForceValue() : "", //$NON-NLS-1$
-						null);
-				int ret = input.open();
+								newValue -> ForceHandler.validateForceInput(type, newValue));
+				final int ret = input.open();
 				if (ret == org.eclipse.jface.window.Window.OK) {
 					manager.forceValue(monitoringElement, input.getValue());
 				}
@@ -53,18 +58,28 @@ public class ForceHandler extends AbstractMonitoringHandler {
 		return null;
 	}
 
+	private static String validateForceInput(final DataType type, final String newValue) {
+		if (!newValue.isBlank()) {
+			final String validationMsg = ValueValidator.validateValue(type, newValue);
+			if ((validationMsg != null) && (!validationMsg.trim().isEmpty())) {
+				return validationMsg;
+			}
+		}
+		return null;
+	}
+
 	@Override
-	public void setEnabled(Object evaluationContext) {
+	public void setEnabled(final Object evaluationContext) {
 		boolean needToAdd = false;
-		Object selection = HandlerUtil.getVariable(evaluationContext, ISources.ACTIVE_CURRENT_SELECTION_NAME);
+		final Object selection = HandlerUtil.getVariable(evaluationContext, ISources.ACTIVE_CURRENT_SELECTION_NAME);
 
 		if (selection instanceof StructuredSelection) {
-			StructuredSelection sel = (StructuredSelection) selection;
-			MonitoringManager manager = MonitoringManager.getInstance();
+			final StructuredSelection sel = (StructuredSelection) selection;
+			final MonitoringManager manager = MonitoringManager.getInstance();
 
 			if (1 == sel.size()) {
 				// only allow to force a value if only one element is selected
-				VarDeclaration variable = getVariable(sel.getFirstElement());
+				final VarDeclaration variable = getVariable(sel.getFirstElement());
 				if ((null != variable) && manager.containsPort(variable)) {
 					needToAdd = true;
 				}
@@ -73,7 +88,7 @@ public class ForceHandler extends AbstractMonitoringHandler {
 		setBaseEnabled(needToAdd);
 	}
 
-	static VarDeclaration getVariable(Object object) {
+	static VarDeclaration getVariable(final Object object) {
 		IInterfaceElement ie = null;
 		if (object instanceof InterfaceEditPart) {
 			ie = ((InterfaceEditPart) object).getModel();
