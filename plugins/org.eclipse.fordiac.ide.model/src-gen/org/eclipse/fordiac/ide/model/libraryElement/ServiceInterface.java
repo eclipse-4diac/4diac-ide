b35658e708b9f322a732d1c1823f6cc86c27da6a
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2021-08-03 18:20:30 +0200

    [575224] added utility methods to the service model
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=575224
    Change-Id: I3c7a2f74f445bdb97cd1d9592ba6e6bffe74ed90
    Signed-off-by: Bianca Wiesmayr <bianca.wiesmayr@jku.at>

--- plugins/org.eclipse.fordiac.ide.model/src-gen/org/eclipse/fordiac/ide/model/libraryElement/ServiceInterface.java
+++ plugins/org.eclipse.fordiac.ide.model/src-gen/org/eclipse/fordiac/ide/model/libraryElement/ServiceInterface.java
@@ -1,6 +1,6 @@
 /********************************************************************************
  * Copyright (c) 2008 - 2017 Profactor GmbH, TU Wien ACIN, fortiss GmbH
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -21,5 +21,17 @@ package org.eclipse.fordiac.ide.model.libraryElement;
  * @model
  * @generated */
 public interface ServiceInterface extends INamedElement {
+
+	/** <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
+	 * @model kind="operation" required="true"
+	 * @generated */
+	Service getService();
+
+	/** <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
+	 * @model kind="operation" required="true"
+	 * @generated */
+	boolean isLeftInterface();
 
 } // ServiceInterface
--- plugins/org.eclipse.fordiac.ide.model/src-gen/org/eclipse/fordiac/ide/model/libraryElement/ServiceSequence.java
+++ plugins/org.eclipse.fordiac.ide.model/src-gen/org/eclipse/fordiac/ide/model/libraryElement/ServiceSequence.java
@@ -1,13 +1,13 @@
 /**
  * *******************************************************************************
  * Copyright (c) 2008 - 2018 Profactor GmbH, TU Wien ACIN, fortiss GmbH
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *    Gerhard Ebenhofer, Alois Zoitl, Ingo Hegny, Monika Wenger, Martin Jobst
  *      - initial API and implementation and/or initial documentation
@@ -35,12 +35,18 @@ public interface ServiceSequence extends INamedElement {
 	/** Returns the value of the '<em><b>Service Transaction</b></em>' containment reference list. The list contents are
 	 * of type {@link org.eclipse.fordiac.ide.model.libraryElement.ServiceTransaction}. <!-- begin-user-doc --> <!--
 	 * end-user-doc -->
-	 * 
+	 *
 	 * @return the value of the '<em>Service Transaction</em>' containment reference list.
 	 * @see org.eclipse.fordiac.ide.model.libraryElement.LibraryElementPackage#getServiceSequence_ServiceTransaction()
 	 * @model containment="true" extendedMetaData="kind='element' name='ServiceTransaction'
 	 *        namespace='##targetNamespace'"
 	 * @generated */
 	EList<ServiceTransaction> getServiceTransaction();
+
+	/** <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
+	 * @model kind="operation" required="true"
+	 * @generated */
+	Service getService();
 
 } // ServiceSequence
