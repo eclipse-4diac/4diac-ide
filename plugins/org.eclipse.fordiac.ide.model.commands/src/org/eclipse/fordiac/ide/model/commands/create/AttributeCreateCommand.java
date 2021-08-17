7b095affd9b30a6b21edb7bcda0ef8952df3543d
WinterMelanie <melanie.winter.02@gmx.at>
2021-08-17 11:29:13 +0200

    [575434] AbstractAttributeSection consistent with other property sheets
    buttons replaced with AddDeleteWidget, insertion after selected
    attribute
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=575434
    Change-Id: I9ab473947a6e1976bb78991be887bdec0b66da3c
    Signed-off-by: WinterMelanie <melanie.winter.02@gmx.at>

--- plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/AttributeCreateCommand.java
+++ plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/AttributeCreateCommand.java
@@ -1,7 +1,8 @@
 /*******************************************************************************
  * Copyright (c) 2017 fortiss GmbH
  * 				 2021 Primetals Technologies Austria GmbH
- * 
+ *               2021 Johannes Kepler University Linz
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -13,27 +14,36 @@
  *     - initial API and implementation and/or initial documentation
  *   Daniel Lindhuber, Bianca Wiesmayr
  *     - extended to create specific attribute
+ *   Melanie Winter - addAttribute inserts after selection
  *******************************************************************************/
 package org.eclipse.fordiac.ide.model.commands.create;
 
 import org.eclipse.fordiac.ide.model.libraryElement.Attribute;
 import org.eclipse.fordiac.ide.model.libraryElement.ConfigurableObject;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
-import org.eclipse.gef.commands.Command;
+import org.eclipse.fordiac.ide.ui.providers.AbstractCreationCommand;
 
-public class AttributeCreateCommand extends Command {
+public class AttributeCreateCommand extends AbstractCreationCommand {
 	private final ConfigurableObject configurableObject;
 	private Attribute attribute;
+	private Attribute refElement;
 
 	private final String name;
 	private final String comment;
 	private final String value;
 
-	public AttributeCreateCommand(ConfigurableObject configurableObject) {
+
+	public AttributeCreateCommand(final ConfigurableObject configurableObject) {
 		this(configurableObject, "name", "comment", "value"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
-	public AttributeCreateCommand(ConfigurableObject configurableObject, String name, String comment, String value) {
+	public AttributeCreateCommand(final ConfigurableObject configurableObject, final Attribute refElement) {
+		this(configurableObject, "name", "comment", "value"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		this.refElement = refElement;
+	}
+
+	public AttributeCreateCommand(final ConfigurableObject configurableObject, final String name, final String comment,
+			final String value) {
 		this.configurableObject = configurableObject;
 		this.name = name;
 		this.comment = comment;
@@ -51,7 +61,7 @@ public class AttributeCreateCommand extends Command {
 		attribute.setName(name);
 		attribute.setComment(comment);
 		attribute.setValue(value);
-		redo();
+		addAttribute();
 	}
 
 	@Override
@@ -61,7 +71,20 @@ public class AttributeCreateCommand extends Command {
 
 	@Override
 	public void redo() {
-		configurableObject.getAttributes().add(attribute);
+		addAttribute();
 	}
 
+	private void addAttribute() {
+		if (null == refElement) {
+			configurableObject.getAttributes().add(attribute);
+		} else {
+			final int index = configurableObject.getAttributes().indexOf(refElement) + 1;
+			configurableObject.getAttributes().add(index, attribute);
+		}
+	}
+
+	@Override
+	public Object getCreatedElement() {
+		return attribute;
+	}
 }
