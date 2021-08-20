a194c79a12248a747344a4c680c81d57ceac2606
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2021-08-20 11:34:41 +0200

    [cleanup] add missing copyright header
    Change-Id: I683f3bb379d7982a06eafe50db6ecad898691cf7
    Signed-off-by: Bianca Wiesmayr <bianca.wiesmayr@jku.at>

--- plugins/org.eclipse.fordiac.ide.fbtypeeditor.servicesequence/src/org/eclipse/fordiac/ide/fbtypeeditor/servicesequence/widgets/InterfaceSelectorButton.java
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.servicesequence/src/org/eclipse/fordiac/ide/fbtypeeditor/servicesequence/widgets/InterfaceSelectorButton.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Bianca Wiesmayr, Melanie Winter
+ *     - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
 package org.eclipse.fordiac.ide.fbtypeeditor.servicesequence.widgets;
 
 import java.util.function.Consumer;
