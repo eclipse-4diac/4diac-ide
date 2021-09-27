29bb119288e3783dd0450f86ffb354c587ecfb84
Ernst Blecha <ernst.blecha@jku.at>
2021-09-27 17:01:24 +0200

    [576654] add tests for cognitive complexity metric
    Change-Id: I749d47d3bf8d9a1c7162a642a8c83addd1c2a967
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=576654

---
+++ tests/org.eclipse.fordiac.ide.test.metrics/src/org/eclipse/fordiac/ide/test/metrics/AbstractCognitiveComplexityTest.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Johannes Kepler University
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Ernst Blecha
+ *     - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.test.metrics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.eclipse.fordiac.ide.metrics.analyzers.CognitiveComplexity;
+import org.eclipse.fordiac.ide.model.Palette.FBTypePaletteEntry;
+import org.eclipse.fordiac.ide.model.Palette.PaletteFactory;
+import org.eclipse.fordiac.ide.model.libraryElement.BasicFBType;
+import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
+import org.eclipse.fordiac.ide.model.libraryElement.STAlgorithm;
+import org.eclipse.fordiac.ide.model.structuredtext.StructuredTextStandaloneSetup;
+import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
+import org.eclipse.fordiac.ide.model.xtext.fbt.FBTypeStandaloneSetup;
+import org.junit.jupiter.api.BeforeAll;
+
+// Actual test-cases (multiline-strings) are implemented using xtend in CognitiveComplexityTest.xtend
+@SuppressWarnings({ "squid:S1694", "squid:S1118" })
+abstract class AbstractCognitiveComplexityTest {
+
+	protected static final String FUNCTIONBLOCK_NAME = "functionblock"; //$NON-NLS-1$
+	protected static final String ALGORITHM_NAME = "algorithm"; //$NON-NLS-1$
+
+	@BeforeAll
+	/** initialize the Equinox extension registry substitute */
+	public static void doSetup() {
+		FBTypeStandaloneSetup.doSetup();
+		StructuredTextStandaloneSetup.doSetup();
+	}
+
+	protected static void verifyCognitiveComplexity(final String algorithm, final double expectedComplexity) {
+		final var alg = createSTAlgorithm(ALGORITHM_NAME, algorithm);
+		assertEquals(expectedComplexity, CognitiveComplexity.analyzeAlgorithm(alg));
+	}
+
+	private static FBTypePaletteEntry preparePaletteWithTypeLib() {
+		final FBTypePaletteEntry pallEntry = PaletteFactory.eINSTANCE.createFBTypePaletteEntry();
+		final TypeLibrary typelib = TypeLibrary.getTypeLibrary(null);
+		pallEntry.setPalette(typelib.getBlockTypeLib());
+		return pallEntry;
+	}
+
+	private static STAlgorithm createSTAlgorithm(final String algorithmName, final String algorithmText) {
+
+		// prepare a function block object including an interface list
+		final BasicFBType functionBlock = LibraryElementFactory.eINSTANCE.createBasicFBType();
+		functionBlock.setInterfaceList(LibraryElementFactory.eINSTANCE.createInterfaceList());
+		functionBlock.setName(FUNCTIONBLOCK_NAME);
+
+		functionBlock.setECC(LibraryElementFactory.eINSTANCE.createECC());
+
+		functionBlock.setPaletteEntry(preparePaletteWithTypeLib());
+
+		final STAlgorithm stAlg = LibraryElementFactory.eINSTANCE.createSTAlgorithm();
+		stAlg.setName(algorithmName);
+		stAlg.setText(algorithmText);
+
+		functionBlock.getAlgorithm().add(stAlg);
+
+		return stAlg;
+	}
+
+}
