026664d59b0d4298427e0fb4c54326e40dfa20c8
Alois Zoitl <alois.zoitl@gmx.at>
2021-10-25 13:38:06 +0200

    [576872] Show interface property sheet also for instance comments
    With this change it is also possible to edit the instance comment when
    the instance comment is selected in the graphical editor.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=576872
    Change-Id: I1b06f4bbd8855c3a532b928d5cc78380ee16e260

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/InterfaceSection.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/InterfaceSection.java
@@ -16,6 +16,7 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.properties;
 
+import org.eclipse.fordiac.ide.application.editparts.InstanceComment;
 import org.eclipse.fordiac.ide.gef.properties.AbstractInterfaceSection;
 import org.eclipse.fordiac.ide.model.libraryElement.FBNetwork;
 import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
@@ -37,6 +38,9 @@ public class InterfaceSection extends AbstractInterfaceSection {
 
 		if (input instanceof FBNetworkElement) {
 			return (FBNetworkElement) input;
+		}
+		if (input instanceof InstanceComment) {
+			return ((InstanceComment) input).getRefElement();
 		}
 		return null;
 	}
