b32f9171f05e83f283c4e22dfb1ea4e8e2055d5a
Alois Zoitl <alois.zoitl@gmx.at>
2021-11-29 21:55:33 +0100

    [577501] remove elements from group on adding to a new one
    When an element is added to a group now the element is removed from the
    old group first. Such the an element is always only in one group.
    
    This should also enable draggig from group to group.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=577501
    Change-Id: Iab15aff76f3ad0f8df918a24d570b0ff469518f2

--- plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/change/AddElementsToGroup.java
+++ plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/change/AddElementsToGroup.java
@@ -13,7 +13,9 @@
 package org.eclipse.fordiac.ide.model.commands.change;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.fordiac.ide.model.helpers.FBNetworkHelper;
 import org.eclipse.fordiac.ide.model.libraryElement.FBNetwork;
@@ -21,16 +23,33 @@ import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.fordiac.ide.model.libraryElement.Group;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.CompoundCommand;
+import org.eclipse.swt.graphics.Point;
 
 public class AddElementsToGroup extends Command {
 
 	private final Group targetGroup;
 	private final List<FBNetworkElement> elementsToAdd;
+	private final CompoundCommand removeFromOtherGroups = new CompoundCommand();
 	private org.eclipse.swt.graphics.Point offset;
 
 	public AddElementsToGroup(final Group targetGroup, final List<?> selection) {
 		this.targetGroup = targetGroup;
 		elementsToAdd = createElementList(selection);
+		identifyRemoveElements();
+	}
+
+	private void identifyRemoveElements() {
+		final Map<Group, List<FBNetworkElement>> groupMap = new HashMap<>();
+		// collect all entries that are in a group and store them by group
+		elementsToAdd.stream().filter(FBNetworkElement::isInGroup).forEach(el -> {
+			final List<FBNetworkElement> entry = groupMap.computeIfAbsent(el.getGroup(), group -> new ArrayList<>());
+			entry.add(el);
+		});
+		// for each entry in the map create one RemoveFromGroupCommand
+		groupMap.forEach(
+				(group, list) -> removeFromOtherGroups.add(new RemoveElementsFromGroup(list,
+						new Point(group.getPosition().getX(), group.getPosition().getY()))));
 	}
 
 	public List<FBNetworkElement> getElementsToAdd() {
@@ -44,12 +63,14 @@ public class AddElementsToGroup extends Command {
 
 	@Override
 	public void execute() {
+		removeFromOtherGroups.execute();
 		offset = FBNetworkHelper.removeXYOffsetForFBNetwork(elementsToAdd);
 		addElementsToGroup();
 	}
 
 	@Override
 	public void redo() {
+		removeFromOtherGroups.redo();
 		FBNetworkHelper.removeXYOffsetForFBNetwork(elementsToAdd);
 		addElementsToGroup();
 	}
@@ -58,6 +79,7 @@ public class AddElementsToGroup extends Command {
 	public void undo() {
 		FBNetworkHelper.moveFBNetworkByOffset(elementsToAdd, getOriginalPositionX(), getOriginalPositionY());
 		removeElementsFromGroup();
+		removeFromOtherGroups.undo();
 	}
 
 	private void addElementsToGroup() {
