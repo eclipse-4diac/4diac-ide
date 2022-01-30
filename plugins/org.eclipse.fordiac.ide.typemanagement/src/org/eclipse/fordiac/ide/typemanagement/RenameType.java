e66061de349113d1dd609ea20dd6e1eadf71b452
Alois Zoitl <alois.zoitl@gmx.at>
2022-01-30 13:34:14 +0100

    [578451] Rename participant checks editor dirty state
    When the editor of a renamed file is dirty user gets now the chance to
    save or abort the renaming process.
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=578451
    Change-Id: I5829cf3ba842a451262d1d6378283eff692f8bb6

--- plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/RenameType.java
+++ plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/RenameType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 -2017 fortiss GmbH
+ * Copyright (c) 2014, 2021 fortiss GmbH, Primetals Technologies GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -8,8 +8,7 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *   Alois Zoitl
- *     - initial API and implementation and/or initial documentation
+ *   Alois Zoitl - initial API and implementation and/or initial documentation
  *******************************************************************************/
 package org.eclipse.fordiac.ide.typemanagement;
 
@@ -20,23 +19,35 @@ import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.mapping.IResourceChangeDescriptionFactory;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.fordiac.ide.model.IdentifierVerifyer;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
+import org.eclipse.fordiac.ide.ui.editors.EditorUtils;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.RenameParticipant;
 import org.eclipse.ltk.core.refactoring.participants.ResourceChangeChecker;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class RenameType extends RenameParticipant {
 
-	private String oldName;
+	private IFile oldFile;
+
+	private String getOldName() {
+		return oldFile.getName();
+	}
 
 	@Override
 	protected boolean initialize(final Object element) {
 		if (element instanceof IFile) {
-			oldName = ((IFile) element).getName();
+			oldFile = ((IFile) element);
 		}
 		return (element instanceof IFile);
 	}
@@ -49,57 +60,74 @@ public class RenameType extends RenameParticipant {
 	@Override
 	public RefactoringStatus checkConditions(final IProgressMonitor pm, final CheckConditionsContext context)
 			throws OperationCanceledException {
+		final RefactoringStatus result = new RefactoringStatus();
+		checkEditor(result);
 		final ResourceChangeChecker resChecker = context.getChecker(ResourceChangeChecker.class);
 		final IResourceChangeDescriptionFactory deltaFactory = resChecker.getDeltaFactory();
 		final IResourceDelta delta = deltaFactory.getDelta();
-		return verifyAffectedChildren(delta.getAffectedChildren());
+		verifyAffectedChildren(delta.getAffectedChildren(), result);
+		return result;
 	}
 
-	private RefactoringStatus verifyAffectedChildren(final IResourceDelta[] affectedChildren) {
+	private void verifyAffectedChildren(final IResourceDelta[] affectedChildren,
+			final RefactoringStatus result) {
 		for (final IResourceDelta resourceDelta : affectedChildren) {
-
 			if (resourceDelta.getMovedFromPath() != null) {
-
 				if (resourceDelta.getResource() instanceof IFile) {
 					final String newName = ((IFile) resourceDelta.getResource()).getName();
 					if (nameExistsInTypeLibrary(resourceDelta, newName)) {
-						return RefactoringStatus.createFatalErrorStatus(
-								MessageFormat.format(Messages.RenameType_TypeExists, newName));
+						result.addFatalError(MessageFormat.format(Messages.RenameType_TypeExists, newName));
 					}
+					final String name = TypeLibrary.getTypeNameFromFileName(newName);
+					if (name != null && !IdentifierVerifyer.isValidIdentifier(name)) {
+						getWrongIdentifierErrorStatus(result);
+					}
+				} else if (resourceDelta.getMovedToPath() == null) {
+					verifyAffectedChildren(resourceDelta.getAffectedChildren(), result);
 				}
-
-				final String name = getTypeName(resourceDelta);
-				if (name != null && !IdentifierVerifyer.isValidIdentifier(name)) {
-					return getWrongIdentifierErrorStatus();
-				}
-
-			} else if (resourceDelta.getMovedToPath() == null) {
-				return verifyAffectedChildren(resourceDelta.getAffectedChildren());
 			}
 		}
-
-		return new RefactoringStatus();
 	}
 
 	protected boolean nameExistsInTypeLibrary(final IResourceDelta resourceDelta, final String newName) {
-		return !oldName.equals(newName) && !newName.equals("a" + oldName) //$NON-NLS-1$
+		return !getOldName().equals(newName) && !newName.equals("a" + getOldName()) //$NON-NLS-1$
 				&& TypeLibrary.getPaletteEntryForFile(((IFile) resourceDelta.getResource())) != null;
 	}
 
-	protected String getTypeName(final IResourceDelta resourceDelta) {
-		if (resourceDelta.getResource() instanceof IFile) {
-			return TypeLibrary.getTypeNameFromFile((IFile) resourceDelta.getResource());
-		}
-		return null;
-	}
-
-	protected RefactoringStatus getWrongIdentifierErrorStatus() {
-		return RefactoringStatus.createFatalErrorStatus(Messages.RenameType_InvalidIdentifierErrorMessage);
+	@SuppressWarnings("static-method")  // allow child classes to overwrite
+	protected void getWrongIdentifierErrorStatus(final RefactoringStatus result) {
+		result.addFatalError(Messages.RenameType_InvalidIdentifierErrorMessage);
 	}
 
 	@Override
 	public Change createChange(final IProgressMonitor pm) throws CoreException, OperationCanceledException {
 		return null;
+	}
+
+	private void checkEditor(final RefactoringStatus result) {
+		// depending if the in-place renaming is active we may not be in the display thread
+		Display.getDefault().syncExec(() -> {
+			final IEditorPart findEditor = EditorUtils
+					.findEditor((final IEditorPart editor) -> editor.getEditorInput() instanceof FileEditorInput
+							&& ((FileEditorInput) editor.getEditorInput()).getFile().equals(oldFile));
+			if (findEditor != null && findEditor.isDirty()) {
+				if (shouldSaveFile(findEditor.getSite().getShell())) {
+					findEditor.doSave(new NullProgressMonitor());
+				} else {
+					result.addFatalError("Abort rename as editor is dirty!");
+				}
+			}
+		});
+	}
+
+	private boolean shouldSaveFile(final Shell shell) {
+		final int result = MessageDialog.open(
+				MessageDialog.QUESTION, shell, "Rename of Type with unsaved changes!",
+				MessageFormat.format(
+						"There are unsaved changes for type \"{0}\". Do you want to save them before renaming?",
+						TypeLibrary.getTypeNameFromFileName(getOldName())),
+				SWT.NONE, "Save", "Cancel");
+		return result == 0;
 	}
 
 }
