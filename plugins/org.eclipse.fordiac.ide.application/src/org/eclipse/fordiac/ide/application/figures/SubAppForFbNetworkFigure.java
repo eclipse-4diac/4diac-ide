35871fb0574849ae8aa4be81bee9fb38bb9ac221
Alois Zoitl <alois.zoitl@gmx.at>
2022-03-13 00:26:28 +0100

    Bug 579237 - moved comment part to the top of the subapp figure
    The comment part of an expanded subapp is now shown on the top of the
    subapp figure simmilar to groups. Such making space for the event
    connections.
    
    Furthermore direct edit on the subapp will now go to the comment of the
    expanded subapp similar as the group.
    
    Change-Id: I5284c42a0a7d5958c55590209f750ade2df87ddf

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/figures/SubAppForFbNetworkFigure.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/figures/SubAppForFbNetworkFigure.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2008 - 2017 Profactor GmbH, fortiss GmbH
- * 				 2019 Johannes Kepler University Linz
+ * Copyright (c) 2008 - 2022 Profactor GmbH, fortiss GmbH,
+ *                           Johannes Kepler University Linz
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -14,9 +14,13 @@
  *   Alois Zoitl - separated FBNetworkElement from instance name for better
  *                 direct editing of instance names
  *               - extracted common FB shape for interface and fbn editors
+ *               - hide middle part when expanded
+ *               - show comment part on top of the subapp figure
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.figures;
 
+import org.eclipse.draw2d.GridData;
+import org.eclipse.draw2d.MarginBorder;
 import org.eclipse.fordiac.ide.application.editparts.SubAppForFBNetworkEditPart;
 import org.eclipse.fordiac.ide.gef.figures.FBShapeShadowBorder;
 import org.eclipse.fordiac.ide.gef.figures.RoundedRectangleShadowBorder;
@@ -29,10 +33,16 @@ import org.eclipse.fordiac.ide.ui.imageprovider.FordiacImage;
  */
 public class SubAppForFbNetworkFigure extends FBNetworkElementFigure {
 
+	private InstanceCommentFigure commentFigure;
+
 	public SubAppForFbNetworkFigure(final SubApp model, final SubAppForFBNetworkEditPart editPart) {
 		super(model, editPart);
 		updateTypeLabel(model);
 		updateExpandedFigure();
+	}
+
+	public InstanceCommentFigure getCommentFigure() {
+		return commentFigure;
 	}
 
 	public void updateTypeLabel(final SubApp model) {
@@ -49,20 +59,47 @@ public class SubAppForFbNetworkFigure extends FBNetworkElementFigure {
 		return (SubApp) super.getModel();
 	}
 
+	public void refreshComment() {
+		if (commentFigure != null) {
+			commentFigure.setText(getModel().getComment());
+		}
+	}
+
 	public final void updateExpandedFigure() {
 		if (getModel().isUnfolded()) {
 			if (getMiddleContainer().getParent() != null) {
 				getFbFigureContainer().remove(getMiddleContainer());
 				setBorder(null);
 				getFbFigureContainer().setBorder(new RoundedRectangleShadowBorder());
+				addComment();
 			}
 		} else {
 			if (getMiddleContainer().getParent() == null) {
 				getFbFigureContainer().add(getMiddleContainer(), createMiddleLayoutData(), 1);
 				getFbFigureContainer().setBorder(null);
 				setBorder(new FBShapeShadowBorder());
+				removeComment();
 			}
 		}
 	}
 
+	private void addComment() {
+		commentFigure = new InstanceCommentFigure();
+		getTop().add(commentFigure, createCommentLayoutData(), 0);
+		commentFigure.setBorder(new MarginBorder(2, 2, 2, 2));
+		refreshComment();
+	}
+
+	private static GridData createCommentLayoutData() {
+		final GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL);
+		gridData.horizontalSpan = 3;
+		return gridData;
+	}
+
+	private void removeComment() {
+		if (commentFigure != null) {
+			commentFigure.getParent().remove(commentFigure);
+			commentFigure = null;
+		}
+	}
 }
