363b955923815bdb2f3487e095a65d7edc766efd
Alois Zoitl <alois.zoitl@gmx.at>
2022-03-27 21:34:24 +0200

    [clean-up] Always set the connection fbnetwork to the root of the editor
    This clean-up commit is preparational work for improving connection
    creation across expanded subapps. In this first step the FBNtwork for
    connections is always the the root fbnetowrk of the current editor page.
    Which can be best accessed via the root editpart and its adapt method.
    
    This also reduced code in the interface bar interface element editparts.
    
    Change-Id: I5118bc8a078f7007193d54ea7dd4bdb8744cb1d3

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/policies/InterfaceElementEditPolicy.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/policies/InterfaceElementEditPolicy.java
@@ -16,12 +16,9 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.policies;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.fordiac.ide.gef.editparts.InterfaceEditPart;
 import org.eclipse.fordiac.ide.model.commands.create.AbstractConnectionCreateCommand;
-import org.eclipse.fordiac.ide.model.libraryElement.CompositeFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.FBNetwork;
-import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.GraphicalNodeEditPolicy;
 import org.eclipse.gef.requests.CreateConnectionRequest;
@@ -31,7 +28,7 @@ public abstract class InterfaceElementEditPolicy extends GraphicalNodeEditPolicy
 
 	@Override
 	protected Command getConnectionCompleteCommand(final CreateConnectionRequest request) {
-		AbstractConnectionCreateCommand command = (AbstractConnectionCreateCommand) request.getStartCommand();
+		final AbstractConnectionCreateCommand command = (AbstractConnectionCreateCommand) request.getStartCommand();
 		command.setDestination(((InterfaceEditPart) getHost()).getModel());
 		return command;
 	}
@@ -47,17 +44,7 @@ public abstract class InterfaceElementEditPolicy extends GraphicalNodeEditPolicy
 	}
 
 	protected FBNetwork getParentNetwork() {
-		EObject obj = (EObject) getHost().getModel();
-		while ((obj != null) && !(obj instanceof FBNetworkElement || obj instanceof CompositeFBType)) {
-			obj = obj.eContainer();
-		}
-		if (obj instanceof CompositeFBType) {
-			return ((CompositeFBType) obj).getFBNetwork();
-		}
-		if (obj instanceof FBNetworkElement) {
-			return ((FBNetworkElement) obj).getFbNetwork();
-		}
-		return null;
+		return getHost().getRoot().getAdapter(FBNetwork.class);
 	}
 
 	protected abstract Command createReconnectCommand(ReconnectRequest request);
