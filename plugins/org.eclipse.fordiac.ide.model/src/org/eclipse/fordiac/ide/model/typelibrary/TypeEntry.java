921b91b1eceb95bdcd21fcfde7ce9b1819f27a8a
Alois Zoitl <alois.zoitl@gmx.at>
2022-04-10 00:46:12 +0200

    Bug 579648 - Reworked the TypeEntry interface and TypeEntryImpl class
    This commit updates the TypeEntry base interface and its TypeEntryImpl
    class to the new POJO style.
    
    Not yet usable!
    
    Change-Id: Ice7732759a9360be24eb6d8437edd2c753f3de66

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/TypeEntry.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/TypeEntry.java
@@ -16,130 +16,30 @@
 package org.eclipse.fordiac.ide.model.typelibrary;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
 
-/** <!-- begin-user-doc --> A representation of the model object '<em><b>Entry</b></em>'. <!-- end-user-doc -->
- *
- * <p>
- * The following features are supported:
- * </p>
- * <ul>
- * <li>{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getLabel <em>Label</em>}</li>
- * <li>{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getFile <em>File</em>}</li>
- * <li>{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getLastModificationTimestamp <em>Last Modification
- * Timestamp</em>}</li>
- * <li>{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getType <em>Type</em>}</li>
- * <li>{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getPalette <em>Palette</em>}</li>
- * <li>{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getTypeEditable <em>Type Editable</em>}</li>
- * </ul>
- *
- * @see org.eclipse.fordiac.ide.model.Palette.PalettePackage#getPaletteEntry()
- * @model abstract="true"
- * @generated */
-public interface TypeEntry extends EObject {
-	/** Returns the value of the '<em><b>Label</b></em>' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @return the value of the '<em>Label</em>' attribute.
-	 * @see #setLabel(String)
-	 * @see org.eclipse.fordiac.ide.model.Palette.PalettePackage#getPaletteEntry_Label()
-	 * @model
-	 * @generated */
-	String getLabel();
+public interface TypeEntry {
 
-	/** Sets the value of the '{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getLabel <em>Label</em>}'
-	 * attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @param value the new value of the '<em>Label</em>' attribute.
-	 * @see #getLabel()
-	 * @generated */
-	void setLabel(String value);
+	String getTypeName();
 
-	/** Returns the value of the '<em><b>File</b></em>' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @return the value of the '<em>File</em>' attribute.
-	 * @see #setFile(IFile)
-	 * @see org.eclipse.fordiac.ide.model.Palette.PalettePackage#getPaletteEntry_File()
-	 * @model dataType="org.eclipse.fordiac.ide.model.Palette.IFile"
-	 * @generated */
 	IFile getFile();
 
-	/** Sets the value of the '{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getFile <em>File</em>}'
-	 * attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @param value the new value of the '<em>File</em>' attribute.
-	 * @see #getFile()
-	 * @generated */
 	void setFile(IFile value);
 
-	/** Returns the value of the '<em><b>Last Modification Timestamp</b></em>' attribute. <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 *
-	 * @return the value of the '<em>Last Modification Timestamp</em>' attribute.
-	 * @see #setLastModificationTimestamp(long)
-	 * @see org.eclipse.fordiac.ide.model.Palette.PalettePackage#getPaletteEntry_LastModificationTimestamp()
-	 * @model unique="false" dataType="org.eclipse.emf.ecore.xml.type.Long" transient="true" ordered="false"
-	 * @generated */
 	long getLastModificationTimestamp();
 
-	/** Sets the value of the '{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getLastModificationTimestamp
-	 * <em>Last Modification Timestamp</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @param value the new value of the '<em>Last Modification Timestamp</em>' attribute.
-	 * @see #getLastModificationTimestamp()
-	 * @generated */
 	void setLastModificationTimestamp(long value);
 
-	/** Returns the value of the '<em><b>Type</b></em>' reference. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @return the value of the '<em>Type</em>' reference.
-	 * @see #setType(LibraryElement)
-	 * @see org.eclipse.fordiac.ide.model.Palette.PalettePackage#getPaletteEntry_Type()
-	 * @model resolveProxies="false" required="true"
-	 * @generated */
 	LibraryElement getType();
 
-	/** Sets the value of the '{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getType <em>Type</em>}'
-	 * reference. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @param value the new value of the '<em>Type</em>' reference.
-	 * @see #getType()
-	 * @generated */
 	void setType(LibraryElement value);
 
-
-	/** Returns the value of the '<em><b>Type Editable</b></em>' reference. It is bidirectional and its opposite is
-	 * '{@link org.eclipse.fordiac.ide.model.libraryElement.LibraryElement#getPaletteEntry <em>Palette Entry</em>}'.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @return the value of the '<em>Type Editable</em>' reference.
-	 * @see #setTypeEditable(LibraryElement)
-	 * @see org.eclipse.fordiac.ide.model.Palette.PalettePackage#getPaletteEntry_TypeEditable()
-	 * @see org.eclipse.fordiac.ide.model.libraryElement.LibraryElement#getPaletteEntry
-	 * @model opposite="paletteEntry" resolveProxies="false" required="true"
-	 * @generated */
 	LibraryElement getTypeEditable();
 
-	/** Sets the value of the '{@link org.eclipse.fordiac.ide.model.Palette.PaletteEntry#getTypeEditable <em>Type
-	 * Editable</em>}' reference. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @param value the new value of the '<em>Type Editable</em>' reference.
-	 * @see #getTypeEditable()
-	 * @generated */
 	void setTypeEditable(LibraryElement value);
 
-	/** <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @model kind="operation" dataType="org.eclipse.emf.ecore.xml.type.String" required="true"
-	 * @generated */
-	String getProjectRelativeTypePath();
-
-	/** <!-- begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @model kind="operation" dataType="org.eclipse.fordiac.ide.model.Palette.TypeLibrary"
-	 * @generated */
 	TypeLibrary getTypeLibrary();
 
 	void setTypeLibrary(TypeLibrary typeLib);
 
-} // PaletteEntry
+}
--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/impl/AbstractTypeEntryImpl.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/impl/AbstractTypeEntryImpl.java
@@ -18,16 +18,8 @@ package org.eclipse.fordiac.ide.model.typelibrary.impl;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.InternalEObject;
-import org.eclipse.emf.ecore.impl.ENotificationImpl;
-import org.eclipse.emf.ecore.impl.EObjectImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.fordiac.ide.model.Palette.Palette;
-import org.eclipse.fordiac.ide.model.Palette.PalettePackage;
 import org.eclipse.fordiac.ide.model.dataimport.CommonElementImporter;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
 import org.eclipse.fordiac.ide.model.resource.FordiacTypeResource;
@@ -35,230 +27,45 @@ import org.eclipse.fordiac.ide.model.typelibrary.TypeEntry;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
 import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
 
-/**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Entry</b></em>'.
- * <!-- end-user-doc -->
- * <p>
- * The following features are implemented:
- * </p>
- * <ul>
- *   <li>{@link org.eclipse.fordiac.ide.model.Palette.impl.PaletteEntryImpl#getLabel <em>Label</em>}</li>
- *   <li>{@link org.eclipse.fordiac.ide.model.Palette.impl.PaletteEntryImpl#getFile <em>File</em>}</li>
- *   <li>{@link org.eclipse.fordiac.ide.model.Palette.impl.PaletteEntryImpl#getLastModificationTimestamp <em>Last Modification Timestamp</em>}</li>
- *   <li>{@link org.eclipse.fordiac.ide.model.Palette.impl.PaletteEntryImpl#getType <em>Type</em>}</li>
- *   <li>{@link org.eclipse.fordiac.ide.model.Palette.impl.PaletteEntryImpl#getPalette <em>Palette</em>}</li>
- *   <li>{@link org.eclipse.fordiac.ide.model.Palette.impl.PaletteEntryImpl#getTypeEditable <em>Type Editable</em>}</li>
- * </ul>
- *
- * @generated
- */
-public abstract class AbstractTypeEntryImpl extends EObjectImpl implements TypeEntry {
-	/**
-	 * The default value of the '{@link #getLabel() <em>Label</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getLabel()
-	 * @generated
-	 * @ordered
-	 */
-	protected static final String LABEL_EDEFAULT = null;
+public abstract class AbstractTypeEntryImpl implements TypeEntry {
 
-	/**
-	 * The cached value of the '{@link #getLabel() <em>Label</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getLabel()
-	 * @generated
-	 * @ordered
-	 */
-	protected String label = LABEL_EDEFAULT;
+	protected IFile file;
 
-	/**
-	 * The default value of the '{@link #getFile() <em>File</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getFile()
-	 * @generated
-	 * @ordered
-	 */
-	protected static final IFile FILE_EDEFAULT = null;
+	protected long lastModificationTimestamp = IResource.NULL_STAMP;
 
-	/**
-	 * The cached value of the '{@link #getFile() <em>File</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getFile()
-	 * @generated
-	 * @ordered
-	 */
-	protected IFile file = FILE_EDEFAULT;
-
-	/**
-	 * The default value of the '{@link #getLastModificationTimestamp() <em>Last Modification Timestamp</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getLastModificationTimestamp()
-	 * @generated
-	 * @ordered
-	 */
-	protected static final long LAST_MODIFICATION_TIMESTAMP_EDEFAULT = 0L;
-
-	/**
-	 * The cached value of the '{@link #getLastModificationTimestamp() <em>Last Modification Timestamp</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getLastModificationTimestamp()
-	 * @generated
-	 * @ordered
-	 */
-	protected long lastModificationTimestamp = LAST_MODIFICATION_TIMESTAMP_EDEFAULT;
-
-	/**
-	 * The cached value of the '{@link #getType() <em>Type</em>}' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getType()
-	 * @generated
-	 * @ordered
-	 */
 	protected LibraryElement type;
-
-	/**
-	 * The cached value of the '{@link #getPalette() <em>Palette</em>}' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getPalette()
-	 * @generated
-	 * @ordered
-	 */
-	protected Palette palette;
-
-	/**
-	 * The cached value of the '{@link #getTypeEditable() <em>Type Editable</em>}' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #getTypeEditable()
-	 * @generated
-	 * @ordered
-	 */
 	protected LibraryElement typeEditable;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected AbstractTypeEntryImpl() {
-		super();
-	}
+	protected TypeLibrary typeLibray;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
-	protected EClass eStaticClass() {
-		return PalettePackage.Literals.PALETTE_ENTRY;
+	public String getTypeName() {
+		return (file != null) ? TypeLibrary.getTypeNameFromFile(getFile()) : ""; //$NON-NLS-1$
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public String getLabel() {
-		return label;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public void setLabel(final String newLabel) {
-		final String oldLabel = label;
-		label = newLabel;
-		if (eNotificationRequired()) {
-			eNotify(new ENotificationImpl(this, Notification.SET, PalettePackage.PALETTE_ENTRY__LABEL, oldLabel, label));
-		}
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public IFile getFile() {
 		return file;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public void setFile(final IFile newFile) {
-		final IFile oldFile = file;
 		file = newFile;
-		if (eNotificationRequired()) {
-			eNotify(new ENotificationImpl(this, Notification.SET, PalettePackage.PALETTE_ENTRY__FILE, oldFile, file));
-		}
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public long getLastModificationTimestamp() {
 		return lastModificationTimestamp;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public void setLastModificationTimestamp(final long newLastModificationTimestamp) {
-		final long oldLastModificationTimestamp = lastModificationTimestamp;
 		lastModificationTimestamp = newLastModificationTimestamp;
-		if (eNotificationRequired()) {
-			eNotify(new ENotificationImpl(this, Notification.SET, PalettePackage.PALETTE_ENTRY__LAST_MODIFICATION_TIMESTAMP, oldLastModificationTimestamp, lastModificationTimestamp));
-		}
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated not
-	 */
 	@Override
 	public LibraryElement getType() {
-		if (type != null && type.eIsProxy()) {
-			final InternalEObject oldType = (InternalEObject)type;
-			type = (LibraryElement)eResolveProxy(oldType);
-			if (type != oldType) {
-				final InternalEObject newType = (InternalEObject)type;
-				NotificationChain msgs = oldType.eInverseRemove(this, EOPPOSITE_FEATURE_BASE - PalettePackage.PALETTE_ENTRY__TYPE, null, null);
-				if (newType.eInternalContainer() == null) {
-					msgs = newType.eInverseAdd(this, EOPPOSITE_FEATURE_BASE - PalettePackage.PALETTE_ENTRY__TYPE, null, msgs);
-				}
-				if (msgs != null) {
-					msgs.dispatch();
-				}
-				if (eNotificationRequired()) {
-					eNotify(new ENotificationImpl(this, Notification.RESOLVE, PalettePackage.PALETTE_ENTRY__TYPE, oldType, type));
-				}
-			}
-		} else if (getFile() != null) {
+		if (getFile() != null) {
 			if (type == null) {
 				reloadType();
 			} else if (isFileContentChanged()) {
@@ -280,18 +87,9 @@ public abstract class AbstractTypeEntryImpl extends EObjectImpl implements TypeE
 				&& getFile().getModificationStamp() != lastModificationTimestamp;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public LibraryElement basicGetType() {
-		return type;
-	}
-
 	@Override
 	public void setType(final LibraryElement newType) {
-		setTypeGen(newType);
+		type = newType;
 		if (newType != null) {
 			encloseInResource(newType);
 			// FIXME when the model is update to the new typeentry system
@@ -310,61 +108,18 @@ public abstract class AbstractTypeEntryImpl extends EObjectImpl implements TypeE
 		}
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public void setTypeGen(final LibraryElement newType) {
-		final LibraryElement oldType = type;
-		type = newType;
-		if (eNotificationRequired()) {
-			eNotify(new ENotificationImpl(this, Notification.SET, PalettePackage.PALETTE_ENTRY__TYPE, oldType, type));
-		}
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated not
-	 */
 	@Override
 	public LibraryElement getTypeEditable() {
-		if (typeEditable != null && typeEditable.eIsProxy()) {
-			final InternalEObject oldTypeEditable = (InternalEObject)typeEditable;
-			typeEditable = (LibraryElement)eResolveProxy(oldTypeEditable);
-			if (typeEditable != oldTypeEditable) {
-				final InternalEObject newTypeEditable = (InternalEObject)typeEditable;
-				NotificationChain msgs = oldTypeEditable.eInverseRemove(this, EOPPOSITE_FEATURE_BASE - PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE, null, null);
-				if (newTypeEditable.eInternalContainer() == null) {
-					msgs = newTypeEditable.eInverseAdd(this, EOPPOSITE_FEATURE_BASE - PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE, null, msgs);
-				}
-				if (msgs != null) {
-					msgs.dispatch();
-				}
-				if (eNotificationRequired()) {
-					eNotify(new ENotificationImpl(this, Notification.RESOLVE, PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE, oldTypeEditable, typeEditable));
-				}
-			}
-		} else if ((getFile() != null) && (typeEditable == null || isFileContentChanged())) {
+		if ((getFile() != null) && (typeEditable == null || isFileContentChanged())) {
 			// if the editable type is null load it from the file and set a copy
 			setTypeEditable(EcoreUtil.copy(getType()));
 		}
 		return typeEditable;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public LibraryElement basicGetTypeEditable() {
-		return typeEditable;
-	}
-
 	@Override
 	public void setTypeEditable(final LibraryElement newTypeEditable) {
-		setTypeEditableGen(newTypeEditable);
+		typeEditable = newTypeEditable;
 		if (newTypeEditable != null) {
 			encloseInResource(newTypeEditable);
 			// FIXME when the model is update to the new typeentry system
@@ -372,34 +127,6 @@ public abstract class AbstractTypeEntryImpl extends EObjectImpl implements TypeE
 		}
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public void setTypeEditableGen(final LibraryElement newTypeEditable) {
-		final LibraryElement oldTypeEditable = typeEditable;
-		typeEditable = newTypeEditable;
-		if (eNotificationRequired()) {
-			eNotify(new ENotificationImpl(this, Notification.SET, PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE, oldTypeEditable, typeEditable));
-		}
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public String getProjectRelativeTypePath() {
-		return getFile().getProjectRelativePath().toOSString();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	private LibraryElement loadType() {
 		final CommonElementImporter importer = getImporter();
 		importer.loadElement();
@@ -414,155 +141,23 @@ public abstract class AbstractTypeEntryImpl extends EObjectImpl implements TypeE
 		return retval;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	protected abstract CommonElementImporter getImporter();
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public TypeLibrary getTypeLibrary() {
-		// TODO implement
-		return null;
+		return typeLibray;
 	}
 
 	@Override
 	public void setTypeLibrary(final TypeLibrary typeLib) {
-		// TODO implement
+		typeLibray = typeLib;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public Object eGet(final int featureID, final boolean resolve, final boolean coreType) {
-		switch (featureID) {
-		case PalettePackage.PALETTE_ENTRY__LABEL:
-			return getLabel();
-		case PalettePackage.PALETTE_ENTRY__FILE:
-			return getFile();
-		case PalettePackage.PALETTE_ENTRY__LAST_MODIFICATION_TIMESTAMP:
-			return getLastModificationTimestamp();
-		case PalettePackage.PALETTE_ENTRY__TYPE:
-			if (resolve) {
-				return getType();
-			}
-			return basicGetType();
-		case PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE:
-			if (resolve) {
-				return getTypeEditable();
-			}
-			return basicGetTypeEditable();
-		default:
-			return super.eGet(featureID, resolve, coreType);
-		}
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public void eSet(final int featureID, final Object newValue) {
-		switch (featureID) {
-		case PalettePackage.PALETTE_ENTRY__LABEL:
-			setLabel((String)newValue);
-			return;
-		case PalettePackage.PALETTE_ENTRY__FILE:
-			setFile((IFile)newValue);
-			return;
-		case PalettePackage.PALETTE_ENTRY__LAST_MODIFICATION_TIMESTAMP:
-			setLastModificationTimestamp((Long)newValue);
-			return;
-		case PalettePackage.PALETTE_ENTRY__TYPE:
-			setType((LibraryElement)newValue);
-			return;
-		case PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE:
-			setTypeEditable((LibraryElement)newValue);
-			return;
-		default:
-			super.eSet(featureID, newValue);
-			return;
-		}
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public void eUnset(final int featureID) {
-		switch (featureID) {
-		case PalettePackage.PALETTE_ENTRY__LABEL:
-			setLabel(LABEL_EDEFAULT);
-			return;
-		case PalettePackage.PALETTE_ENTRY__FILE:
-			setFile(FILE_EDEFAULT);
-			return;
-		case PalettePackage.PALETTE_ENTRY__LAST_MODIFICATION_TIMESTAMP:
-			setLastModificationTimestamp(LAST_MODIFICATION_TIMESTAMP_EDEFAULT);
-			return;
-		case PalettePackage.PALETTE_ENTRY__TYPE:
-			setType((LibraryElement)null);
-			return;
-		case PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE:
-			setTypeEditable((LibraryElement)null);
-			return;
-		default:
-			super.eUnset(featureID);
-			return;
-		}
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public boolean eIsSet(final int featureID) {
-		switch (featureID) {
-		case PalettePackage.PALETTE_ENTRY__LABEL:
-			return LABEL_EDEFAULT == null ? label != null : !LABEL_EDEFAULT.equals(label);
-		case PalettePackage.PALETTE_ENTRY__FILE:
-			return FILE_EDEFAULT == null ? file != null : !FILE_EDEFAULT.equals(file);
-		case PalettePackage.PALETTE_ENTRY__LAST_MODIFICATION_TIMESTAMP:
-			return lastModificationTimestamp != LAST_MODIFICATION_TIMESTAMP_EDEFAULT;
-		case PalettePackage.PALETTE_ENTRY__TYPE:
-			return type != null;
-		case PalettePackage.PALETTE_ENTRY__PALETTE:
-			return palette != null;
-		case PalettePackage.PALETTE_ENTRY__TYPE_EDITABLE:
-			return typeEditable != null;
-		default:
-			return super.eIsSet(featureID);
-		}
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public String toString() {
-		if (eIsProxy()) {
-			return super.toString();
-		}
-
 		final StringBuilder result = new StringBuilder(super.toString());
 		result.append(" (label: "); //$NON-NLS-1$
-		result.append(label);
+		result.append(getTypeName());
 		result.append(", file: "); //$NON-NLS-1$
 		result.append(file);
 		result.append(", lastModificationTimestamp: "); //$NON-NLS-1$
@@ -571,4 +166,4 @@ public abstract class AbstractTypeEntryImpl extends EObjectImpl implements TypeE
 		return result.toString();
 	}
 
-} //PaletteEntryImpl
+}
