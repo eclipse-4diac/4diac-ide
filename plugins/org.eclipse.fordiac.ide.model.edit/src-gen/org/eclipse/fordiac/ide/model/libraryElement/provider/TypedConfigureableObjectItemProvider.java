007c6c8725804a243df2598ac943512ea448438a
Alois Zoitl <alois.zoitl@gmx.at>
2022-04-10 21:00:36 +0200

    Bug 579648 - Updated models edit plugin to new TypeEntry
    Change-Id: I8897712e1b6ffaad5b561025e98124d3d82df718

--- plugins/org.eclipse.fordiac.ide.model.edit/src-gen/org/eclipse/fordiac/ide/model/libraryElement/provider/TypedConfigureableObjectItemProvider.java
+++ plugins/org.eclipse.fordiac.ide.model.edit/src-gen/org/eclipse/fordiac/ide/model/libraryElement/provider/TypedConfigureableObjectItemProvider.java
@@ -1,11 +1,12 @@
 /**
  * *******************************************************************************
- *  Copyright (c) 2008 - 2017 Profactor GmbH, TU Wien ACIN, fortiss GmbH
+ * Copyright (c) 2008 - 2018 Profactor GmbH, TU Wien ACIN, fortiss GmbH
+ *               2022 Martin Erich Jobst
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- *
+ * 
  * SPDX-License-Identifier: EPL-2.0
  * 
  * Contributors:
@@ -15,88 +16,130 @@
  */
 package org.eclipse.fordiac.ide.model.libraryElement.provider;
 
+
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementPackage;
 import org.eclipse.fordiac.ide.model.libraryElement.TypedConfigureableObject;
 
-/** This is the item provider adapter for a
- * {@link org.eclipse.fordiac.ide.model.libraryElement.TypedConfigureableObject} object. <!-- begin-user-doc --> <!--
- * end-user-doc -->
- * 
- * @generated */
+/**
+ * This is the item provider adapter for a {@link org.eclipse.fordiac.ide.model.libraryElement.TypedConfigureableObject} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
 public class TypedConfigureableObjectItemProvider extends ConfigurableObjectItemProvider {
-	/** This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated */
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	public TypedConfigureableObjectItemProvider(AdapterFactory adapterFactory) {
 		super(adapterFactory);
 	}
 
-	/** This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated */
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	@Override
 	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
-			addPaletteEntryPropertyDescriptor(object);
+			addTypeEntryPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
 
-	/** This adds a property descriptor for the Palette Entry feature. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated */
-	protected void addPaletteEntryPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add(createItemPropertyDescriptor(
-				((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(), getResourceLocator(),
-				getString("_UI_TypedConfigureableObject_paletteEntry_feature"), //$NON-NLS-1$
-				getString("_UI_PropertyDescriptor_description", "_UI_TypedConfigureableObject_paletteEntry_feature", //$NON-NLS-1$ //$NON-NLS-2$
-						"_UI_TypedConfigureableObject_type"),  //$NON-NLS-1$
-				LibraryElementPackage.Literals.TYPED_CONFIGUREABLE_OBJECT__PALETTE_ENTRY, true, false, true, null, null,
-				null));
+	/**
+	 * This adds a property descriptor for the Type Entry feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addTypeEntryPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_TypedConfigureableObject_typeEntry_feature"), //$NON-NLS-1$
+				 getString("_UI_PropertyDescriptor_description", "_UI_TypedConfigureableObject_typeEntry_feature", "_UI_TypedConfigureableObject_type"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+				 LibraryElementPackage.Literals.TYPED_CONFIGUREABLE_OBJECT__TYPE_ENTRY,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
 	}
 
-	/** This returns TypedConfigureableObject.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated */
+	/**
+	 * This returns TypedConfigureableObject.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	@Override
 	public Object getImage(Object object) {
 		return overlayImage(object, getResourceLocator().getImage("full/obj16/TypedConfigureableObject")); //$NON-NLS-1$
 	}
 
-	/** This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated */
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	@Override
 	public String getText(Object object) {
-		String label = ((TypedConfigureableObject) object).getName();
-		return label == null || label.length() == 0 ? getString("_UI_TypedConfigureableObject_type") : //$NON-NLS-1$
-				getString("_UI_TypedConfigureableObject_type") + " " + label; //$NON-NLS-1$ //$NON-NLS-2$
+		String label = ((TypedConfigureableObject)object).getName();
+		return label == null || label.length() == 0 ?
+			getString("_UI_TypedConfigureableObject_type") : //$NON-NLS-1$
+			getString("_UI_TypedConfigureableObject_type") + " " + label; //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
-	/** This handles model notifications by calling {@link #updateChildren} to update any cached children and by
-	 * creating a viewer notification, which it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 * 
-	 * @generated */
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
-		super.notifyChanged(notification);
+
+		switch (notification.getFeatureID(TypedConfigureableObject.class)) {
+			case LibraryElementPackage.TYPED_CONFIGUREABLE_OBJECT__TYPE_ENTRY:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+			default:
+				super.notifyChanged(notification);
+				return;
+			}
 	}
 
-	/** This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that can be created
-	 * under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated */
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
