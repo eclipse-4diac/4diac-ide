b797c2bf64fad4511d2a10f0b218f149bd29947f
Martin Melik Merkumians <melik-merkumians@acin.tuwien.ac.at>
2022-04-29 12:12:09 +0200

    Bug 192793 - Add some bit, conversion and math funcs
    Change-Id: I921ca601b71656bcdef6e61f325780627b5ee4d7
    Signed-off-by: Martin Melik Merkumians <melik-merkumians@acin.tuwien.ac.at>
    Also-by: Martin Jobst <martin.jobst@posteo.de>

--- plugins/org.eclipse.fordiac.ide.model.eval/src/org/eclipse/fordiac/ide/model/eval/function/StandardFunctions.java
+++ plugins/org.eclipse.fordiac.ide.model.eval/src/org/eclipse/fordiac/ide/model/eval/function/StandardFunctions.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2022 Martin Erich Jobst
+ * 				 2022 Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,6 +10,7 @@
  *
  * Contributors:
  *   Martin Jobst - initial API and implementation and/or initial documentation
+ *   Martin Melik Merkumians - added some functions
  *******************************************************************************/
 package org.eclipse.fordiac.ide.model.eval.function;
 
@@ -16,16 +18,31 @@ import java.util.stream.Stream;
 
 import org.eclipse.fordiac.ide.model.eval.value.AnyBitValue;
 import org.eclipse.fordiac.ide.model.eval.value.AnyCharsValue;
+import org.eclipse.fordiac.ide.model.eval.value.AnyDurationValue;
 import org.eclipse.fordiac.ide.model.eval.value.AnyElementaryValue;
+import org.eclipse.fordiac.ide.model.eval.value.AnyIntValue;
 import org.eclipse.fordiac.ide.model.eval.value.AnyMagnitudeValue;
 import org.eclipse.fordiac.ide.model.eval.value.AnyNumValue;
+import org.eclipse.fordiac.ide.model.eval.value.AnyRealValue;
 import org.eclipse.fordiac.ide.model.eval.value.AnyValue;
 import org.eclipse.fordiac.ide.model.eval.value.BoolValue;
+import org.eclipse.fordiac.ide.model.eval.value.ByteValue;
+import org.eclipse.fordiac.ide.model.eval.value.DIntValue;
+import org.eclipse.fordiac.ide.model.eval.value.DWordValue;
+import org.eclipse.fordiac.ide.model.eval.value.IntValue;
+import org.eclipse.fordiac.ide.model.eval.value.LIntValue;
+import org.eclipse.fordiac.ide.model.eval.value.LRealValue;
+import org.eclipse.fordiac.ide.model.eval.value.RealValue;
+import org.eclipse.fordiac.ide.model.eval.value.TimeValue;
 import org.eclipse.fordiac.ide.model.eval.value.ULIntValue;
+import org.eclipse.fordiac.ide.model.eval.value.USIntValue;
 import org.eclipse.fordiac.ide.model.eval.value.ValueOperations;
+import org.eclipse.fordiac.ide.model.eval.value.WordValue;
 
+@SuppressWarnings("squid:S100") // ST Name conventions must be used here
 public interface StandardFunctions extends Functions {
 
+	// Math functions
 	@SuppressWarnings("unchecked")
 	static <T extends AnyMagnitudeValue> T ADD(final T... values) {
 		return Stream.of(values).reduce((a, b) -> (T) ValueOperations.add(a, b)).orElseThrow();
@@ -56,11 +73,37 @@ public interface StandardFunctions extends Functions {
 		return (T) ValueOperations.power(first, second);
 	}
 
-	static <T extends AnyValue> T MOVE(final T first) {
-		return first;
+	@SuppressWarnings("unchecked")
+	static <T extends AnyNumValue> T ABS(final T value) {
+		return (T) ValueOperations.abs(value);
+	}
+
+	// Selection Functions
+	static <T extends AnyValue> T MOVE(final T value) {
+		return value;
+	}
+
+	static <T extends AnyValue> T SEL(final BoolValue g, final T in0, final T in1) {
+		return g.boolValue() ? in1 : in0;
 	}
 
 	// TODO: shift and rotation
+
+	static <T extends AnyBitValue, U extends AnyIntValue> T SHR(final T value, final U moveby) {
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	static <T extends AnyBitValue, U extends AnyIntValue> T SHL(final T value, final U moveby) {
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	static <T extends AnyBitValue, U extends AnyIntValue> T ROR(final T value, final U moveby) {
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	static <T extends AnyBitValue, U extends AnyIntValue> T ROL(final T value, final U moveby) {
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
 
 	@SuppressWarnings("unchecked")
 	static <T extends AnyValue> T AND(final T... values) {
@@ -152,9 +195,106 @@ public interface StandardFunctions extends Functions {
 		return ULIntValue.toULIntValue(string.length());
 	}
 
+	static <T extends AnyRealValue> T SQRT(final T value) {
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	// TODO: conversion functions
+	static RealValue LREAL_TO_REAL(final LRealValue value) {
+		return RealValue.toRealValue(value);
+	}
+
+	static DIntValue REAL_TO_DINT(final RealValue value) {
+		return DIntValue.toDIntValue(value);
+	}
+
+	static IntValue REAL_TO_INT(final RealValue value) {
+		return IntValue.toIntValue(value);
+	}
+
+	static DWordValue REAL_TO_DWORD(final RealValue value) {
+		return DWordValue.toDWordValue(Float.floatToRawIntBits(value.floatValue()));
+	}
+
+	static DIntValue LINT_TO_DINT(final LIntValue value) {
+		return DIntValue.toDIntValue(value);
+	}
+
+	static RealValue DINT_TO_REAL(final DIntValue value) {
+		return RealValue.toRealValue(value);
+	}
+
+	static IntValue DINT_TO_INT(final DIntValue value) {
+		return IntValue.toIntValue(value);
+	}
+
+	static DWordValue DINT_TO_DWORD(final DIntValue value) {
+		return DWordValue.toDWordValue(value.intValue());
+	}
+
+	static RealValue INT_TO_REAL(final IntValue value) {
+		return RealValue.toRealValue(value);
+	}
+
+	static DIntValue INT_TO_DINT(final IntValue value) {
+		return DIntValue.toDIntValue(value);
+	}
+
+	static USIntValue INT_TO_USINT(final IntValue value) {
+		return USIntValue.toUSIntValue(value);
+	}
+
+	static DWordValue INT_TO_DWORD(final IntValue value) {
+		return DWordValue.toDWordValue(value.intValue());
+	}
+
+	static WordValue INT_TO_WORD(final IntValue value) {
+		return WordValue.toWordValue(value.shortValue());
+	}
+
+	static ByteValue INT_TO_BYTE(final IntValue value) {
+		return ByteValue.toByteValue(value.byteValue());
+	}
+
+	static DIntValue DWORD_TO_DINT(final DWordValue value) {
+		return DIntValue.toDIntValue(value.intValue());
+	}
+
+	static IntValue DWORD_TO_INT(final DWordValue value) {
+		return IntValue.toIntValue(value.shortValue());
+	}
+
+	static IntValue WORD_TO_INT(final WordValue value) {
+		return IntValue.toIntValue(value.shortValue());
+	}
+
+	static IntValue BYTE_TO_INT(final ByteValue value) {
+		return IntValue.toIntValue(value.shortValue());
+	}
+
+	static IntValue TRUNC(final RealValue value) {
+		return IntValue.toIntValue(value);
+	}
+
+	static TimeValue NOW_MONOTONIC() {
+		return TimeValue.toTimeValue(System.nanoTime());
+	}
+
+	static LIntValue TIME_IN_MS_TO_LINT(final AnyDurationValue value) {
+		return LIntValue.toLIntValue(value.toDuration().toMillis());
+	}
+
+	static LRealValue TIME_IN_MS_TO_LREAL(final AnyDurationValue value) {
+		return LRealValue.toLRealValue(value.toDuration().toNanos() / 1000000.0);
+	}
+
+	static LRealValue TIME_IN_US_TO_LREAL(final AnyDurationValue value) {
+		return LRealValue.toLRealValue(value.toDuration().toNanos() / 1000.0);
+	}
+
 	// TODO: rest of string functions
 
 	// TODO: date and duration functions
 
-	// TODO: conversion functions
+
 }
