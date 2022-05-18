30ce4cc8e1f2ff5ab9a4694997775e6bb533996c
Fabio Gandolfi <fabio.gandolfi@jku.at>
2022-05-18 13:20:05 +0200

    [579885] Grey inherited comments & values in comments property
    Added grey comments and initial values to comments property tab
    
    Change-Id: Ie4b4756ae61d4c4d8d91f8cef8cc5bccda861323
    Signed-off-by: Fabio Gandolfi <fabio.gandolfi@jku.at>

--- plugins/org.eclipse.fordiac.ide.model.edit/src/org/eclipse/fordiac/ide/model/edit/providers/CommentLabelProvider.java
+++ plugins/org.eclipse.fordiac.ide.model.edit/src/org/eclipse/fordiac/ide/model/edit/providers/CommentLabelProvider.java
@@ -8,20 +8,56 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *   Dunja Životin - initial API and implementation and/or initial documentation
+ *   Dunja Životin & Fabio Gandolfi
+ *     - initial API and implementation and/or initial documentation
  *******************************************************************************/
 package org.eclipse.fordiac.ide.model.edit.providers;
 
-import org.eclipse.fordiac.ide.model.libraryElement.INamedElement;
+import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
+import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
+import org.eclipse.fordiac.ide.model.libraryElement.InterfaceList;
+import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
 
 public class CommentLabelProvider extends ColumnLabelProvider {
 	@Override
 	public String getText(final Object element) {
-		if (element instanceof INamedElement) {
-			return ((INamedElement) element).getComment();
+		if (element instanceof VarDeclaration) {
+			final VarDeclaration varDec = (VarDeclaration) element;
+			if (hasComment(element)) {
+				return (varDec).getComment();
+			}
+			return getTypeComment(varDec);
 		}
 		return super.getText(element);
 	}
 
+	@Override
+	public Color getForeground(final Object element) {
+		if (hasComment(element)) {
+			return super.getForeground(element);
+		}
+		return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GRAY);
+	}
+
+	private static boolean hasComment(final Object element) {
+		return (((VarDeclaration) element).getComment() != null && !((VarDeclaration) element).getComment().isBlank());
+	}
+
+	private static String getTypeComment(final VarDeclaration varDec) {
+		if (varDec.eContainer() instanceof InterfaceList) {
+			final FBNetworkElement fbn = (FBNetworkElement) varDec.eContainer().eContainer();
+			if (fbn != null && fbn.getType() != null) {
+				final IInterfaceElement interfaceElement = fbn.getType().getInterfaceList()
+						.getInterfaceElement(varDec.getName());
+				if (interfaceElement != null) {
+					return interfaceElement.getComment() != null ? interfaceElement.getComment() : ""; //$NON-NLS-1$
+				}
+			}
+		}
+		return "";
+	}
 }
