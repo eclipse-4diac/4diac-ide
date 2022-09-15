00dd7147803566db220f9758da6a3a851772b365
Sebastian Hollersbacher <sebastian.hollersbacher@jku.at>
2022-09-15 13:01:36 +0200

    Bug 580597 - Reworked paste for NatTable
    Paste into NatTable now only works for a single range to remove unwanted
    behaviour
    
    Change-Id: I4fd9db739f5fc8f5ea97897258659a0d924eb3b1

--- plugins/org.eclipse.fordiac.ide.ui/src/org/eclipse/fordiac/ide/ui/widget/PasteDataIntoTableAction.java
+++ plugins/org.eclipse.fordiac.ide.ui/src/org/eclipse/fordiac/ide/ui/widget/PasteDataIntoTableAction.java
@@ -2,7 +2,10 @@ package org.eclipse.fordiac.ide.ui.widget;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.eclipse.fordiac.ide.ui.FordiacMessages;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.edit.command.UpdateDataCommand;
 import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
@@ -11,6 +14,7 @@ import org.eclipse.nebula.widgets.nattable.ui.action.IKeyAction;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 
 public class PasteDataIntoTableAction implements IKeyAction {
@@ -20,35 +24,44 @@ public class PasteDataIntoTableAction implements IKeyAction {
 		final Clipboard clipboard = new Clipboard(Display.getCurrent());
 		final Object clipboardContents = clipboard.getContents(TextTransfer.getInstance());
 		clipboard.dispose();
-		final String[] content = parseContent(clipboardContents);
+		final String[][] content = parseContent(clipboardContents);
 
 		final SelectionLayer selectionLayer = (SelectionLayer) natTable.getLayer().getUnderlyingLayerByPosition(0, 1)
 				.getUnderlyingLayerByPosition(0, 0);
+		final List<Rectangle> ranges = selectionLayer.getSelectionModel().getSelections().stream().distinct()
+				.collect(Collectors.toList());
 
-		int i = 0;
-		for (final ILayerCell c : selectionLayer.getSelectedCells()) {
-			if (i < content.length) {
-				selectionLayer.doCommand(
-						new UpdateDataCommand(selectionLayer, c.getColumnIndex(), c.getRowIndex(), content[i++]));
+		if (ranges.size() != 1) {
+			MessageDialog.openInformation(Display.getCurrent().getActiveShell(), "", //$NON-NLS-1$
+					FordiacMessages.NatTable_TEXT_Paste);
+			return;
+		}
+
+		final int startingRow = ranges.get(0).y;
+		final int startingColumn = ranges.get(0).x;
+		for (final ILayerCell cell : selectionLayer.getSelectedCells()) {
+			final int arrayRowIndex = cell.getRowIndex() - startingRow;
+			final int arrayColumnIndex = cell.getColumnIndex() - startingColumn;
+
+			if (arrayRowIndex < content.length && arrayColumnIndex < content[0].length) {
+				selectionLayer.doCommand(new UpdateDataCommand(selectionLayer, cell.getColumnIndex(),
+						cell.getRowIndex(), content[arrayRowIndex][arrayColumnIndex]));
 			}
 		}
 	}
 
-	private static String[] parseContent(final Object contents) {
-		final List<String[]> content = new ArrayList<>();
+	private static String[][] parseContent(final Object contents) {
+		final List<String[]> lines = new ArrayList<>();
 		((String) contents).lines().forEach(s -> {
 			final String[] st = s.split("\t", -1); //$NON-NLS-1$
-			content.add(st);
+			lines.add(st);
 		});
 
-		final List<String> c = new ArrayList<>();
-		for (int i = 0; i < content.get(0).length; i++) {
-			for (final String[] str : content) {
-				// if (!str[i].isEmpty()) {
-				c.add(str[i]);
-				// }
-			}
+		final String[][] content = new String[lines.size()][];
+		for (int i = 0; i < lines.size(); i++) {
+			content[i] = lines.get(i);
 		}
-		return c.toArray(new String[0]);
+
+		return content;
 	}
 }
