8268e54b0df492d79572480e9df28c4dbddb1bee
Alois Zoitl <alois.zoitl@gmx.at>
2022-09-25 15:10:30 +0200

    Bug 580513 - Added debug context listener
    The debug view gets is update now via the debug context listener.
    
    Change-Id: I1bec333d31059349d8ce9fa92e10b86752bbd1e4

--- plugins/org.eclipse.fordiac.ide.debug.ui/src/org/eclipse/fordiac/ide/debug/ui/view/FBDebugView.java
+++ plugins/org.eclipse.fordiac.ide.debug.ui/src/org/eclipse/fordiac/ide/debug/ui/view/FBDebugView.java
@@ -12,27 +12,20 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.debug.ui.view;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.debug.ui.contexts.DebugContextEvent;
+import org.eclipse.debug.ui.contexts.IDebugContextListener;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.ScalableFreeformLayeredPane;
-import org.eclipse.fordiac.ide.debug.EvaluatorDebugStackFrame;
+import org.eclipse.fordiac.ide.debug.EvaluatorDebugElement;
 import org.eclipse.fordiac.ide.debug.EvaluatorDebugTarget;
-import org.eclipse.fordiac.ide.debug.EvaluatorDebugThread;
 import org.eclipse.fordiac.ide.fbtypeeditor.editparts.FBInterfaceEditPartFactory;
 import org.eclipse.fordiac.ide.gef.FordiacContextMenuProvider;
-import org.eclipse.fordiac.ide.model.libraryElement.AutomationSystem;
-import org.eclipse.fordiac.ide.model.libraryElement.BasicFBType;
-import org.eclipse.fordiac.ide.model.libraryElement.FBNetwork;
 import org.eclipse.fordiac.ide.model.libraryElement.FBType;
-import org.eclipse.fordiac.ide.model.typelibrary.TypeEntry;
-import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
-import org.eclipse.fordiac.ide.systemmanagement.SystemManager;
-import org.eclipse.fordiac.ide.test.fb.interpreter.infra.AbstractInterpreterTest;
 import org.eclipse.fordiac.ide.ui.imageprovider.FordiacImage;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.KeyHandler;
 import org.eclipse.gef.KeyStroke;
@@ -46,25 +39,18 @@ import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.layout.LayoutConstants;
-import org.eclipse.jface.viewers.TreePath;
-import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.ISelectionListener;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.part.ViewPart;
 
-public class FBDebugView extends ViewPart {
+public class FBDebugView extends ViewPart implements IDebugContextListener {
 
 	private GraphicalViewer viewer;
 	private ActionRegistry actionRegistry;
 	private static final int NUM_COLUMNS = 1;
-	private static final String DEBUG_VIEW = "Debug";
-	private FBType fbType;
-	private ISelectionListener selectionListener;
 	private KeyHandler sharedKeyHandler;
 
 	@Override
@@ -77,7 +63,6 @@ public class FBDebugView extends ViewPart {
 		.generateLayout(composite);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(composite);
 
-		fbType = AbstractInterpreterTest.loadFBType("E_CTU");
 		createGraphicalViewer(composite);
 		setTitleImage(FordiacImage.ICON_BASIC_FB.getImage());
 	}
@@ -89,6 +74,7 @@ public class FBDebugView extends ViewPart {
 		configureGraphicalViewer();
 		initializeGraphicalViewer();
 		hookGraphicalViewer();
+		DebugUITools.addPartDebugContextListener(getSite(), this);
 	}
 
 
@@ -96,7 +82,7 @@ public class FBDebugView extends ViewPart {
 		viewer.getControl().setBackground(ColorConstants.listBackground);
 		final ScalableFreeformRootEditPart root = createRootEditPart();
 		viewer.setRootEditPart(root);
-		viewer.setEditPartFactory(getEditpartFactory());
+		viewer.setEditPartFactory(new FBInterfaceEditPartFactory(null, null));
 		viewer.setContextMenu(new FordiacContextMenuProvider(viewer, root.getZoomManager(), getActionRegistry()));
 		viewer.setProperty(MouseWheelHandler.KeyGenerator.getKey(SWT.MOD1), MouseWheelZoomHandler.SINGLETON);
 		final KeyHandler viewerKeyHandler = new GraphicalViewerKeyHandler(viewer).setParent(getCommonKeyHandler());
@@ -106,98 +92,6 @@ public class FBDebugView extends ViewPart {
 
 	private void initializeGraphicalViewer() {
 		// Placing a listener and setting the appropriate content
-		displaySelectedType();
-	}
-
-	private void displaySelectedType() {
-		selectionListener = (part, sel) -> {
-			if (part.getTitle().equals(DEBUG_VIEW) && !sel.isEmpty() && sel instanceof TreeSelection) {
-				final TreeSelection ts = (TreeSelection) sel;
-				try {
-					checkSelectionType(ts.getPaths());
-				} catch (final DebugException e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		getSite().getPage().addSelectionListener(selectionListener);
-	}
-
-	private void checkSelectionType(final TreePath[] paths) throws DebugException {
-		String[] fbNameAndExtension = new String[0];
-		if (paths[0].getLastSegment() instanceof EvaluatorDebugTarget) {
-
-			final EvaluatorDebugTarget debugTarget = (EvaluatorDebugTarget) paths[0].getLastSegment();
-			try {
-				fbNameAndExtension = debugTarget.getName().split("\\.");
-				fbType = AbstractInterpreterTest.loadFBType(fbNameAndExtension[0]);
-			} catch (final DebugException e) {
-				e.printStackTrace();
-			}
-			viewer.setContents(fbType);
-		} else if (paths[0].getLastSegment() instanceof EvaluatorDebugThread) {
-			final EvaluatorDebugThread debugThread = (EvaluatorDebugThread) paths[0].getLastSegment();
-			if (debugThread.getTopStackFrame().getEvaluator().getContext().getType() instanceof BasicFBType) {
-				// Get the type's name:
-				fbType = AbstractInterpreterTest.loadFBType(
-						((BasicFBType) debugThread.getTopStackFrame().getEvaluator().getContext().getType()).getName());
-				viewer.setContents(fbType);
-			}
-		} else if (paths[0].getLastSegment() instanceof EvaluatorDebugStackFrame) {
-			final EvaluatorDebugStackFrame debugStackFrame = (EvaluatorDebugStackFrame) paths[0].getLastSegment();
-			debugStackFrame.getEvaluator().getSourceElement();
-
-			if (debugStackFrame.getEvaluator().getContext().getType() instanceof BasicFBType) {
-				// Get the type's name:
-				fbType = AbstractInterpreterTest
-						.loadFBType(((BasicFBType) debugStackFrame.getEvaluator().getContext().getType()).getName());
-				viewer.setContents(fbType);
-			}
-
-		}
-	}
-
-	private FBType loadFBType(final String selectedFbName) {
-
-		// (FBType) TypeLibraryManager.INSTANCE.getTypeEntryForFile((IFile) resource).getType();
-
-		final List<AutomationSystem> searchRootSystems = new ArrayList<>();
-
-		Display.getDefault().syncExec(() -> {
-			final IEditorPart openEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-					.getActiveEditor();
-			final IProject project = openEditor.getAdapter(FBNetwork.class).getAutomationSystem().getTypeLibrary()
-					.getProject();
-			searchRootSystems.addAll(SystemManager.INSTANCE.getProjectSystems(project));
-		});
-
-		TypeEntry te = fbType.getTypeEntry();
-		for (final AutomationSystem sys : searchRootSystems) {
-			te = searchTypeLibrary(sys, selectedFbName);
-		}
-
-		if (te.getType() instanceof FBType) {
-			return (FBType) te.getType();
-		}
-		// TODO: think of adapters and don't return null
-		return null;
-
-	}
-
-	private static TypeEntry searchTypeLibrary(final AutomationSystem sys, final String selectedFbName) {
-		final TypeLibrary lib = sys.getTypeLibrary();
-		for (final TypeEntry entry : lib.getFbTypes().values()) {
-			if (entry.getTypeName().equals(selectedFbName)) {
-				return entry;
-			}
-		}
-		for (final TypeEntry entry : lib.getAdapterTypes().values()) {
-			if (entry.getTypeName().equals(selectedFbName)) {
-				return entry;
-			}
-		}
-		return null;
-
 	}
 
 	private void hookGraphicalViewer() {
@@ -224,7 +118,6 @@ public class FBDebugView extends ViewPart {
 			// This is to overwrite the comments + add buttons to the viewer
 			@Override
 			protected List getModelChildren() {
-				getModelChildren();
 				return super.getModelChildren();
 			}
 
@@ -236,11 +129,6 @@ public class FBDebugView extends ViewPart {
 		};
 	}
 
-	private FBInterfaceEditPartFactory getEditpartFactory() {
-		final TypeLibrary typeLib = fbType.getTypeLibrary();
-		return new FBInterfaceEditPartFactory(null, typeLib);
-	}
-
 	private ActionRegistry getActionRegistry() {
 		if (actionRegistry == null) {
 			actionRegistry = new ActionRegistry();
@@ -250,7 +138,7 @@ public class FBDebugView extends ViewPart {
 
 	@Override
 	public void dispose() {
-		getSite().getPage().removeSelectionListener(selectionListener);
+		DebugUITools.removePartDebugContextListener(getSite(), this);
 	}
 
 	private KeyHandler getCommonKeyHandler() {
@@ -271,6 +159,44 @@ public class FBDebugView extends ViewPart {
 	@Override
 	public void setFocus() {
 		// Nothing for now
+	}
+
+	@Override
+	public void debugContextChanged(final DebugContextEvent event) {
+		if ((event.getFlags() & DebugContextEvent.ACTIVATED) > 0) {
+			contextActivated(event.getContext());
+		}
+	}
+
+	/** Updates actions and sets the viewer input when a context is activated.
+	 *
+	 * @param selection New selection to activate. */
+	protected void contextActivated(final ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			final Object source = ((IStructuredSelection) selection).getFirstElement();
+			final FBType type = getFBTypeFromDebugContext(source);
+
+			if (!isViewerContent(type)) {
+				viewer.setContents(type);
+			}
+		}
+	}
+
+	private boolean isViewerContent(final FBType type) {
+		final EditPart content = viewer.getContents();
+		return (content != null && content.getModel() == type) || type == null;
+	}
+
+	private static FBType getFBTypeFromDebugContext(final Object source) {
+		if (source instanceof EvaluatorDebugTarget) {
+			final Object sourceElement = ((EvaluatorDebugTarget) source).getProcess().getEvaluator().getSourceElement();
+			if (sourceElement instanceof FBType) {
+				return (FBType) sourceElement;
+			}
+		} else if (source instanceof EvaluatorDebugElement) {
+			return getFBTypeFromDebugContext(((EvaluatorDebugElement) source).getDebugTarget());
+		}
+		return null;
 	}
 
 }
