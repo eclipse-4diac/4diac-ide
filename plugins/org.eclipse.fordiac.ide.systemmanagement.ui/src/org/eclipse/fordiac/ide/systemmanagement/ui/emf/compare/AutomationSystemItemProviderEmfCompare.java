c4d536dd8ab718caa1710d7351c404b6a71471c9
mjger <michael.oberlehner@jku.at>
2022-10-05 11:46:06 +0200

    Bug 580647 - Added EMF Compare functionality
    Changed target to also have emf.compare.rcp.ui
    
    Added Item own EmfCompare Automationsystemprovider, since we want to
    have a custom tree in this view, for example we want to display the
    interface or connections as subtree of blocks in the structural merge
    viewer. Therefore a empty provider has been added that can be extented
    
    also added an empty filter for filtering out stuff
    
    Change-Id: Ibbcc792a0c53862ac354befaeadcf3508952ae5f

---
+++ plugins/org.eclipse.fordiac.ide.systemmanagement.ui/src/org/eclipse/fordiac/ide/systemmanagement/ui/emf/compare/AutomationSystemItemProviderEmfCompare.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Michael Oberlehner - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.systemmanagement.ui.emf.compare;
+
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.fordiac.ide.systemmanagement.ui.providers.AutomationSystemProviderAdapterFactory;
+
+public class AutomationSystemItemProviderEmfCompare extends AutomationSystemProviderAdapterFactory {
+
+	@Override
+	public Adapter createFBNetworkElementAdapter() {
+		if (fbNetworkElementItemProvider == null) {
+			fbNetworkElementItemProvider = new FbNetworkElementItemProviderEmfCompare(this);
+		}
+		return fbNetworkElementItemProvider;
+	}
+}
