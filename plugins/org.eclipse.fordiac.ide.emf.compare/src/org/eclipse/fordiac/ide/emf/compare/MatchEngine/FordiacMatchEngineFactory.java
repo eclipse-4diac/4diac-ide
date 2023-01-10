2c57797a1edc56bf9b2f3f5440c68562a9ab3213
mjger <michael.oberlehner@jku.at>
2023-01-10 15:54:50 +0100

    Bug 581313 - Emf compare uses wrong matcher
    added custom matcher for fordiac.
    now emf compare checks the provided id for all root elements like types
    or systems
    
    Copyed the IdentifierEObject matcher from emf compare and removed the
    guava dependecies
    
    Change-Id: Ia1fc18a2549829ebb9ff8f1905f781ee26ec75fa

---
+++ plugins/org.eclipse.fordiac.ide.emf.compare/src/org/eclipse/fordiac/ide/emf/compare/MatchEngine/FordiacMatchEngineFactory.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Michael Oberlehner - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.emf.compare.MatchEngine;
+
+import org.eclipse.emf.compare.match.IMatchEngine;
+import org.eclipse.emf.compare.scope.IComparisonScope;
+
+public class FordiacMatchEngineFactory implements IMatchEngine.Factory {
+
+	private int ranking = 20;
+
+	@Override
+	public IMatchEngine getMatchEngine() {
+		return new FordiacMatchEngine();
+	}
+
+	@Override
+	public int getRanking() {
+		return ranking;
+	}
+
+	@Override
+	public void setRanking(final int parseInt) {
+		this.ranking = 20;
+	}
+
+	@Override
+	public boolean isMatchEngineFactoryFor(final IComparisonScope scope) {
+		return true;
+	}
+
+}
