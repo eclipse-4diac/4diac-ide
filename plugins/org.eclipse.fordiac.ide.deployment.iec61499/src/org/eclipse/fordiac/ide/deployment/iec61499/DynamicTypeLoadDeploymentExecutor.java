81c286482eff5cf632a8aefe7c113ce3c032a0c6
Fabio Gandolfi <fabio.gandolfi@jku.at>
2023-01-17 17:37:27 +0100

    Bug 581370 - Reconstruct nested subapps from resource
    When downloading the resources from the running forte also
    nested subapps will be created now.
    
    Change-Id: I265aef55c5b35acce013b509df8f48a7bcfa599a
    Signed-off-by: Fabio Gandolfi <fabio.gandolfi@jku.at>

--- plugins/org.eclipse.fordiac.ide.deployment.iec61499/src/org/eclipse/fordiac/ide/deployment/iec61499/DynamicTypeLoadDeploymentExecutor.java
+++ plugins/org.eclipse.fordiac.ide.deployment.iec61499/src/org/eclipse/fordiac/ide/deployment/iec61499/DynamicTypeLoadDeploymentExecutor.java
@@ -15,6 +15,7 @@
  *   Alois Zoitl - reworked this class for the new device managment interaction
  *                 interface
  *   Jan Holzweber - reworked deploying mechanism
+ *   Fabio Gandolfi - reconstruct subapp hierarchy from resources
  *******************************************************************************/
 package org.eclipse.fordiac.ide.deployment.iec61499;
 
@@ -26,6 +27,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -41,6 +44,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+import org.eclipse.fordiac.ide.application.commands.NewSubAppCommand;
 import org.eclipse.fordiac.ide.deployment.IDeviceManagementCommunicationHandler;
 import org.eclipse.fordiac.ide.deployment.data.FBDeploymentData;
 import org.eclipse.fordiac.ide.deployment.devResponse.Response;
@@ -68,6 +72,7 @@ import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
 import org.eclipse.fordiac.ide.model.libraryElement.InterfaceList;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
 import org.eclipse.fordiac.ide.model.libraryElement.Resource;
+import org.eclipse.fordiac.ide.model.libraryElement.SubApp;
 import org.eclipse.fordiac.ide.model.libraryElement.Value;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 import org.eclipse.fordiac.ide.model.typelibrary.FBTypeEntry;
@@ -375,14 +380,29 @@ public class DynamicTypeLoadDeploymentExecutor extends DeploymentExecutor {
 			if (!"E_RESTART".equals(fbresult.getType())) { //$NON-NLS-1$
 				FBTypeEntry entry = res.getDevice().getAutomationSystem().getTypeLibrary()
 						.getFBTypeEntry(fbresult.getType());
+
 				if (null == entry) {
 					addTypeToTypelib(res, fbresult.getType(), TypeLibraryTags.FB_TYPE_FILE_ENDING, QUERY_FB_TYPE);
 					entry = res.getDevice().getAutomationSystem().getTypeLibrary().getFBTypeEntry(fbresult.getType());
 				}
-				final FBCreateCommand fbcmd = new FBCreateCommand(entry, res.getFBNetwork(), 100 * i, 10);
-				if (fbcmd.canExecute()) {
+
+				FBCreateCommand fbcmd = null;
+				if (fbresult.getName().contains(".")) {
+					final SubApp parent = findSubAppOfFB(
+							fbresult.getName().substring(0, fbresult.getName().lastIndexOf(".")), res.getFBNetwork());
+					fbcmd = new FBCreateCommand(entry, parent.getSubAppNetwork(),
+							10, 10);
+				} else {
+					fbcmd = new FBCreateCommand(entry, res.getFBNetwork(), 100 * i, 10);
+				}
+				if (fbcmd != null && fbcmd.canExecute()) {
 					fbcmd.execute();
-					fbcmd.getFB().setName(fbresult.getName());
+					if (fbresult.getName().contains(".")) {
+						fbcmd.getFB().setName(fbresult.getName().substring(fbresult.getName().lastIndexOf(".") + 1,
+								fbresult.getName().length()));
+					} else {
+						fbcmd.getFB().setName(fbresult.getName());
+					}
 				}
 				i++;
 			}
@@ -417,6 +437,38 @@ public class DynamicTypeLoadDeploymentExecutor extends DeploymentExecutor {
 						e);
 			}
 		}
+	}
+
+	private SubApp findSubAppOfFB(final String path, final FBNetwork network) {
+		final String[] paths = path.split("\\.");
+		SubApp subapp = null;
+		for (final String pathPice : paths) {
+			if (Arrays.asList(paths).indexOf(pathPice) == 0) {
+				subapp = network.getSubAppNamed(pathPice);
+				if (subapp == null) {
+					subapp = createSubApp(network, pathPice);
+				}
+			} else {
+				final SubApp newSubapp = subapp.getSubAppNetwork().getSubAppNamed(pathPice);
+				if (newSubapp == null) {
+					subapp = createSubApp(subapp.getSubAppNetwork(), pathPice);
+				} else {
+					subapp = newSubapp;
+				}
+			}
+		}
+		return subapp;
+	}
+
+	private SubApp createSubApp(final FBNetwork network, final String name) {
+		final NewSubAppCommand subapp = new NewSubAppCommand(network, Collections.emptyList(), 10, 10);
+
+		if (subapp.canExecute()) {
+			subapp.execute();
+			subapp.getElement().setName(name);
+		}
+
+		return subapp.getElement();
 	}
 
 	private static ResourceTypeEntry getResourceType(final Device device, final String resTypeName) {
