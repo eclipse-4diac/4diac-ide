4f95f85b1809204f26d8879b55c1d46c47c3b974
Martin Jobst <martin.jobst@posteo.de>
2023-01-17 17:46:41 +0100

    Bug 581415 - Adopt new ST editor for ECC transition conditions
    Change-Id: I33cceb06fec0f9948aa6b1203e093d922ab1c4cd
    Signed-off-by: Martin Jobst <martin.jobst@posteo.de>

--- plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/properties/TransitionSection.java
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/properties/TransitionSection.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2015 - 2017 fortiss GmbH
  *               2019 Johannes Kepler University Linz
+ *               2023 Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -15,89 +16,52 @@
  *    - consistent dropdown menu edit
  *   Virendra Ashiwal, Bianca Wiesmayr
  *   - Used extracted method from ECCContentAndLabelProvider
+ *   Martin Jobst
+ *   - adopt ST editor for transition conditions
  *******************************************************************************/
 package org.eclipse.fordiac.ide.fbtypeeditor.ecc.properties;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.EcoreUtil;
+import java.util.Collections;
+
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.Messages;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeConditionEventCommand;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeConditionExpressionCommand;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.ChangeECTransitionCommentCommand;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.contentprovider.ECCContentAndLabelProvider;
 import org.eclipse.fordiac.ide.gef.properties.AbstractSection;
-import org.eclipse.fordiac.ide.model.libraryElement.AdapterDeclaration;
+import org.eclipse.fordiac.ide.model.datatype.helper.IecTypes.ElementaryTypes;
 import org.eclipse.fordiac.ide.model.libraryElement.BasicFBType;
+import org.eclipse.fordiac.ide.model.libraryElement.ECC;
 import org.eclipse.fordiac.ide.model.libraryElement.ECTransition;
-import org.eclipse.fordiac.ide.model.libraryElement.FBType;
+import org.eclipse.fordiac.ide.structuredtextalgorithm.ui.editor.embedded.STAlgorithmConditionEditedResourceProvider;
+import org.eclipse.fordiac.ide.structuredtextalgorithm.ui.editor.embedded.STAlgorithmEmbeddedEditorUtil;
+import org.eclipse.fordiac.ide.ui.providers.SourceViewerColorProvider;
 import org.eclipse.fordiac.ide.ui.widget.ComboBoxWidgetFactory;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.CommandStack;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
-import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
-import org.eclipse.xtext.Constants;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditor;
-import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditorFactory;
 import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditorModelAccess;
 import org.eclipse.xtext.ui.editor.embedded.IEditedResourceProvider;
 
-import com.google.inject.Inject;
-import com.google.inject.Provider;
-import com.google.inject.name.Named;
-
-/**
- * Section that appears in the Properties view, when a Transition is selected in
- * the ECC
- *
- */
+/** Section that appears in the Properties view, when a Transition is selected in the ECC */
 @SuppressWarnings("restriction")
 public class TransitionSection extends AbstractSection {
-	private static final String LINKING_FILE_EXTENSION = "xtextfbt"; //$NON-NLS-1$
 	private Text commentText;
-	private CCombo eventCombo;
+
 	private Composite conditionEditingContainer;
-	// the closing bracket label needed for putting the xtext editor before it.
-	private CLabel closingBracket;
-	private EmbeddedEditor editor;
-	private Composite composite;
+	private CCombo eventCombo;
 
-	@Inject
-	private EmbeddedEditorFactory editorFactory;
-
-	@Inject
-	private Provider<XtextResourceSet> resourceSetProvider;
-
-	@Inject
-	@Named(Constants.FILE_EXTENSIONS)
-	private String fileExtension;
-
-	private EmbeddedEditorModelAccess embeddedEditorModelAccess;
-
-	private final IDocumentListener listener = new IDocumentListener() {
-		@Override
-		public void documentChanged(final DocumentEvent event) {
-			executeCommand(
-					new ChangeConditionExpressionCommand(getType(), embeddedEditorModelAccess.getEditablePart()));
-		}
-
-		@Override
-		public void documentAboutToBeChanged(final DocumentEvent event) {
-			// nothing to do here
-		}
-	};
+	private EmbeddedEditor conditionEditor;
+	private EmbeddedEditorModelAccess conditionEditorModelAccess;
 
 	@Override
 	protected ECTransition getType() {
@@ -105,7 +69,14 @@ public class TransitionSection extends AbstractSection {
 	}
 
 	protected BasicFBType getBasicFBType() {
-		return (null != getType().getECC()) ? getType().getECC().getBasicFBType() : null;
+		final ECTransition type = getType();
+		if (type != null) {
+			final ECC ecc = type.getECC();
+			if (ecc != null) {
+				return ecc.getBasicFBType();
+			}
+		}
+		return null;
 	}
 
 	@Override
@@ -122,12 +93,13 @@ public class TransitionSection extends AbstractSection {
 	@Override
 	public void createControls(final Composite parent, final TabbedPropertySheetPage tabbedPropertySheetPage) {
 		super.createControls(parent, tabbedPropertySheetPage);
-		composite = getWidgetFactory().createComposite(parent);
-		composite.setLayout(new GridLayout(2, false));
-		composite.setLayoutData(new GridData(SWT.FILL, 0, true, false));
+		final Composite composite = getWidgetFactory().createComposite(parent);
+		GridLayoutFactory.fillDefaults().numColumns(2).applyTo(composite);
+		GridDataFactory.swtDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).applyTo(composite);
 		getWidgetFactory().createCLabel(composite, Messages.TransitionSection_Condition);
 
-		createConditionEditingPlaceHolder(composite);
+		createConditionEditingContainer(composite);
+		GridDataFactory.swtDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).applyTo(conditionEditingContainer);
 
 		getWidgetFactory().createCLabel(composite, Messages.TransitionSection_Comment);
 		commentText = createGroupText(composite, true);
@@ -138,42 +110,65 @@ public class TransitionSection extends AbstractSection {
 		});
 	}
 
-	/**
-	 * creates a composite where the transition condition editing widgets can be
-	 * placed later on.
-	 *
-	 * The reason for having this is that the condition expression editor needs the
-	 * FB Type for context resolution (e.g., allow resolving of data inputs outputs
-	 * and internal variables). The type is only available later therefore these
-	 * widgets will be created when the input is set.
-	 *
-	 * @param parent
-	 */
-	private void createConditionEditingPlaceHolder(final Composite parent) {
+	private void createConditionEditingContainer(final Composite parent) {
 		conditionEditingContainer = getWidgetFactory().createComposite(parent);
-		conditionEditingContainer.setLayout(new GridLayout(4, false));
-		final GridData compositeLayoutData = new GridData(SWT.FILL, 0, true, false);
-		compositeLayoutData.verticalIndent = 0;
-		conditionEditingContainer.setLayoutData(compositeLayoutData);
+		GridLayoutFactory.fillDefaults().numColumns(4).applyTo(conditionEditingContainer);
 
 		eventCombo = ComboBoxWidgetFactory.createCombo(getWidgetFactory(), conditionEditingContainer);
 		eventCombo.addListener(SWT.Selection, event -> {
 			removeContentAdapter();
 			executeCommand(new ChangeConditionEventCommand(getType(), eventCombo.getText()));
-			setExpressionTextboxEnablement();
+			updateConditionEditor();
 			addContentAdapter();
 		});
 
 		getWidgetFactory().createCLabel(conditionEditingContainer, "["); //$NON-NLS-1$
 
-		closingBracket = getWidgetFactory().createCLabel(conditionEditingContainer, "]"); //$NON-NLS-1$
+		createConditionEditor(conditionEditingContainer);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(conditionEditor.getViewer().getControl());
+
+		getWidgetFactory().createCLabel(conditionEditingContainer, "]"); //$NON-NLS-1$
+	}
+
+	protected void createConditionEditor(final Composite parent) {
+		final IEditedResourceProvider editedResourceProvider = new STAlgorithmConditionEditedResourceProvider(
+				getBasicFBType(), Collections.emptySet(), ElementaryTypes.BOOL);
+		conditionEditor = STAlgorithmEmbeddedEditorUtil.getEmbeddedEditorFactory().newEditor(editedResourceProvider)
+				.withStyle(SWT.SINGLE | SWT.BORDER).withParent(parent);
+		conditionEditorModelAccess = conditionEditor.createPartialEditor();
+		conditionEditor.getViewer().setEditable(false);
+		conditionEditor.getDocument().addDocumentListener(new IDocumentListener() {
+			@Override
+			public void documentChanged(final DocumentEvent event) {
+				removeContentAdapter();
+				executeCommand(
+						new ChangeConditionExpressionCommand(getType(), conditionEditorModelAccess.getEditablePart()));
+				addContentAdapter();
+			}
+
+			@Override
+			public void documentAboutToBeChanged(final DocumentEvent event) {
+				// do nothing
+			}
+		});
+		SourceViewerColorProvider.initializeSourceViewerColors(conditionEditor.getViewer());
+	}
+
+	protected void updateConditionEditor() {
+		if ((getType().getConditionExpression() != null)
+				&& getType().getConditionExpression().equals(ECCContentAndLabelProvider.ONE_CONDITION)) {
+			conditionEditorModelAccess.updateModel(""); //$NON-NLS-1$
+			conditionEditor.getViewer().setEditable(false);
+		} else {
+			conditionEditorModelAccess.updateModel(getType().getConditionExpression());
+			conditionEditor.getViewer().setEditable(true);
+		}
 	}
 
 	@Override
 	protected void setInputInit() {
-		if (null == editor) {
-			createTransitionEditor(conditionEditingContainer);
-		}
+		STAlgorithmEmbeddedEditorUtil.updateEditor(conditionEditor, getType().eResource().getURI(), getBasicFBType(),
+				null, ElementaryTypes.BOOL);
 	}
 
 	@Override
@@ -181,61 +176,7 @@ public class TransitionSection extends AbstractSection {
 		commentText.setEnabled(false);
 		eventCombo.removeAll();
 		eventCombo.setEnabled(false);
-	}
-
-	private void createTransitionEditor(final Composite parent) {
-		final FBType fbType = getBasicFBType();
-
-		final IEditedResourceProvider resourceProvider = new IEditedResourceProvider() {
-
-			@Override
-			public XtextResource createResource() {
-				final XtextResourceSet resourceSet = resourceSetProvider.get();
-				final EcoreUtil.Copier copier = new EcoreUtil.Copier();
-				final Resource fbResource = resourceSet.createResource(computeUnusedUri(resourceSet, LINKING_FILE_EXTENSION));
-				fbResource.getContents().add(copier.copy(EcoreUtil.getRootContainer(fbType)));
-				for (final AdapterDeclaration adapter : fbType.getInterfaceList().getSockets()) {
-					createAdapterResource(resourceSet, copier, adapter);
-				}
-				for (final AdapterDeclaration adapter : fbType.getInterfaceList().getPlugs()) {
-					createAdapterResource(resourceSet, copier, adapter);
-				}
-				copier.copyReferences();
-				final Resource resource = resourceSet.createResource(computeUnusedUri(resourceSet, fileExtension));
-				return (XtextResource) resource;
-			}
-
-			private void createAdapterResource(final XtextResourceSet resourceSet, final EcoreUtil.Copier copier,
-					final AdapterDeclaration adapter) {
-				if ((null != adapter.getType()) && (null != adapter.getAdapterType().getAdapterFBType())) {
-					final Resource adapterResource = resourceSet
-							.createResource(computeUnusedUri(resourceSet, LINKING_FILE_EXTENSION));
-					copier.copy(adapter.getType());
-					adapterResource.getContents()
-					.add(copier.copy(EcoreUtil.getRootContainer(adapter.getAdapterType().getAdapterFBType())));
-				}
-			}
-
-			protected URI computeUnusedUri(final ResourceSet resourceSet, final String fileExtension) {
-				final String name = "__synthetic"; //$NON-NLS-1$
-				for (int i = 0; i < Integer.MAX_VALUE; i++) {
-					final URI syntheticUri = URI.createURI(name + i + "." + fileExtension); //$NON-NLS-1$
-					if (resourceSet.getResource(syntheticUri, false) == null) {
-						return syntheticUri;
-					}
-				}
-				throw new IllegalStateException();
-			}
-		};
-
-		editor = editorFactory.newEditor(resourceProvider).withParent(parent);
-		final StyledText conditionText = (StyledText) editor.getViewer().getControl();
-		conditionText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		conditionText.moveAbove(closingBracket);
-
-		embeddedEditorModelAccess = editor.createPartialEditor();
-		editor.getDocument().addDocumentListener(listener);
-		composite.layout();
+		conditionEditor.getViewer().setEditable(false);
 	}
 
 	@Override
@@ -245,7 +186,6 @@ public class TransitionSection extends AbstractSection {
 		if ((null != type) && (null != getBasicFBType())) {
 			fillEventConditionDropdown();
 			commentText.setText(getType().getComment() != null ? getType().getComment() : ""); //$NON-NLS-1$
-			updateConditionExpressionText(getType().getConditionExpression());
 			if ((getType().getConditionExpression() != null)
 					&& getType().getConditionExpression().equals(ECCContentAndLabelProvider.ONE_CONDITION)) {
 				eventCombo.select(eventCombo.indexOf(ECCContentAndLabelProvider.ONE_CONDITION));
@@ -254,26 +194,7 @@ public class TransitionSection extends AbstractSection {
 						? eventCombo.indexOf(getType().getConditionEvent().getName())
 								: (eventCombo.getItemCount() - 1));
 			}
-			setExpressionTextboxEnablement();
-		}
-		commandStack = commandStackBuffer;
-	}
-
-	private void updateConditionExpressionText(final String conditionExpression) {
-		if (null != embeddedEditorModelAccess) {
-			embeddedEditorModelAccess.updateModel((null != conditionExpression) ? conditionExpression : ""); //$NON-NLS-1$
-		}
-	}
-
-	private void setExpressionTextboxEnablement() {
-		final CommandStack commandStackBuffer = commandStack;
-		commandStack = null;
-		if ((getType().getConditionExpression() != null)
-				&& getType().getConditionExpression().equals(ECCContentAndLabelProvider.ONE_CONDITION)) {
-			updateConditionExpressionText(""); //$NON-NLS-1$
-			editor.getViewer().getControl().setEnabled(false);
-		} else {
-			editor.getViewer().getControl().setEnabled(true);
+			updateConditionEditor();
 		}
 		commandStack = commandStackBuffer;
 	}
