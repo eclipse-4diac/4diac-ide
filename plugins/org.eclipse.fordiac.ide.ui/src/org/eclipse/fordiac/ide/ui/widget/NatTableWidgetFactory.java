b1bdba3a39d03ec001d2356a9046f1636cb0927f
Dunja Zivotin <dunja.zivotin@jku.at>
2023-01-25 11:08:56 +0100

    Bug 581413 - NatTable's Type Selection Dialog
    Moved the NatTable button for type selection into a different plugin so
    certain things (like type library) are available for future commits.
    
    Change-Id: Ia074f0d1ed5609f61cc57aefda58eee420a16002
    Signed-off-by: Dunja Zivotin <dunja.zivotin@jku.at>

--- plugins/org.eclipse.fordiac.ide.ui/src/org/eclipse/fordiac/ide/ui/widget/NatTableWidgetFactory.java
+++ plugins/org.eclipse.fordiac.ide.ui/src/org/eclipse/fordiac/ide/ui/widget/NatTableWidgetFactory.java
@@ -94,7 +94,10 @@ public final class NatTableWidgetFactory {
 			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 			'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
 			'3', '4', '5', '6', '7', '8', '9', '_', '.', SWT.BS };
-
+	
+	public static char[] getActivationChars() {
+		return ACTIVATION_CHARS;
+	}
 
 	public static NatTable createNatTable(final Composite parent, final DataLayer dataLayer,
 			final IDataProvider headerDataProvider) {
@@ -416,4 +419,5 @@ public final class NatTableWidgetFactory {
 	private NatTableWidgetFactory() {
 		throw new UnsupportedOperationException("Helper class should not be insantiated!");  //$NON-NLS-1$
 	}
+
 }
