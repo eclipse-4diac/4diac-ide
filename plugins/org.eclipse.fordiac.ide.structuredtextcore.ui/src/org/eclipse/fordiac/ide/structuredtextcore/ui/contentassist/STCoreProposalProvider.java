ff6748c6a35a9f5ee54407dfa5eba0aa9fd79c6a
Martin Melik Merkumians <melik-merkumians@acin.tuwien.ac.at>
2023-01-26 10:17:27 +0100

    Bug 581421 - Fixes STAlgorithm provider
    This fix should now filter on only context relevant proposals
    
    Change-Id: I57c98038585028e3b036f923e02c6be300799107
    Signed-off-by: Martin Melik Merkumians <melik-merkumians@acin.tuwien.ac.at>

--- plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/contentassist/STCoreProposalProvider.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/contentassist/STCoreProposalProvider.java
@@ -13,12 +13,14 @@
  */
 package org.eclipse.fordiac.ide.structuredtextcore.ui.contentassist;
 
+import java.util.Optional;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.fordiac.ide.model.libraryElement.ICallable;
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.STFeatureExpression;
-import org.eclipse.fordiac.ide.structuredtextcore.stcore.STStatement;
 import org.eclipse.xtext.Assignment;
 import org.eclipse.xtext.CrossReference;
+import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
@@ -41,30 +43,30 @@ public class STCoreProposalProvider extends AbstractSTCoreProposalProvider {
 					public boolean apply(final IEObjectDescription arg0) {
 						final var current = context.getCurrentModel();
 						final var callable = getICallable(current);
-						if (current instanceof ICallable || current instanceof STStatement) {
-							return isFunction(arg0) || isLocal(arg0, callable);
-						}
-						if (current instanceof STFeatureExpression) {
+						if (current instanceof STFeatureExpression && callable.isPresent()) {
 							final var featureExpr = (STFeatureExpression) current;
 							final var called = featureExpr.getFeature() instanceof ICallable
 									? (ICallable) featureExpr.getFeature()
 									: null;
-							return isFunction(arg0) || isLocal(arg0, callable) || isParameter(arg0, called);
+							return isFunction(arg0) || isLocal(arg0, callable.get()) || isParameter(arg0, called);
 						}
-						return true;
+						if (callable.isPresent()) {
+							return isFunction(arg0) || isLocal(arg0, callable.get());
+						}
+						return false;
 					}
 				});
 	}
 
-	protected ICallable getICallable(final EObject object) {
+	protected Optional<ICallable> getICallable(final EObject object) {
 		EObject current = object;
 		while (current != null) {
 			if (current instanceof ICallable) {
-				return (ICallable) current;
+				return Optional.of((ICallable) current);
 			}
 			current = current.eContainer();
 		}
-		return null;
+		return Optional.empty();
 	}
 
 	protected boolean isFunction(final IEObjectDescription description) {
@@ -75,7 +77,33 @@ public class STCoreProposalProvider extends AbstractSTCoreProposalProvider {
 	protected boolean isLocal(final IEObjectDescription description, final ICallable callable) {
 		final var candidateFQN = description.getQualifiedName();
 		final var currentCallableName = callable.getName();
-		return candidateFQN.getFirstSegment().equals(currentCallableName);
+		final var candidateFileURI = description.getEObjectURI().trimFragment();
+		final var callableFileContextURI = callable.eResource().getURI().trimFragment();
+		final var contextFileName = callableFileContextURI.lastSegment();
+
+		var contextName = candidateFQN.getFirstSegment();
+		// if the candidate is from an FB's ST section, the relevant information is in
+		// the second segment
+		if (isFBStructuredTextContext(candidateFQN) && candidateFQN.getSegmentCount() > 1) {
+			contextName = candidateFQN.getSegment(1);
+			// In case of a method, it needs to be checked that the method is in the same
+			// file context (FB definition file)
+			if (candidateFQN.getSegmentCount() == 2) {
+				return callableFileContextURI.equals(candidateFileURI);
+			}
+		}
+		// if the candidate is not in an ST context, then it needs to be in the same
+		// file for FBs (ins, outs, internals)
+		else if (contextFileName.contains(contextName)) {
+			return true;
+		}
+		// in other cases, the candidate is a algorithm- or method-specific variable,
+		// which needs to be in the file and in the callable context
+		return contextName.equals(currentCallableName) && callableFileContextURI.equals(candidateFileURI);
+	}
+
+	private boolean isFBStructuredTextContext(final QualifiedName candidateFQN) {
+		return "st".contentEquals(candidateFQN.getFirstSegment());
 	}
 
 	protected boolean isParameter(final IEObjectDescription description, final ICallable callable) {
@@ -91,6 +119,5 @@ public class STCoreProposalProvider extends AbstractSTCoreProposalProvider {
 		final var isInOut = callable.getInOutParameters().stream()
 				.anyMatch(parameter -> parameter.getName().equals(candidateSimpleName));
 		return isInOut || isInput || isOutput;
-
 	}
 }
