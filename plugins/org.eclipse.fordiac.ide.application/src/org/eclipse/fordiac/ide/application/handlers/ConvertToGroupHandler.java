cc0c99ef9e8a5c9ff1556a2117461dec4ac5bd00
Alois Zoitl <alois.zoitl@gmx.at>
2023-02-21 21:33:17 +0100

    Bug 581567 - Removed distributed system listener calls in App Plugin
    All calls to notify distributed system listeners are removed from the
    application plugin.
    
    Change-Id: I44c02a0acd1b51fd2a0f43b0834ea86132f9520d

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/ConvertToGroupHandler.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/ConvertToGroupHandler.java
@@ -23,7 +23,6 @@ import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.fordiac.ide.model.libraryElement.Group;
 import org.eclipse.fordiac.ide.model.libraryElement.SubApp;
 import org.eclipse.fordiac.ide.model.ui.editors.HandlerHelper;
-import org.eclipse.fordiac.ide.systemmanagement.SystemManager;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.gef.commands.CommandStackEvent;
 import org.eclipse.gef.commands.CommandStackEventListener;
@@ -47,7 +46,6 @@ public class ConvertToGroupHandler extends AbstractHandler implements CommandSta
 				commandStack.execute(cmd);
 			}
 			commandStack.addCommandStackEventListener(this);
-			SystemManager.INSTANCE.notifyListeners();
 		}
 		return Status.OK_STATUS;
 	}
