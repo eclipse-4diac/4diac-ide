d7fdcc9aa0579fcec0d8746475ec5da9aed7118d
Prankur Agarwal <prankur.agarwal@jku.at>
2023-03-14 15:33:57 +0100

    Bug 581658 - Add GitLab Package Downloader Preference Page
    This commit adds a preference page in settings dialog box. Go under
    4diac IDE -> GitLab Package Downloader.
    
    Change-Id: Ie03c5924db60e8acefaddcb29e18ecf5495334b0
    Signed-off-by: Prankur Agarwal <prankur.agarwal@jku.at>

---
+++ plugins/org.eclipse.fordiac.ide.gitlab/src/org/eclipse/fordiac/ide/gitlab/Messages.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Johannes Kepler University, Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Prankur Agarwal - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.gitlab;
+
+import org.eclipse.osgi.util.NLS;
+
+/** The Class Messages. */
+@SuppressWarnings("squid:S3008") // tell sonar the java naming convention does not make sense for this class
+public final class Messages extends NLS {
+	private static String BUNDLE_NAME = "org.eclipse.fordiac.ide.gitlab.messages"; //$NON-NLS-1$
+
+	public static String GitLab_Token;
+
+	public static String GitLab_Token_Title;
+
+	public static String GitLab_Url;
+
+	public static String GitLab_Url_Title;
+
+	public static String GitLab_PreferenceId;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+		// empty private constructor
+	}
+}
