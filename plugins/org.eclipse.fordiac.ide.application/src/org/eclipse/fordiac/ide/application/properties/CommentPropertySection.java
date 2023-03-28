a1036e42d2ed3a253b54e45c423835d136c9cf16
fabio.gandolfi <fabio.gandolfi@jku.at>
2023-03-28 17:07:40 +0200

    Bug 581719 - Added property sheet for comments
    added a new property sheet + filter for comments
    
    Change-Id: I1098ca19265e9d0001b0fc84f7cda4e287e9bf1a
    Signed-off-by: fabio.gandolfi <fabio.gandolfi@jku.at>

---
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/CommentPropertySection.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Fabio Gandolfi
+ *     - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.application.properties;
+
+import org.eclipse.fordiac.ide.gef.properties.AbstractSection;
+import org.eclipse.fordiac.ide.model.commands.change.ChangeCommentCommand;
+import org.eclipse.fordiac.ide.model.libraryElement.Comment;
+import org.eclipse.gef.commands.CommandStack;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+
+public class CommentPropertySection extends AbstractSection {
+
+	private Text commentText;
+
+	@Override
+	public void createControls(final Composite parent, final TabbedPropertySheetPage tabbedPropertySheetPage) {
+		super.createControls(parent, tabbedPropertySheetPage);
+		final Composite fbInfoContainer = createFBInfoContainer(parent);
+		createCommentEntry(fbInfoContainer);
+	}
+
+	@Override
+	public void refresh() {
+		if ((getType() != null)) {
+			final CommandStack commandStackBuffer = commandStack;
+			commandStack = null;
+			commentText.setText(getType().getComment() != null ? getType().getComment() : ""); //$NON-NLS-1$
+			commandStack = commandStackBuffer;
+		}
+	}
+
+	protected Composite createFBInfoContainer(final Composite parent) {
+		final Composite fbInfoContainer = getWidgetFactory().createComposite(parent);
+		GridLayoutFactory.fillDefaults().numColumns(1).applyTo(fbInfoContainer);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(fbInfoContainer);
+		return fbInfoContainer;
+	}
+
+	private void createCommentEntry(final Composite parent) {
+		commentText = createGroupText(parent, true, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(commentText);
+		commentText.addModifyListener(e -> {
+			removeContentAdapter();
+			executeCommand(new ChangeCommentCommand(getType(), commentText.getText()));
+			addContentAdapter();
+		});
+	}
+
+	@Override
+	protected Object getInputType(final Object input) {
+		return CommentSectionFilter.commentFromSelectedObject(input);
+	}
+
+	@Override
+	protected Comment getType() {
+		if (type instanceof Comment) {
+			return (Comment) type;
+		}
+		return null;
+	}
+
+	@Override
+	protected void setInputCode() {
+		// Nothing for now
+	}
+
+	@Override
+	protected void setInputInit() {
+		// nothing to do for now
+	}
+}
