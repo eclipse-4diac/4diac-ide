67a3bc2689564f3e3a1ac558e7c180ed72cc37ae
Martin Jobst <martin.jobst@posteo.de>
2023-03-31 12:45:41 +0200

    Bug 581741 - Improve accuracy of comment associations
    Improve accuracy of comment associations by avoiding surrounding
    objects. Associate comments with (in order of preference):
     - previous object if on the same line and AFTER previous
     - next object if BEFORE next
     - previous object if AFTER previous
     - previous object if on the same line
     - next object
    
    Change-Id: I7018b4a7ad1825db35b7121a29bf72c64fcfa6f0
    Signed-off-by: Martin Jobst <martin.jobst@posteo.de>

--- plugins/org.eclipse.fordiac.ide.structuredtextfunctioneditor/src/org/eclipse/fordiac/ide/structuredtextfunctioneditor/STFunctionRuntimeModule.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextfunctioneditor/src/org/eclipse/fordiac/ide/structuredtextfunctioneditor/STFunctionRuntimeModule.java
@@ -13,7 +13,7 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.structuredtextfunctioneditor;
 
-import org.eclipse.fordiac.ide.structuredtextcore.util.STCoreExportCommentAssociater;
+import org.eclipse.fordiac.ide.structuredtextcore.parsetree.reconstr.STCoreCommentAssociater;
 import org.eclipse.fordiac.ide.structuredtextfunctioneditor.converter.STFunctionValueConverters;
 import org.eclipse.xtext.conversion.IValueConverterService;
 import org.eclipse.xtext.parsetree.reconstr.ICommentAssociater;
@@ -22,6 +22,7 @@ import org.eclipse.xtext.parsetree.reconstr.ICommentAssociater;
  * Use this class to register components to be used at runtime / without the
  * Equinox extension registry.
  */
+@SuppressWarnings("static-method")
 public class STFunctionRuntimeModule extends AbstractSTFunctionRuntimeModule {
 
 	@Override
@@ -30,6 +31,6 @@ public class STFunctionRuntimeModule extends AbstractSTFunctionRuntimeModule {
 	}
 
 	public Class<? extends ICommentAssociater> bindICommentAssociater() {
-		return STCoreExportCommentAssociater.class;
+		return STCoreCommentAssociater.class;
 	}
 }
