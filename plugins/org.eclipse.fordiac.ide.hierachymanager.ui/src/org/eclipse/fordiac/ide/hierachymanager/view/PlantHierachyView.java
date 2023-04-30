33e59860dd64821a53bb5a4af7fe0dfc559c85b6
Alois Zoitl <alois.zoitl@gmx.at>
2023-04-30 16:55:04 +0200

    Bug 581831 - Allow to set the content of the plant hierachy view
    With the menu entry Show in on a project the plant hierarchy view is now
    populated with new content.
    
    As we have no content and label providers yet the view is still empty.
    
    Also the link with editor and any filter menu entries have been
    disabled.
    
    Change-Id: Ic61e53234f554a235e9968f46bbbab1705bce300

---
+++ plugins/org.eclipse.fordiac.ide.hierachymanager.ui/src/org/eclipse/fordiac/ide/hierachymanager/view/PlantHierachyView.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Primetals Technology Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Alois Zoitl - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.hierachymanager.view;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.navigator.CommonNavigator;
+import org.eclipse.ui.part.ShowInContext;
+
+public class PlantHierachyView extends CommonNavigator {
+
+	@Override
+	public boolean show(final ShowInContext context) {
+		if (context == null) {
+			return false;
+		}
+		final ISelection selection = context.getSelection();
+		if (selection instanceof final IStructuredSelection structSel && !selection.isEmpty()
+				&& structSel.getFirstElement() instanceof final IProject proj) {
+			setInput(proj);
+			return true;
+		}
+		return super.show(context);
+	}
+
+	private void setInput(final IProject proj) {
+		if (getCommonViewer().getInput() != proj) {
+			// the new project is different set
+			getCommonViewer().setInput(proj);
+		}
+	}
+}
