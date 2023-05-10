dc02646c6fefa4c47c9aa86170a7118e8b822979
Alois Zoitl <alois.zoitl@gmx.at>
2023-05-10 21:16:03 +0200

    Bug 581916 - do not show navigation buttons in plant hierarchy view
    Change-Id: I619a49a822bf5784629365c280ea6f1132cc7e79

--- plugins/org.eclipse.fordiac.ide.hierarchymanager.ui/src/org/eclipse/fordiac/ide/hierarchymanager/ui/providers/HierarchyContentProvider.java
+++ plugins/org.eclipse.fordiac.ide.hierarchymanager.ui/src/org/eclipse/fordiac/ide/hierarchymanager/ui/providers/HierarchyContentProvider.java
@@ -13,11 +13,22 @@
 package org.eclipse.fordiac.ide.hierarchymanager.ui.providers;
 
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
+import org.eclipse.fordiac.ide.hierarchymanager.model.hierarchy.RootLevel;
 import org.eclipse.fordiac.ide.hierarchymanager.model.hierarchy.provider.HierarchyItemProviderAdapterFactory;
 
 public class HierarchyContentProvider extends AdapterFactoryContentProvider {
 
 	public HierarchyContentProvider() {
 		super(new HierarchyItemProviderAdapterFactory());
+	}
+
+	@Override
+	public Object getParent(final Object object) {
+		if (object instanceof RootLevel) {
+			// we don't want to provide anything above our root level
+			// this avoids an frame navigation buttons in the plant hierarchy view.
+			return null;
+		}
+		return super.getParent(object);
 	}
 }
