bebde8bd292a9eeeeb17e41463a879e0777b6521
fabio.gandolfi <fabio.gandolfi@jku.at>
2023-06-12 14:24:49 +0200

    Bug 582022 - add Level name to property sheet
    added the name of the level, with rename operation to the property
    sheet. Changes Tab to "level"
    
    Change-Id: I579b37bfdb21e5824ded70244b06e97eda86c182
    Signed-off-by: fabio.gandolfi <fabio.gandolfi@jku.at>

--- plugins/org.eclipse.fordiac.ide.hierarchymanager.ui/src/org/eclipse/fordiac/ide/hierarchymanager/ui/properties/CommentSection.java
+++ plugins/org.eclipse.fordiac.ide.hierarchymanager.ui/src/org/eclipse/fordiac/ide/hierarchymanager/ui/properties/CommentSection.java
@@ -17,10 +17,12 @@ import org.eclipse.fordiac.ide.gef.properties.AbstractSection;
 import org.eclipse.fordiac.ide.hierarchymanager.model.hierarchy.Level;
 import org.eclipse.fordiac.ide.hierarchymanager.ui.handlers.AbstractHierarchyHandler;
 import org.eclipse.fordiac.ide.hierarchymanager.ui.operations.ChangeLevelCommentOperation;
-import org.eclipse.gef.commands.CommandStack;
+import org.eclipse.fordiac.ide.hierarchymanager.ui.operations.RenameLevelOperation;
+import org.eclipse.fordiac.ide.ui.FordiacMessages;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
@@ -28,38 +30,56 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 public class CommentSection extends AbstractSection {
 
 	private Text commentText;
+	private Text nameText;
 
 	@Override
 	public void createControls(final Composite parent, final TabbedPropertySheetPage tabbedPropertySheetPage) {
 		super.createControls(parent, tabbedPropertySheetPage);
 		final Composite fbInfoContainer = createFBInfoContainer(parent);
+		createNameEntry(fbInfoContainer);
 		createCommentEntry(fbInfoContainer);
 	}
 
 	@Override
 	public void refresh() {
 		if ((getType() != null)) {
-			final CommandStack commandStackBuffer = commandStack;
-			commandStack = null;
 			commentText.setText(getType().getComment() != null ? getType().getComment() : ""); //$NON-NLS-1$
-			commandStack = commandStackBuffer;
+			nameText.setText(getType().getName() != null ? getType().getName() : "N/A"); //$NON-NLS-1$
 		}
 	}
 
 	protected Composite createFBInfoContainer(final Composite parent) {
 		final Composite fbInfoContainer = getWidgetFactory().createComposite(parent);
-		GridLayoutFactory.fillDefaults().numColumns(1).applyTo(fbInfoContainer);
-		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(fbInfoContainer);
+		GridLayoutFactory.fillDefaults().numColumns(2).applyTo(fbInfoContainer);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.TOP).grab(true, true).applyTo(fbInfoContainer);
 		return fbInfoContainer;
 	}
 
+	private void createNameEntry(final Composite parent) {
+		final CLabel nameLabel = getWidgetFactory().createCLabel(parent, FordiacMessages.Name + ":"); //$NON-NLS-1$
+		GridDataFactory.fillDefaults().align(SWT.LEFT, SWT.TOP).grab(false, false).applyTo(nameLabel);
+
+		nameText = createGroupText(parent, true, SWT.BORDER | SWT.SINGLE);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, false).applyTo(nameText);
+		nameText.addModifyListener(e -> {
+			removeContentAdapter();
+			AbstractHierarchyHandler
+			.executeOperation(new RenameLevelOperation(getType(), nameText.getText()));
+			addContentAdapter();
+		});
+	}
+
 	private void createCommentEntry(final Composite parent) {
+		final CLabel commentLabel = getWidgetFactory().createCLabel(parent, FordiacMessages.Comment + ":"); //$NON-NLS-1$
+		GridDataFactory.fillDefaults().align(SWT.LEFT, SWT.TOP).grab(false, false).applyTo(commentLabel);
+
 		commentText = createGroupText(parent, true, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
-		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(commentText);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.TOP).hint(SWT.DEFAULT, 6 * commentText.getLineHeight())
+		.grab(true, false).applyTo(commentText);
 		commentText.addModifyListener(e -> {
 			removeContentAdapter();
 			AbstractHierarchyHandler
-					.executeOperation(new ChangeLevelCommentOperation(getType(), commentText.getText()));
+			.executeOperation(new ChangeLevelCommentOperation(getType(), commentText.getText()));
 			addContentAdapter();
 		});
 	}
