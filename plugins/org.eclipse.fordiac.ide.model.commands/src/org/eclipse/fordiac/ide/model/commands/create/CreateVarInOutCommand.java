e97aa1563c686aa961c8b8b6189c4344c82e7455
Alois Zoitl <alois.zoitl@gmx.at>
2023-06-27 23:24:19 +0200

    Bug 582118 - Added create command for var in out variables
    cleaned createInterfaceCommand
    
    Change-Id: I7cdede7ae312f28f1484090015448c98de7f3196

---
+++ plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/CreateVarInOutCommand.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Alois Zoitl - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.model.commands.create;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.fordiac.ide.model.data.DataType;
+import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
+import org.eclipse.fordiac.ide.model.libraryElement.InterfaceList;
+
+public class CreateVarInOutCommand extends CreateInterfaceElementCommand {
+
+	public CreateVarInOutCommand(final IInterfaceElement copySrc, final InterfaceList targetInterfaceList,
+			final int index) {
+		super(copySrc, true, targetInterfaceList, index);
+	}
+
+	public CreateVarInOutCommand(final DataType dataType, final String name, final InterfaceList interfaceList,
+			final int index) {
+		super(dataType, name, interfaceList, true, index);
+	}
+
+	public CreateVarInOutCommand(final DataType dataType, final InterfaceList interfaceList, final int index) {
+		super(dataType, interfaceList, true, index);
+	}
+
+	public CreateVarInOutCommand(final DataType dataType, final String name, final InterfaceList interfaceList,
+			final String arraySize, final String value, final int index) {
+		super(dataType, name, interfaceList, true, arraySize, value, index);
+	}
+
+	@Override
+	protected EList<? extends IInterfaceElement> getInterfaceListContainer() {
+		return getInterfaceList().getInOutVars();
+	}
+
+}
