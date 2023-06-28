610c605656f07c63906fab43ce8f795e027b7d33
Martin Jobst <martin.jobst@posteo.de>
2023-06-28 23:04:10 +0200

    Bug 582077 - Add default events for function FB types
    Change-Id: I021a1407afd3edfdc1223bde2fcc560127b2c501
    Signed-off-by: Martin Jobst <martin.jobst@posteo.de>

--- plugins/org.eclipse.fordiac.ide.structuredtextfunctioneditor/src/org/eclipse/fordiac/ide/structuredtextfunctioneditor/util/STFunctionReconciler.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextfunctioneditor/src/org/eclipse/fordiac/ide/structuredtextfunctioneditor/util/STFunctionReconciler.java
@@ -12,19 +12,28 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.structuredtextfunctioneditor.util;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.eclipse.emf.common.util.ECollections;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.fordiac.ide.model.libraryElement.Event;
 import org.eclipse.fordiac.ide.model.libraryElement.FBType;
 import org.eclipse.fordiac.ide.model.libraryElement.FunctionFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.ICallable;
+import org.eclipse.fordiac.ide.model.libraryElement.InterfaceList;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
 import org.eclipse.fordiac.ide.model.libraryElement.STFunction;
 import org.eclipse.fordiac.ide.model.libraryElement.STFunctionBody;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
+import org.eclipse.fordiac.ide.model.libraryElement.With;
+import org.eclipse.fordiac.ide.model.typelibrary.EventTypeLibrary;
 import org.eclipse.fordiac.ide.structuredtextcore.util.STCoreReconciler;
 
 public class STFunctionReconciler implements STCoreReconciler {
+	private static final String DEFAULT_INPUT_EVENT_NAME = "REQ"; //$NON-NLS-1$
+	private static final String DEFAULT_OUTPUT_EVENT_NAME = "CNF"; //$NON-NLS-1$
 
 	@Override
 	public void reconcile(final FBType dest, final EList<? extends ICallable> source) {
@@ -45,23 +54,48 @@ public class STFunctionReconciler implements STCoreReconciler {
 		source.forEach(callable -> merge(dest, callable));
 		// reconcile interface
 		if (!source.isEmpty() && source.get(0) instanceof final STFunction function) {
-			reconcileInterface(dest, function);
+			reconcileInterface(dest.getInterfaceList(), function);
 		}
 	}
 
-	protected static void reconcileInterface(final FunctionFBType dest, final STFunction source) {
-		ECollections.setEList(dest.getInterfaceList().getInputVars(),
+	protected static void reconcileInterface(final InterfaceList interfaceList, final STFunction source) {
+		ECollections.setEList(interfaceList.getEventInputs(), List.of(createEvent(DEFAULT_INPUT_EVENT_NAME, true)));
+		ECollections.setEList(interfaceList.getEventOutputs(), List.of(createEvent(DEFAULT_OUTPUT_EVENT_NAME, false)));
+		ECollections.setEList(interfaceList.getInputVars(),
 				source.getInputParameters().stream().map(VarDeclaration.class::cast).map(EcoreUtil::copy).toList());
-		ECollections.setEList(dest.getInterfaceList().getOutputVars(),
+		ECollections.setEList(interfaceList.getOutputVars(),
 				source.getOutputParameters().stream().map(VarDeclaration.class::cast).map(EcoreUtil::copy).toList());
-		ECollections.setEList(dest.getInterfaceList().getInOutVars(),
+		ECollections.setEList(interfaceList.getInOutVars(),
 				source.getInOutParameters().stream().map(VarDeclaration.class::cast).map(EcoreUtil::copy).toList());
 		if (source.getReturnType() != null) {
 			final VarDeclaration returnVar = LibraryElementFactory.eINSTANCE.createVarDeclaration();
 			returnVar.setName(""); //$NON-NLS-1$
 			returnVar.setType(source.getReturnType());
-			dest.getInterfaceList().getOutputVars().add(0, returnVar);
+			interfaceList.getOutputVars().add(0, returnVar);
 		}
+		addWiths(interfaceList.getEventInputs().get(0),
+				Stream.concat(interfaceList.getInputVars().stream(), interfaceList.getInOutVars().stream()));
+		addWiths(interfaceList.getEventOutputs().get(0),
+				Stream.concat(interfaceList.getOutputVars().stream(), interfaceList.getOutMappedInOutVars().stream()));
+	}
+
+	protected static Event createEvent(final String name, final boolean input) {
+		final Event event = LibraryElementFactory.eINSTANCE.createEvent();
+		event.setName(name);
+		event.setType(EventTypeLibrary.getInstance().getType(null));
+		event.setIsInput(input);
+		return event;
+	}
+
+	protected static void addWiths(final Event event, final Stream<VarDeclaration> withs) {
+		withs.forEach(variable -> addWith(event, variable));
+	}
+
+	private static With addWith(final Event event, final VarDeclaration variable) {
+		final With with = LibraryElementFactory.eINSTANCE.createWith();
+		event.getWith().add(with);
+		with.setVariables(variable);
+		return with;
 	}
 
 	protected static void merge(final FunctionFBType dest, final ICallable source) {
