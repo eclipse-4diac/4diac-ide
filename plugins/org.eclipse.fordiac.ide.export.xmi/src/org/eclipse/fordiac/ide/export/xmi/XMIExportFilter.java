9bd83a4ac1c1b7cd5c973ce9ae0c2756140cb627
Martin Jobst <martin.jobst@posteo.de>
2023-06-30 22:24:14 +0200

    Bug 582077 - Add function FB type in XMI export
    Change-Id: I0a9cda6bb25bc25fc17d772a0279c7a380f1255a

--- plugins/org.eclipse.fordiac.ide.export.xmi/src/org/eclipse/fordiac/ide/export/xmi/XMIExportFilter.java
+++ plugins/org.eclipse.fordiac.ide.export.xmi/src/org/eclipse/fordiac/ide/export/xmi/XMIExportFilter.java
@@ -37,6 +37,7 @@ import org.eclipse.fordiac.ide.structuredtextcore.parsetree.reconstr.STCoreComme
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.STInitializerExpressionSource;
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.STSource;
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.util.STCoreUtil;
+import org.eclipse.fordiac.ide.structuredtextfunctioneditor.ui.resource.STFunctionResourceSetInitializer;
 import org.eclipse.fordiac.ide.xmiexport.xmiexport.XMIExportFactory;
 import org.eclipse.fordiac.ide.xmiexport.xmiexport.XMIExportInitialValue;
 import org.eclipse.fordiac.ide.xmiexport.xmiexport.XMIExportInitialValues;
@@ -68,6 +69,7 @@ public class XMIExportFilter extends ExportFilter {
 
 		final XtextResourceSet resourceSet = new XtextResourceSet();
 		new STAlgorithmResourceSetInitializer().initialize(resourceSet, typeFile.getProject());
+		new STFunctionResourceSetInitializer().initialize(resourceSet, typeFile.getProject());
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 
 		final Resource resource = resourceSet.getResource(sourceUri, true);
