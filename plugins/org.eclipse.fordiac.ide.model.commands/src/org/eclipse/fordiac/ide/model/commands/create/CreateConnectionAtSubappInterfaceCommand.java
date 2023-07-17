c003bd1235077bc1cd1b93de959b80dfb04d827b
fabio.gandolfi <fabio.gandolfi@jku.at>
2023-07-17 14:51:48 +0200

    Bug 582209 - Create a SubApp interface pin via context menu
    create a subapp interface pin and its connection via the context menu of
    a pin of an block contained in a subapp
    
    Change-Id: I57869aa023643478f149aad50ba268e5b6fdc3d1
    Signed-off-by: fabio.gandolfi <fabio.gandolfi@jku.at>

---
+++ plugins/org.eclipse.fordiac.ide.model.commands/src/org/eclipse/fordiac/ide/model/commands/create/CreateConnectionAtSubappInterfaceCommand.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Fabio Gandolfi - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.model.commands.create;
+
+import org.eclipse.fordiac.ide.model.data.EventType;
+import org.eclipse.fordiac.ide.model.libraryElement.AdapterType;
+import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
+import org.eclipse.fordiac.ide.model.libraryElement.SubApp;
+import org.eclipse.gef.commands.Command;
+
+public class CreateConnectionAtSubappInterfaceCommand extends Command {
+
+	CreateInterfaceElementCommand createIECmdCommand;
+	AbstractConnectionCreateCommand createConCommand;
+	IInterfaceElement ie;
+	SubApp subApp;
+
+	public CreateConnectionAtSubappInterfaceCommand(final IInterfaceElement ie, final SubApp subApp) {
+		this.ie = ie;
+		this.subApp = subApp;
+	}
+
+	@Override
+	public void execute() {
+		createIECmdCommand = new CreateInterfaceElementCommand(ie.getType(), ie.getName(), subApp.getInterface(),
+				ie.isIsInput(), -1);
+
+		if (ie.getType() instanceof EventType) {
+			createConCommand = new EventConnectionCreateCommand(subApp.getSubAppNetwork());
+		} else if (ie.getType() instanceof AdapterType) {
+			createConCommand = new AdapterConnectionCreateCommand(subApp.getSubAppNetwork());
+		} else {
+			createConCommand = new DataConnectionCreateCommand(subApp.getSubAppNetwork());
+		}
+
+		if (createIECmdCommand.canExecute()) {
+			createIECmdCommand.execute();
+
+			if (ie.isIsInput()) {
+				createConCommand.setSource(createIECmdCommand.getCreatedElement());
+				createConCommand.setDestination(ie);
+			} else {
+				createConCommand.setDestination(createIECmdCommand.getCreatedElement());
+				createConCommand.setSource(ie);
+			}
+
+			if (createConCommand.canExecute()) {
+				createConCommand.execute();
+			} else {
+				createIECmdCommand.canUndo();
+			}
+		}
+	}
+
+	@Override
+	public void undo() {
+		createConCommand.undo();
+		createIECmdCommand.undo();
+	}
+
+	@Override
+	public void redo() {
+		createIECmdCommand.redo();
+		createConCommand.redo();
+	}
+
+}
