303df34a8588fa0d0e8cae8284f98353bad49b39
Prankur Agarwal <prankur.agarwal@jku.at>
2023-08-23 16:23:15 +0200

    Bug 581681 - shape of hidden pin indicator
    This commit fixes the shape of the triangle to be displayed.
    
    Change-Id: I3744d401d52758288a3da2c8d754d19c07ee8b16

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/editparts/HiddenPinIndicatorEditPart.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/editparts/HiddenPinIndicatorEditPart.java
@@ -13,21 +13,82 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.editparts;
 
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.Triangle;
+import org.eclipse.draw2d.Shape;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.fordiac.ide.ui.preferences.PreferenceConstants;
 import org.eclipse.fordiac.ide.ui.preferences.PreferenceGetter;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
 public class HiddenPinIndicatorEditPart extends AbstractGraphicalEditPart {
+	public static final class FordiacTriangle extends Shape {
+
+		private boolean isInput = true;
+
+		public FordiacTriangle(final boolean isInput) {
+			this.isInput = isInput;
+		}
+
+		/** The points of the triangle. */
+		private final PointList triangle = new PointList(3);
+
+		/** @see Shape#fillShape(Graphics) */
+		@Override
+		protected void fillShape(final Graphics g) {
+			g.fillPolygon(triangle);
+		}
+
+		/** @see Shape#outlineShape(Graphics) */
+		@Override
+		protected void outlineShape(final Graphics g) {
+			g.drawPolygon(triangle);
+		}
+
+		/** @see Figure#primTranslate(int, int) */
+		@Override
+		public void primTranslate(final int dx, final int dy) {
+			super.primTranslate(dx, dy);
+			triangle.translate(dx, dy);
+		}
+
+		/** @see IFigure#validate() */
+		@Override
+		public void validate() {
+			super.validate();
+			final Rectangle r = new Rectangle();
+			r.setBounds(getBounds());
+			r.crop(getInsets());
+			r.resize(-1, -1);
+			int size = Math.min(r.height, r.width);
+			size = Math.max(size, 1); // Size cannot be negative
+			final Point head, p2, p3;
+			if (isInput) {
+				head = new Point(r.x, r.y);
+				p2 = new Point(head.x, head.y + size);
+				p3 = new Point(head.x + size, head.y + size);
+			} else {
+				head = new Point(r.x + size, r.y);
+				p2 = new Point(head.x, head.y + size);
+				p3 = new Point(head.x - size, head.y + size);
+			}
+			triangle.removeAllPoints();
+			triangle.addPoint(head);
+			triangle.addPoint(p2);
+			triangle.addPoint(p3);
+		}
+	}
 
 	@Override
 	protected IFigure createFigure() {
-		final Triangle triangle = new Triangle();
+		final FordiacTriangle triangle = new FordiacTriangle(((HiddenPinIndicator) super.getModel()).isInput());
 		triangle.setBorder(null);
-		triangle.setBounds(new Rectangle(0, 0, 15, 8));
+		triangle.setBounds(new Rectangle(0, 0, 8, 8));
 		triangle.setBackgroundColor(PreferenceGetter.getColor(PreferenceConstants.P_ANY_INT_CONNECTOR_COLOR));
+		triangle.setOutline(false);
 		return triangle;
 	}
 
