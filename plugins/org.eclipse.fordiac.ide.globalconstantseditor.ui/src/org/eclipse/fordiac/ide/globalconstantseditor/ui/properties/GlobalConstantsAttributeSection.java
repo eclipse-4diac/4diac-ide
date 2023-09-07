afd37c66b5b4cb66f6c1c7b0c3f66f08fcda3441
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2023-09-07 11:31:57 +0200

    Bug 582362 - Added AttributeSection to GlobalConstants
    Created new AttributeSection for GlobalConstants
    Only added section, attributes are not getting saved correctly
    
    Change-Id: I67ba3e12b53b39b0c7eff5c17f92eb2992d2aff6
    Signed-off-by: Sebastian Hollersbacher <s.hollersbacher@outlook.at>

---
+++ plugins/org.eclipse.fordiac.ide.globalconstantseditor.ui/src/org/eclipse/fordiac/ide/globalconstantseditor/ui/properties/GlobalConstantsAttributeSection.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Johannes Kepler University
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Sebastian Hollersbacher - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+package org.eclipse.fordiac.ide.globalconstantseditor.ui.properties;
+
+import org.eclipse.fordiac.ide.gef.properties.AttributeSection;
+import org.eclipse.fordiac.ide.globalconstantseditor.ui.document.GlobalConstantsDocument;
+import org.eclipse.fordiac.ide.globalconstantseditor.ui.editor.GlobalConstantsEditor;
+import org.eclipse.fordiac.ide.model.libraryElement.ConfigurableObject;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class GlobalConstantsAttributeSection extends AttributeSection {
+	@Override
+	protected ConfigurableObject getInputType(final Object input) {
+		if (input instanceof final GlobalConstantsEditor editor
+				&& editor.getDocument() instanceof final GlobalConstantsDocument document
+				&& document.getResourceLibraryElement() instanceof final ConfigurableObject configurableObject) {
+			return configurableObject;
+		}
+		return null;
+	}
+
+	@Override
+	public void setInput(final IWorkbenchPart part, final ISelection selection) {
+		commandStack = getCommandStack(part, null);
+		if (null == commandStack) {
+			setInputCode();
+		}
+		setType(part);
+		setInputInit();
+	}
+}
