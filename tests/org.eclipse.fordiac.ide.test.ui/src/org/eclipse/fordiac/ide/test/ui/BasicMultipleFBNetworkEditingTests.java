6ca384ff712de97cc6ad2c56748cac3c9a3663a1
Andrea Zoitl <andrea.f.zoitl@gmail.com>
2023-09-12 17:11:14 +0200

    Bug 582300 - Created BasicMultipleFBNetworkEditingTests class
    Creation of BasicMultipleFBNetworkEditingTests class and collected some
    ideas for future tests.
    
    Change-Id: Ifdea7e1175af96578c4d2ad1c8fcfde5225ff70a

---
+++ tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/BasicMultipleFBNetworkEditingTests.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Andrea Zoitl
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Andrea Zoitl - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.test.ui;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class BasicMultipleFBNetworkEditingTests extends Abstract4diacUITests {
+
+	/**
+	 * Drag and drops a few FBs of the same kind onto the canvas and checks if new
+	 * FBs gets the next free running number.
+	 *
+	 */
+	@Disabled("until implementation")
+	@Test
+	public void checkIfNewFbGetsNextFreeRunningNumber() {
+		// in progress
+	}
+
+	/**
+	 * Drag and drops a several (e.g. 5) FBs of the same type onto the canvas, than
+	 * deletes 2 or 3 and drags again FBs of the same type onto the canvas. Checks
+	 * that the new FBs get the smallest free running numbers.
+	 *
+	 */
+	@Disabled("until implementation")
+	@Test
+	public void checkIfNewFbGetsSmallestNextFreeRunningNumber() {
+		// in progress
+	}
+
+	/**
+	 * Create Blinky Test from Tutorial
+	 */
+	@Disabled("until implementation")
+	@Test
+	public void createBinkyTestApp() {
+		// in progress
+	}
+
+}
