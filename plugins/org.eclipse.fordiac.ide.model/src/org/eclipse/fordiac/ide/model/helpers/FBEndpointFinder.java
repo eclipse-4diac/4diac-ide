93021d64bff4f5bc7d56b8414421db0fdd118310
Lukas Leimeister <lukas.leimeister@gmx.at>
2023-10-25 09:31:00 +0200

    [Cleanup] replace .collect() with .tolist()
    Change-Id: I9551f7ccb15dea1a0de79a491e702bda590c6729

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/helpers/FBEndpointFinder.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/helpers/FBEndpointFinder.java
@@ -21,7 +21,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.eclipse.emf.common.util.EList;
@@ -395,7 +394,7 @@ public class FBEndpointFinder {
 
 		// trace find right interface from the interface stack
 		for (final IInterfaceElement structMem : ((StructManipulator) fb).getStructType().getMemberVariables().stream()
-				.filter(mem -> fb.getInterfaceElement(mem.getName()) != null).collect(Collectors.toList())) {
+				.filter(mem -> fb.getInterfaceElement(mem.getName()) != null).toList()) {
 			final IInterfaceElement realInt = fb.getInterfaceElement(structMem.getName());
 
 			Deque<String> subStack;
