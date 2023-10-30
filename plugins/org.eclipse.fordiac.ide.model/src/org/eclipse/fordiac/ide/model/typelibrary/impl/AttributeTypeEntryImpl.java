4c4218e2871c68a142a9e83796ede3566a172335
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2023-10-30 14:23:46 +0100

    Bug 582590 - Added AttributeTypeEntry
    Change-Id: I2afeb89510ddeed52682cb32d12c86777d14c82b
    Signed-off-by: Sebastian Hollersbacher <s.hollersbacher@outlook.at>

---
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/impl/AttributeTypeEntryImpl.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Johannes Kepler University, Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Sebastian Hollersbacher - initial API and implementation and/or initial documentation
+ ******************************************************************************/
+package org.eclipse.fordiac.ide.model.typelibrary.impl;
+
+import org.eclipse.fordiac.ide.model.dataexport.AbstractTypeExporter;
+import org.eclipse.fordiac.ide.model.dataimport.CommonElementImporter;
+import org.eclipse.fordiac.ide.model.libraryElement.AttributeDeclaration;
+import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
+import org.eclipse.fordiac.ide.model.typelibrary.AttributeTypeEntry;
+import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
+
+public class AttributeTypeEntryImpl extends AbstractTypeEntryImpl implements AttributeTypeEntry {
+
+	@Override
+	public synchronized AttributeDeclaration getType() {
+		final LibraryElement type = super.getType();
+		if (type instanceof final AttributeDeclaration attributeDeclaration) {
+			return attributeDeclaration;
+		}
+		return null;
+	}
+
+	@Override
+	public synchronized AttributeDeclaration getTypeEditable() {
+		final LibraryElement type = super.getTypeEditable();
+		if (type instanceof final AttributeDeclaration attributeDeclaration) {
+			return attributeDeclaration;
+		}
+		return null;
+	}
+
+	@Override
+	public synchronized void setType(final LibraryElement type) {
+		if (type instanceof AttributeDeclaration) {
+			super.setType(type);
+		} else {
+			super.setType(null);
+			if (null != type) {
+				FordiacLogHelper.logError("tried to set no AttributeDeclaration as type entry for AttributeTypeEntry"); //$NON-NLS-1$
+			}
+		}
+	}
+
+	@Override
+	protected CommonElementImporter getImporter() {
+		return null; // TODO add Importer, new AttributeTypeImporter(getFile());
+	}
+
+	@Override
+	protected AbstractTypeExporter getExporter() {
+		return null; // TODO add Exporter, new AttributeTypeExporter(getTypeEditable());
+	}
+
+}
