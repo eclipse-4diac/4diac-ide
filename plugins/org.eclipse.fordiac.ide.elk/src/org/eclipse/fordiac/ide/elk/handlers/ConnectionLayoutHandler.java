27ae1d71e4409a78f40d669990fe7f4e223ec0c4
Daniel Lindhuber <daniel.lindhuber@jku.at>
2024-01-22 14:05:09 +0100

    Bug 582788 - reworked connection layout for expanded subapps
    - separate handler for expanded subapps
    - catches exceptions of expanded subapps, allowing the layout to finish
    
    In the future, the additional layout handler is meant to layout the
    connections to expanded interfaces.
    
    Change-Id: I840901ccf395c2b36606f204b8ff9544a84664b7

--- plugins/org.eclipse.fordiac.ide.elk/src/org/eclipse/fordiac/ide/elk/handlers/ConnectionLayoutHandler.java
+++ plugins/org.eclipse.fordiac.ide.elk/src/org/eclipse/fordiac/ide/elk/handlers/ConnectionLayoutHandler.java
@@ -19,21 +19,15 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.elk.alg.libavoid.server.LibavoidServerException;
-import org.eclipse.elk.core.RecursiveGraphLayoutEngine;
-import org.eclipse.elk.core.util.BasicProgressMonitor;
-import org.eclipse.fordiac.ide.application.editparts.UnfoldedSubappContentEditPart;
 import org.eclipse.fordiac.ide.elk.FordiacLayoutData;
 import org.eclipse.fordiac.ide.elk.Messages;
-import org.eclipse.fordiac.ide.elk.commands.ConnectionLayoutCommand;
 import org.eclipse.fordiac.ide.elk.connection.ConnectionLayoutMapping;
 import org.eclipse.fordiac.ide.elk.connection.ConnectionRoutingHelper;
-import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class ConnectionLayoutHandler extends AbstractLayoutHandler {
+public class ConnectionLayoutHandler extends AbstractConnectionLayoutHandler {
 
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
@@ -50,56 +44,6 @@ public class ConnectionLayoutHandler extends AbstractLayoutHandler {
 			}
 		}
 		return Status.OK_STATUS;
-	}
-
-	private static void executeCommand(final ExecutionEvent event, final IWorkbenchPart part,
-			final FordiacLayoutData data) {
-		final ConnectionLayoutCommand cmd = new ConnectionLayoutCommand(data);
-		if (isAutomaticLayout(event)) {
-			// passes the cmd back to the editor to be handled there
-			((Event) event.getTrigger()).data = cmd;
-		} else {
-			part.getAdapter(CommandStack.class).execute(cmd);
-		}
-	}
-
-	private static boolean isAutomaticLayout(final ExecutionEvent event) {
-		// widget is in case of manual activation the menu item
-		// the automatic execution is programmatic and does therefore not have a widget
-		return event.getTrigger() instanceof final Event ev && ev.widget == null;
-	}
-
-	public static void runSubapps(final ConnectionLayoutMapping mapping, final FordiacLayoutData data) {
-		for (final UnfoldedSubappContentEditPart subapp : mapping.getExpandedSubapps()) {
-			final ConnectionLayoutMapping subappMapping = run(subapp);
-			final FordiacLayoutData subappData = ConnectionRoutingHelper.calculateConnections(subappMapping);
-			runSubapps(subappMapping, subappData);
-			// combine the recursive subapp runs
-			data.getConnectionPoints().addAll(subappData.getConnectionPoints());
-		}
-	}
-
-	private static ConnectionLayoutMapping run(final Object parent) {
-		final ConnectionLayoutMapping mapping = getLayoutMapping(parent);
-
-		if (mapping != null && mapping.hasNetwork()) {
-			// build graph
-			ConnectionRoutingHelper.buildGraph(mapping);
-			// layout graph
-			new RecursiveGraphLayoutEngine().layout(mapping.getLayoutGraph(), new BasicProgressMonitor());
-		}
-
-		return mapping;
-	}
-
-	private static ConnectionLayoutMapping getLayoutMapping(final Object parent) {
-		if (parent instanceof final IWorkbenchPart wbp) {
-			return new ConnectionLayoutMapping(wbp);
-		}
-		if (parent instanceof final UnfoldedSubappContentEditPart unfSubAppContentEP) {
-			return new ConnectionLayoutMapping(unfSubAppContentEP);
-		}
-		return null;
 	}
 
 }
