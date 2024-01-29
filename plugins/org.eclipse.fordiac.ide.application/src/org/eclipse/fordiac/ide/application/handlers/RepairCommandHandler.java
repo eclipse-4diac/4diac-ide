316adf89ad0c20f366cefedc731e97ca065aa8f9
darioromano <dario.romano@jku.at>
2024-01-29 15:33:16 +0100

    Bug 581913 - Restrict project selection in newTypeDialog
    restrict dialog for pre-set usage.
    
    Change-Id: Ibd5ef7a472c2fa4b6e05ba4837d8c62ac9f2e0d1

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/RepairCommandHandler.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/RepairCommandHandler.java
@@ -24,6 +24,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.fordiac.ide.application.editparts.ErrorMarkerFBNEditPart;
 import org.eclipse.fordiac.ide.gef.editparts.AbstractConnectableEditPart;
 import org.eclipse.fordiac.ide.model.libraryElement.ErrorMarkerDataType;
+import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.fordiac.ide.model.libraryElement.impl.ErrorMarkerDataTypeImpl;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeEntry;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
@@ -41,6 +42,8 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
@@ -59,9 +62,9 @@ public class RepairCommandHandler extends AbstractHandler {
 		editor = HandlerUtil.getActiveEditor(event);
 		final AbstractConnectableEditPart editPart = getEditPartFromSelection(sel);
 		typeLib = getTypeLibraryFromEditorInput(editor.getEditorInput());
-
-		repairEditPart(editPart);
-
+		if (editPart != null) {
+			repairEditPart(editPart, typeLib.getProject().getName());
+		}
 		typeLib.reload();
 		return null;
 	}
@@ -70,15 +73,16 @@ public class RepairCommandHandler extends AbstractHandler {
 		return TypeLibraryManager.INSTANCE.getTypeEntryForFile(((FileEditorInput) input).getFile()).getTypeLibrary();
 	}
 
-	private void repairEditPart(final AbstractConnectableEditPart editPart) {
+	private void repairEditPart(final AbstractConnectableEditPart editPart, final String projectName) {
 		final ErrorMarkerDataType dataTypeMarker = editPart.getAdapter(ErrorMarkerDataTypeImpl.class);
 		if (dataTypeMarker != null) {
-			showRestrictedWizard(dataTypeMarker.getName(), TypeLibraryTags.DATA_TYPE_FILE_ENDING_WITH_DOT);
+			showRestrictedWizard(dataTypeMarker.getTypeEntry().getTypeName(),
+					TypeLibraryTags.DATA_TYPE_FILE_ENDING_WITH_DOT, projectName);
 		}
 		final ErrorMarkerFBNEditPart fBNEditPartMarker = editPart.getAdapter(ErrorMarkerFBNEditPart.class);
 		if (fBNEditPartMarker != null) {
-			showRestrictedWizard(fBNEditPartMarker.getINamedElement().getName(),
-					TypeLibraryTags.FB_TYPE_FILE_ENDING_WITH_DOT);
+			showRestrictedWizard(((FBNetworkElement) fBNEditPartMarker.getINamedElement()).getTypeName(),
+					TypeLibraryTags.FB_TYPE_FILE_ENDING_WITH_DOT, projectName);
 		}
 	}
 
@@ -89,7 +93,7 @@ public class RepairCommandHandler extends AbstractHandler {
 		return part.isPresent() ? part.get() : null;
 	}
 
-	private void showRestrictedWizard(final String name, final String fileEnding) {
+	private void showRestrictedWizard(final String name, final String fileEnding, final String projectName) {
 		final NewTypeWizard wizard = new NewTypeWizard() {
 			@Override
 			protected NewFBTypeWizardPage createNewFBTypeWizardPage() {
@@ -102,21 +106,20 @@ public class RepairCommandHandler extends AbstractHandler {
 		final NewFBTypeWizardPage page = (NewFBTypeWizardPage) dialog.getCurrentPage();
 		page.setTemplateFileFilter(pathname -> pathname.getName().toUpperCase().endsWith(fileEnding));
 		page.setFileName(name);
-		restrictName(page.getControl());
 
 		final TableViewer tv = page.getTemplateViewer();
 		final Object selection = tv.getElementAt(0);
+
 		// have to check if any template at all could be found
 		if (selection != null) {
 			tv.setSelection(new StructuredSelection(selection), true);
-			// TODO Check out of you can find the right children via composites.
-			// project pre via go into button code
-
 			// unlock table if there are possible selections
 			if (tv.getElementAt(1) == null) {
 				tv.getTable().setEnabled(false);
 			}
 		}
+		restrictName(page.getControl());
+		restrictProjectSelection(page.getControl(), projectName);
 		if (Window.OK == dialog.open()) {
 			// the type could be created new execute the command
 			final TypeEntry newEntry = wizard.getTypeEntry();
@@ -138,6 +141,20 @@ public class RepairCommandHandler extends AbstractHandler {
 			.forEach(c -> c.setEnabled(false));
 		//@formatter:on
 
+	}
+
+	private static void restrictProjectSelection(final Control control, final String projectName) {
+		final Composite root = (Composite) control;
+		final List<Control> elements = getFlattenedList(root);
+		//@formatter:off
+		elements.stream()
+			.filter(Tree.class::isInstance)
+			.map(Tree.class::cast) //get the tree
+			.flatMap(a-> Arrays.stream(a.getItems())) //get all root elements
+			.filter(i -> !i.getText().equals(projectName)) //find all elements which are not the current project
+			.forEach(TreeItem::dispose); //dispose them, so only the current project is left
+		//@formatter:on
+		control.redraw();
 	}
 
 	private static List<Control> getFlattenedList(final Composite root) {
