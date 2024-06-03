f9f07838529ec2f7b20888dbed770f56ce9f532d
Mario Kastner <mario.kastner@jku.at>
2024-06-03 09:30:38 +0200

    add device restart option to ui

---
+++ plugins/org.eclipse.fordiac.ide.deployment.debug.ui/src/org/eclipse/fordiac/ide/deployment/debug/ui/handler/RestartDeviceHandler.java
@@ -0,0 +1,21 @@
+
+package org.eclipse.fordiac.ide.deployment.debug.ui.handler;
+
+import org.eclipse.fordiac.ide.deployment.exceptions.DeploymentException;
+import org.eclipse.fordiac.ide.deployment.interactors.IDeviceManagementInteractor;
+
+public class RestartDeviceHandler extends AbstractDeviceDeploymentCommand {
+
+	@Override
+	protected void executeCommand(final IDeviceManagementInteractor executor) throws DeploymentException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String getErrorMessageHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
