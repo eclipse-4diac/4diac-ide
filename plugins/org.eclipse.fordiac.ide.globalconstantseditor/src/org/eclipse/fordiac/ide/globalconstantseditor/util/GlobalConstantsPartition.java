e9974f27656b84209fed4c9531adbc0de36f62d4
Martin Jobst <martin.jobst@posteo.de>
2024-07-01 14:13:44 +0200

    Refactor partitioners in ST editor
    Also fix missing imports and FQN handling in recovered function and
    global constants sources.

--- plugins/org.eclipse.fordiac.ide.globalconstantseditor/src/org/eclipse/fordiac/ide/globalconstantseditor/util/GlobalConstantsPartition.java
+++ plugins/org.eclipse.fordiac.ide.globalconstantseditor/src/org/eclipse/fordiac/ide/globalconstantseditor/util/GlobalConstantsPartition.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Martin Erich Jobst
+ * Copyright (c) 2023, 2024 Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,24 +12,24 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.globalconstantseditor.util;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
-import org.eclipse.emf.common.util.ECollections;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.fordiac.ide.model.libraryElement.Import;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 import org.eclipse.fordiac.ide.structuredtextcore.util.STCorePartition;
 
 public class GlobalConstantsPartition extends STCorePartition {
-	private final EList<VarDeclaration> constants;
+	private final List<VarDeclaration> constants;
 
-	public GlobalConstantsPartition(final String packageName, final EList<Import> imports, final String originalSource,
-			final EList<VarDeclaration> constants) {
+	public GlobalConstantsPartition(final String packageName, final List<Import> imports, final String originalSource,
+			final List<VarDeclaration> constants) {
 		super(packageName, imports, originalSource);
-		this.constants = Objects.requireNonNullElseGet(constants, ECollections::emptyEList);
+		this.constants = Objects.requireNonNullElseGet(constants, Collections::emptyList);
 	}
 
-	public EList<VarDeclaration> getConstants() {
+	public List<VarDeclaration> getConstants() {
 		return constants;
 	}
 }
--- plugins/org.eclipse.fordiac.ide.structuredtextcore/src/org/eclipse/fordiac/ide/structuredtextcore/util/STCorePartition.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextcore/src/org/eclipse/fordiac/ide/structuredtextcore/util/STCorePartition.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Martin Erich Jobst
+ * Copyright (c) 2023, 2024 Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,21 +12,20 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.structuredtextcore.util;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
-import org.eclipse.emf.common.util.ECollections;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.fordiac.ide.model.libraryElement.Import;
 
 public class STCorePartition {
-
 	private final String packageName;
-	private final EList<Import> imports;
+	private final List<Import> imports;
 	private final String originalSource;
 
-	public STCorePartition(final String packageName, final EList<Import> imports, final String originalSource) {
+	public STCorePartition(final String packageName, final List<Import> imports, final String originalSource) {
 		this.packageName = packageName;
-		this.imports = Objects.requireNonNullElseGet(imports, ECollections::emptyEList);
+		this.imports = Objects.requireNonNullElseGet(imports, Collections::emptyList);
 		this.originalSource = originalSource;
 	}
 
@@ -34,7 +33,7 @@ public class STCorePartition {
 		return packageName;
 	}
 
-	public EList<Import> getImports() {
+	public List<Import> getImports() {
 		return imports;
 	}
 
