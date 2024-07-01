17c6b34b0ac4a8c9f176ffde9e09969f6537406b
Martin Jobst <martin.jobst@posteo.de>
2024-07-01 15:15:10 +0200

    Extract proxy name for unresolved data types in ST editor
    Attempt to extract proxy name and create corresponding error marker data
    types for unresolved data types in ST code.

--- plugins/org.eclipse.fordiac.ide.structuredtextcore/src/org/eclipse/fordiac/ide/structuredtextcore/util/STAbstractCorePartitioner.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextcore/src/org/eclipse/fordiac/ide/structuredtextcore/util/STAbstractCorePartitioner.java
@@ -16,8 +16,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.fordiac.ide.model.data.DataType;
 import org.eclipse.fordiac.ide.model.dataexport.CommonElementExporter;
+import org.eclipse.fordiac.ide.model.datatype.helper.IecTypes.GenericTypes;
 import org.eclipse.fordiac.ide.model.helpers.ArraySizeHelper;
 import org.eclipse.fordiac.ide.model.helpers.ImportHelper;
 import org.eclipse.fordiac.ide.model.helpers.PackageNameHelper;
@@ -27,12 +31,16 @@ import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
 import org.eclipse.fordiac.ide.model.libraryElement.Value;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
+import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
+import org.eclipse.fordiac.ide.model.typelibrary.TypeLibraryManager;
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.STCorePackage;
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.STImport;
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.STVarDeclaration;
 import org.eclipse.xtext.documentation.IEObjectDocumentationProvider;
+import org.eclipse.xtext.linking.lazy.LazyLinkingResource;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.util.Triple;
 
 import com.google.inject.Inject;
 
@@ -71,9 +79,7 @@ public abstract class STAbstractCorePartitioner<E extends INamedElement> impleme
 		if (comment != null) {
 			result.setComment(comment);
 		}
-		if (declaration.getType() instanceof final DataType type && !type.eIsProxy()) {
-			result.setType(type);
-		}
+		result.setType(resolveDataType(declaration.getType(), declaration));
 		if (declaration.isArray()) {
 			ArraySizeHelper.setArraySize(result, extractArraySize(declaration));
 		}
@@ -86,8 +92,7 @@ public abstract class STAbstractCorePartitioner<E extends INamedElement> impleme
 	}
 
 	protected static boolean isValidParameter(final STVarDeclaration declaration) {
-		return declaration.getName() != null && !declaration.getName().isEmpty()
-				&& declaration.getType() instanceof final DataType type && !type.eIsProxy();
+		return declaration.getName() != null && !declaration.getName().isEmpty();
 	}
 
 	protected static String extractArraySize(final STVarDeclaration declaration) {
@@ -99,6 +104,34 @@ public abstract class STAbstractCorePartitioner<E extends INamedElement> impleme
 		return NodeModelUtils
 				.findNodesForFeature(declaration, STCorePackage.eINSTANCE.getSTVarDeclaration_DefaultValue()).stream()
 				.map(INode::getText).collect(Collectors.joining()).trim();
+	}
+
+	protected static DataType resolveDataType(final INamedElement type, final EObject context) {
+		if (type != null && type.eIsProxy()) {
+			final String linkName = extractLinkName(type, context);
+			if (!linkName.isEmpty()) {
+				final TypeLibrary typeLibrary = TypeLibraryManager.INSTANCE.getTypeLibraryFromContext(context);
+				if (typeLibrary != null) {
+					return typeLibrary.getDataTypeLibrary().getType(linkName);
+				}
+			}
+		} else if (type instanceof final DataType dataType) {
+			return dataType;
+		}
+		return GenericTypes.ANY;
+	}
+
+	protected static String extractLinkName(final EObject proxy, final EObject context) {
+		return extractLinkName(EcoreUtil.getURI(proxy).fragment(), context);
+	}
+
+	protected static String extractLinkName(final String uriFragment, final EObject context) {
+		if (context.eResource() instanceof final LazyLinkingResource resource
+				&& resource.getEncoder().isCrossLinkFragment(resource, uriFragment)) {
+			final Triple<EObject, EReference, INode> triple = resource.getEncoder().decode(resource, uriFragment);
+			return resource.getLinkingHelper().getCrossRefNodeAsString(triple.getThird(), true);
+		}
+		return ""; //$NON-NLS-1$
 	}
 
 	protected static void generatePackage(final LibraryElement libraryElement, final StringBuilder builder) {
