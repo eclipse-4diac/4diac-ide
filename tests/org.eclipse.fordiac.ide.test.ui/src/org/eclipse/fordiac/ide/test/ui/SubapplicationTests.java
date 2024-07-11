066cad00b0ddead990a079232c057360ec54c206
Andrea Zoitl <andrea.f.zoitl@gmail.com>
2024-07-11 15:39:12 +0200

    Added test with Subapplication with existing connection outside FB
    Added a test where a subapplication is created with 2 FBs that have a
    connection outside the subapplication to another FB

--- tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/SubapplicationTests.java
+++ tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/SubapplicationTests.java
@@ -21,6 +21,7 @@ import java.util.List;
 import org.eclipse.fordiac.ide.application.editparts.FBEditPart;
 import org.eclipse.fordiac.ide.application.editparts.SubAppForFBNetworkEditPart;
 import org.eclipse.fordiac.ide.test.ui.swtbot.SWTBot4diacGefEditor;
+import org.eclipse.fordiac.ide.test.ui.swtbot.SWTBot4diacGefViewer;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
 import org.junit.jupiter.api.Test;
@@ -51,6 +52,44 @@ public class SubapplicationTests extends Abstract4diacUITests {
 				.map(p -> (FBEditPart) p.part()).anyMatch(fb -> E_SWITCH_FB.equals(fb.getModel().getName())));
 		assertTrue(selectedEditParts.stream().filter(p -> p.part() instanceof FBEditPart)
 				.map(p -> (FBEditPart) p.part()).anyMatch(fb -> E_SR_FB.equals(fb.getModel().getName())));
+		bot.menu(SOURCE).menu(NEW_SUBAPPLICATION).click();
+		// renew list of selectedEditParts and then check if SubApp was created
+		selectedEditParts = editor.selectedEditParts();
+		assertTrue(selectedEditParts.stream().filter(p -> p.part() instanceof SubAppForFBNetworkEditPart)
+				.map(p -> (SubAppForFBNetworkEditPart) p.part())
+				.anyMatch(fb -> SUBAPP.equals(fb.getModel().getName())));
+	}
+
+	/**
+	 * Checks whether a subapplication can be created when a connection to a not
+	 * selected FB exists.
+	 *
+	 * First, three FBs are draged&dropped onto the editing area and connections are
+	 * created. Then a rectangle is drawn over two FBs where a connection is to a FB
+	 * outside of the selection. Then a subapplication is created via menu entry
+	 * "Source" -> "New subapplication"
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void createSubappViaMenuWithExistingConnectionOutsideSubapp() {
+		dragAndDropEventsFB(E_CYCLE_TREE_ITEM, new Point(100, 100));
+		dragAndDropEventsFB(E_SWITCH_TREE_ITEM, new Point(300, 100));
+		dragAndDropEventsFB(E_SR_TREE_ITEM, new Point(500, 100));
+		final SWTBot4diacGefViewer viewer = createConnection(EO, EI);
+		assertDoesNotThrow(viewer::waitForConnection);
+
+		// drag rectangle over to FB, therefore FB should be selected
+		final SWTBot4diacGefEditor editor = (SWTBot4diacGefEditor) bot.gefEditor(PROJECT_NAME);
+		editor.drag(250, 50, 600, 200);
+		assertDoesNotThrow(() -> editor.waitForSelectedFBEditPart());
+		List<SWTBotGefEditPart> selectedEditParts = editor.selectedEditParts();
+		assertFalse(selectedEditParts.isEmpty());
+		assertTrue(selectedEditParts.stream().filter(p -> p.part() instanceof FBEditPart)
+				.map(p -> (FBEditPart) p.part()).anyMatch(fb -> E_SWITCH_FB.equals(fb.getModel().getName())));
+		assertTrue(selectedEditParts.stream().filter(p -> p.part() instanceof FBEditPart)
+				.map(p -> (FBEditPart) p.part()).anyMatch(fb -> E_SR_FB.equals(fb.getModel().getName())));
+		assertFalse(selectedEditParts.stream().filter(p -> p.part() instanceof FBEditPart)
+				.map(p -> (FBEditPart) p.part()).anyMatch(fb -> E_CYCLE_TREE_ITEM.equals(fb.getModel().getName())));
 		bot.menu(SOURCE).menu(NEW_SUBAPPLICATION).click();
 		// renew list of selectedEditParts and then check if SubApp was created
 		selectedEditParts = editor.selectedEditParts();
