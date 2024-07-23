3a968c82a0ea1fcb1dcb756d5cea410f1efbe521
pazi146 <pazi146@gmail.com>
2024-07-23 11:04:48 +0200

    Fixed GitLab downloader
    now automatically imports the downloaded project
    fixed logic with autoimport (|| vs &&)

--- plugins/org.eclipse.fordiac.ide.gitlab/src/org/eclipse/fordiac/ide/gitlab/wizard/GitLabImportWizard.java
+++ plugins/org.eclipse.fordiac.ide.gitlab/src/org/eclipse/fordiac/ide/gitlab/wizard/GitLabImportWizard.java
@@ -13,47 +13,47 @@
 package org.eclipse.fordiac.ide.gitlab.wizard;
 
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
 
 public class GitLabImportWizard extends Wizard implements IImportWizard {
-	
-	private GitLabImportWizardPage mainPage; 
-	private GitLabListedLibsPage listOfLibsPage;
 
-	public GitLabImportWizard() {
-		super();
-	}
+	private GitLabImportWizardPage mainPage;
+	private GitLabListedLibsPage listOfLibsPage;
+	private StructuredSelection selection;
 
 	// What happens after the "Finish" button has been clicked
+	@Override
 	public boolean performFinish() {
 		listOfLibsPage.finish();
 		// should be the last thing to be done
-		
-		return true; 
-	}
-	
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		setWindowTitle("File Import Wizard"); //NON-NLS-1
-		setNeedsProgressMonitor(true);
-		
-	}
-	
-    @Override
-    public void addPages() {
-        mainPage = new GitLabImportWizardPage("Import GitLab Files"); //NON-NLS-1
-        addPage(mainPage);
-        listOfLibsPage = new GitLabListedLibsPage("Results"); //NON-NLS-1
-        addPage(listOfLibsPage);
-    }
 
-    @Override
-    public IWizardPage getNextPage(IWizardPage page) {
-    	if(page == mainPage) {
-    		return listOfLibsPage;
-    	}
-    	return null;
-    }
+		return true;
+	}
+
+	@Override
+	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
+		setWindowTitle("File Import Wizard"); //$NON-NLS-1$
+		setNeedsProgressMonitor(true);
+		this.selection = new StructuredSelection(selection.toList());
+	}
+
+	@Override
+	public void addPages() {
+		mainPage = new GitLabImportWizardPage("Import GitLab Files"); //$NON-NLS-1$
+		addPage(mainPage);
+		listOfLibsPage = new GitLabListedLibsPage("Results", selection); //$NON-NLS-1$
+		addPage(listOfLibsPage);
+	}
+
+	@Override
+	public IWizardPage getNextPage(final IWizardPage page) {
+		if (page == mainPage) {
+			return listOfLibsPage;
+		}
+		return null;
+	}
 }
