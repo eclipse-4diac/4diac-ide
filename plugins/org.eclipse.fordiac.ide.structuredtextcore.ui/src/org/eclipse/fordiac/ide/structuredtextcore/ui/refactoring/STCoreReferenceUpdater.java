d8879be104cc6997c58c64c6978af3f4e46f04cf
Martin Jobst <martin.jobst@posteo.de>
2024-07-29 15:46:14 +0200

    Update names with different casing during rename refactoring

--- plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/refactoring/STCoreReferenceUpdater.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/refactoring/STCoreReferenceUpdater.java
@@ -12,9 +12,18 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.structuredtextcore.ui.refactoring;
 
+import org.eclipse.xtext.formatting2.regionaccess.ISemanticRegion;
+import org.eclipse.xtext.formatting2.regionaccess.ITextRegionDiffBuilder;
 import org.eclipse.xtext.ide.serializer.hooks.IReferenceUpdaterContext;
+import org.eclipse.xtext.ide.serializer.hooks.IUpdatableReference;
 import org.eclipse.xtext.ide.serializer.impl.ReferenceUpdater;
 import org.eclipse.xtext.ide.serializer.impl.RelatedResourcesProvider.RelatedResource;
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
+import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.scoping.IScope;
+import org.eclipse.xtext.scoping.IScopeProvider;
+import org.eclipse.xtext.serializer.tokens.SerializerScopeProviderBinding;
 
 import com.google.inject.Inject;
 
@@ -24,10 +33,37 @@ public class STCoreReferenceUpdater extends ReferenceUpdater {
 	@Inject
 	private STCoreImportUpdater importUpdater;
 
+	@Inject
+	private IQualifiedNameConverter nameConverter;
+
+	@Inject
+	@SerializerScopeProviderBinding
+	private IScopeProvider scopeProvider;
+
 	@Override
 	protected void updateExternalReferences(final IReferenceUpdaterContext context,
 			final RelatedResource relatedResource) {
 		super.updateExternalReferences(context, relatedResource);
 		context.modifyModel(() -> importUpdater.updateImports(context));
+	}
+
+	@Override
+	public void updateReference(final ITextRegionDiffBuilder rewriter, final IUpdatableReference upd) {
+		final IUpdatableReference updatable = upd;
+		if (rewriter.isModified(updatable.getReferenceRegion())) {
+			return;
+		}
+		final IScope scope = scopeProvider.getScope(updatable.getSourceEObject(), updatable.getEReference());
+		final ISemanticRegion region = updatable.getReferenceRegion();
+		final QualifiedName oldName = nameConverter.toQualifiedName(region.getText());
+		final IEObjectDescription oldDesc = scope.getSingleElement(oldName);
+		if (oldDesc != null && oldDesc.getEObjectOrProxy() == updatable.getTargetEObject()
+				&& oldDesc.getName().equals(oldName)) {
+			return;
+		}
+		final String newName = findValidName(updatable, scope);
+		if (newName != null && !newName.equals(region.getText())) {
+			rewriter.replace(region, newName);
+		}
 	}
 }
