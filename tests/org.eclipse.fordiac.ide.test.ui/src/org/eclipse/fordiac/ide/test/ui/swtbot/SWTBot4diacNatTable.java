32e4bd3418a34dd1674baf8a65ad10fc41f9aa9f
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2024-08-05 01:39:21 +0530

    Added Copyright header in SWTBot4diacNatTable class

--- tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/swtbot/SWTBot4diacNatTable.java
+++ tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/swtbot/SWTBot4diacNatTable.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Prashantkumar Khatri
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *
+ *   Prashantkumar Khatri - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.fordiac.ide.test.ui.swtbot;
 
 import org.eclipse.nebula.widgets.nattable.NatTable;
