b5cbee23ab87b7f9506e808ce1041b4cc569d88c
PStmmr <paul.stemmer@jku.at>
2024-08-06 15:33:11 +0200

    Added copyright headers

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/InfoPropertySection.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/InfoPropertySection.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Paul Stemmer - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.fordiac.ide.application.properties;
 
 import java.util.HashMap;
