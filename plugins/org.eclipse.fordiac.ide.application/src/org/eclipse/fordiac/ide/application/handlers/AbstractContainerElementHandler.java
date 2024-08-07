5f582e878463fd820d5cf085bc39f6c55bc95330
Mario Kastner <mario.kastner@jku.at>
2024-08-07 13:25:36 +0200

    Revert "add default value for spawn point"
    This reverts commit 9ac13678b23e7235cb2c33258e433b24b97b32c8.

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/AbstractContainerElementHandler.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/AbstractContainerElementHandler.java
@@ -22,7 +22,6 @@ import static org.eclipse.fordiac.ide.model.ui.editors.HandlerHelper.getViewer;
 import static org.eclipse.fordiac.ide.model.ui.editors.HandlerHelper.selectElement;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -167,9 +166,8 @@ abstract class AbstractContainerElementHandler extends AbstractHandler {
 	protected Rectangle getEmptyContainerPosSizeRef(final ExecutionEvent event, final EditPartViewer viewer,
 			final FBNetwork network) {
 		Rectangle selectionExtend;
-		final org.eclipse.swt.graphics.Point point = Optional
-				.ofNullable(((FBNetworkContextMenuProvider) viewer.getContextMenu()).getPoint())
-				.orElse(new org.eclipse.swt.graphics.Point(0, 0));
+		final org.eclipse.swt.graphics.Point point = ((FBNetworkContextMenuProvider) viewer.getContextMenu())
+				.getPoint();
 		selectionExtend = new Rectangle(point.x, point.y, 200, 100);
 		final IFigure targetFigure = getTargetFigure(event, viewer, network);
 		targetFigure.translateToRelative(selectionExtend);
