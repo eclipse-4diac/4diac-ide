9f4d2dd7bf17d730c7ab7c042ca2100d991aca30
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2024-08-07 15:24:30 +0200

    Test app generation now works for multiple files at once

---
+++ plugins/org.eclipse.fordiac.ide.fb.interpreter/src/CreateTestAppCommand.java
@@ -0,0 +1,19 @@
+
+/*******************************************************************************
+ * Copyright (c) 2024 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors: Bianca Wiesmayr - initial API and implementation and/or initial
+ * documentation
+ *******************************************************************************/
+
+import org.eclipse.gef.commands.Command;
+
+public class CreateTestAppCommand extends Command {
+
+}
