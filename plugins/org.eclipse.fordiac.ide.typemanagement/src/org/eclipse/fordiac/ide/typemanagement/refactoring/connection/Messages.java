16d615c60fb297eb3385605af7307c8ce6f32e5e
Gatsch <magarstenauer@gmail.com>
2024-08-08 10:11:35 +0200

    Added copyright headers / Corrected typemanagment Manifest

--- plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/refactoring/connection/Messages.java
+++ plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/refactoring/connection/Messages.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Mathias Garstenauer - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
 package org.eclipse.fordiac.ide.typemanagement.refactoring.connection;
 
 import org.eclipse.osgi.util.NLS;
