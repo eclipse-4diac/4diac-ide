e112b83b07d052196d21689c3b2998742b8605a0
Prashantkumar Khatri <khatri2105104@st.jmi.ac.in>
2024-08-13 18:02:20 +0530

    Var In&Outputs Tab Tests

---
+++ tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/fbtype/VarInAndOutputsTabTests.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Prashantkumar Khatri
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Prashantkumar Khatri - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.test.ui.fbtype;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.eclipse.fordiac.ide.model.datatype.helper.RetainHelper.RetainTag;
+import org.eclipse.fordiac.ide.test.ui.PropertySheetHelper;
+import org.eclipse.fordiac.ide.test.ui.swtbot.SWTBot4diacGefEditor;
+import org.eclipse.fordiac.ide.test.ui.swtbot.SWTBot4diacNatTable;
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.swtbot.eclipse.finder.matchers.WidgetMatcherFactory;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VarInAndOutputsTabTests extends NatTableWithEditorBehaviorTests {
+
+	@Override
+	@BeforeEach
+	public void operationsInitialization() {
+		TESTVAR1 = DI1;
+		TESTVAR2 = DI2;
+		TESTVAR3 = DI3;
+		createFBType(PROJECT_NAME, FBT_TEST_PROJECT2, ADAPTER);
+		openFBTypeInEditor(PROJECT_NAME, FBT_TEST_PROJECT2);
+		final SWTBot propertiesBot = selectTabFromInterfaceProperties(VAR_IN_AND_OUTPUTS);
+		assertNotNull(propertiesBot);
+		bot.viewByTitle(PROPERTIES_TITLE).setFocus();
+		bot.editorByTitle(FBT_TEST_PROJECT2).show();
+		PropertySheetHelper.selectPropertyTabItem(VAR_IN_AND_OUTPUTS, propertiesBot);
+		natTable = propertiesBot.widget(WidgetMatcherFactory.widgetOfType(NatTable.class), 0);
+		natTableBot = new SWTBot4diacNatTable(natTable);
+		editor = (SWTBot4diacGefEditor) bot.gefEditor(FBT_TEST_PROJECT2);
+		assertNotNull(editor);
+		NatTableHelper.createNewVariableInDataTypeEditor(natTableBot);
+	}
+
+	/**
+	 * Verifies the ability to change the initial value of a variable.
+	 *
+	 * This test checks whether the initial value of a variable can be changed, and
+	 * asserts that the new value is correctly assigned.
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void changeInitialValueOfVariable() {
+		NatTableHelper.changeCellValueInNatTbale(natTableBot, TRUE, 1, 4);
+		NatTableHelper.changeCellValueInNatTbale(natTableBot, "1", 1, 4); //$NON-NLS-1$
+	}
+
+	/**
+	 * Tests the behavior when an invalid initial value is assigned to a variable.
+	 *
+	 * This test attempts to assign an invalid initial value to a variable and
+	 * verifies whether the assignment is successful and what impact it has on the
+	 * editor.
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void tryToSetInValidInitialValue() {
+		NatTableHelper.setInvalidDataType(natTableBot, 1, 4, TESTVAR);
+	}
+
+	/**
+	 * Toggles the configuration setting of a variable.
+	 *
+	 * This test verifies the toggling behavior of a variable's configuration
+	 * setting, ensuring that the value switches between "true" and "false".
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void toggleVarConfiguration() {
+		assertEquals(natTableBot.getCellDataValueByPosition(2, 6), "false");
+		natTableBot.click(2, 6);
+		assertEquals(natTableBot.getCellDataValueByPosition(2, 6), "true");
+	}
+
+	/**
+	 * Toggles the visibility setting of a variable.
+	 *
+	 * This test checks the toggling behavior of a variable's visibility setting,
+	 * confirming that the value alternates between "true" and "false".
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void toggleVarVisibility() {
+		assertEquals(natTableBot.getCellDataValueByPosition(2, 5), "true");
+		natTableBot.click(2, 5);
+		assertEquals(natTableBot.getCellDataValueByPosition(2, 5), "false");
+	}
+
+	/**
+	 * Changes the retain tag of a variable.
+	 *
+	 * This test verifies that the retain tag of a variable can be changed
+	 * successfully and checks that the new retain tag value is correctly set in the
+	 * NatTable.
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	public void changeRetainTag() {
+		NatTableHelper.setRetainValue(natTableBot, 1, 7, RetainTag.RETAIN);
+	}
+}
