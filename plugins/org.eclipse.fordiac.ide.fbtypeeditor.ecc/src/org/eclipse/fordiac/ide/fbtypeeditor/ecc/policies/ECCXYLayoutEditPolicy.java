3b706bc3ae754dfe61b66daaf687e53153c85d4d
Mario Kastner <mario.kastner@jku.at>
2024-08-14 16:32:13 +0200

    fix alignment

--- plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/policies/ECCXYLayoutEditPolicy.java
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/policies/ECCXYLayoutEditPolicy.java
@@ -15,17 +15,21 @@ package org.eclipse.fordiac.ide.fbtypeeditor.ecc.policies;
 
 import org.eclipse.draw2d.geometry.Insets;
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.CreateECStateCommand;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.editparts.ECActionAlgorithmEditPart;
 import org.eclipse.fordiac.ide.fbtypeeditor.ecc.editparts.ECActionOutputEventEditPart;
 import org.eclipse.fordiac.ide.gef.policies.ModifiedNonResizeableEditPolicy;
 import org.eclipse.fordiac.ide.gef.utilities.RequestUtil;
+import org.eclipse.fordiac.ide.model.CoordinateConverter;
 import org.eclipse.fordiac.ide.model.commands.change.SetPositionCommand;
 import org.eclipse.fordiac.ide.model.libraryElement.ECC;
 import org.eclipse.fordiac.ide.model.libraryElement.ECState;
+import org.eclipse.fordiac.ide.model.libraryElement.Position;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.Request;
+import org.eclipse.gef.RequestConstants;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editparts.ScalableFreeformRootEditPart;
 import org.eclipse.gef.editparts.ZoomManager;
@@ -43,18 +47,35 @@ public class ECCXYLayoutEditPolicy extends XYLayoutEditPolicy {
 	@Override
 	protected Command createChangeConstraintCommand(final ChangeBoundsRequest request, final EditPart child,
 			final Object constraint) {
-		if ((child.getModel() instanceof ECState) && (RequestUtil.isMoveRequest(request))) {
-			final Point moveDelta = request.getMoveDelta().getScaled(1.0 / getZoomManager().getZoom());
-			final ECState state = (ECState) child.getModel();
+		if ((child.getModel() instanceof final ECState state) && RequestUtil.isMoveRequest(request)) {
+			final Point moveDelta;
+			if (constraint instanceof final Rectangle rect && isAlignmentRequest(request)) {
+				moveDelta = getMoveDelta(rect, state);
+			} else {
+				moveDelta = request.getMoveDelta().getScaled(1.0 / getZoomManager().getZoom());
+			}
+
 			return new SetPositionCommand(state, moveDelta.x, moveDelta.y);
 		}
 		return null;
 	}
 
+	private static boolean isAlignmentRequest(final Request command) {
+		return command.getType() == RequestConstants.REQ_ALIGN
+				|| command.getType() == RequestConstants.REQ_ALIGN_CHILDREN;
+	}
+
+	private static Point getMoveDelta(final Rectangle rect, final ECState state) {
+		final Position newPos = CoordinateConverter.INSTANCE.createPosFromScreenCoordinates(rect.x, rect.y);
+		final Position oldPos = state.getPosition();
+		newPos.setX(Math.floor((newPos.getX() - oldPos.getX()) / 2));
+		newPos.setY(Math.floor((newPos.getY() - oldPos.getY()) / 2));
+		return newPos.toScreenPoint();
+	}
+
 	@Override
 	protected Command getAddCommand(final Request generic) {
-		if (generic instanceof ChangeBoundsRequest) {
-			final ChangeBoundsRequest request = (ChangeBoundsRequest) generic;
+		if (generic instanceof final ChangeBoundsRequest request) {
 			for (final Object editPart : request.getEditParts()) {
 				if ((editPart instanceof ECActionAlgorithmEditPart)
 						|| (editPart instanceof ECActionOutputEventEditPart)) {
