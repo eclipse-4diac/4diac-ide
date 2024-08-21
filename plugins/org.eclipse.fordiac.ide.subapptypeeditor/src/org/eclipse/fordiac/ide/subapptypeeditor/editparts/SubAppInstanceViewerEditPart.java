77b2e3bf874e3914e55c2bce80c886ec30dcdd36
Alois Zoitl <alois.zoitl@gmx.at>
2024-08-21 21:50:33 +0200

    Show out mapped VarInOut also in SubAppInstance Viewers

--- plugins/org.eclipse.fordiac.ide.subapptypeeditor/src/org/eclipse/fordiac/ide/subapptypeeditor/editparts/SubAppInstanceViewerEditPart.java
+++ plugins/org.eclipse.fordiac.ide.subapptypeeditor/src/org/eclipse/fordiac/ide/subapptypeeditor/editparts/SubAppInstanceViewerEditPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 Primetals Technologies Austria GmbH
+ * Copyright (c) 2021, 2024 Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -10,13 +10,27 @@
  * Contributors:
  *   Michael Oberlehner
  *     - initial API and implementation and/or initial documentation
+ *   Alois Zoitl - added outMapped varInOuts to the model children
  *******************************************************************************/
 package org.eclipse.fordiac.ide.subapptypeeditor.editparts;
+
+import java.util.List;
 
 import org.eclipse.fordiac.ide.fbtypeeditor.network.viewer.CompositeNetworkViewerEditPart;
 import org.eclipse.gef.EditPart;
 
 public class SubAppInstanceViewerEditPart extends CompositeNetworkViewerEditPart {
+
+	@Override
+	protected List<?> getModelChildren() {
+		@SuppressWarnings("unchecked")
+		final List<Object> modelChildren = (List<Object>) super.getModelChildren();
+		if (getModel() != null) {
+			modelChildren.addAll(getInterfaceList().getOutMappedInOutVars());
+		}
+		return modelChildren;
+	}
+
 	@Override
 	protected boolean isVarVisible(final EditPart childEditPart) {
 		// for subapps all interface vars are visible
