2386e3cefde4c9e20640694d3bc232a06fa22145
Mario Kastner <mario.kastner@jku.at>
2024-08-23 08:36:40 +0200

    use correct bundle name

--- tests/org.eclipse.fordiac.ide.test.deployment/src/org/eclipse/fordiac/ide/test/resourcedeployment/ResourceDeploymentTest.java
+++ tests/org.eclipse.fordiac.ide.test.deployment/src/org/eclipse/fordiac/ide/test/resourcedeployment/ResourceDeploymentTest.java
@@ -86,7 +86,7 @@ class ResourceDeploymentTest {
 	}
 
 	private static EList<Resource> loadResources() throws CoreException, IOException {
-		final Bundle bundle = Platform.getBundle("org.eclipse.fordiac.ide.test.export"); //$NON-NLS-1$
+		final Bundle bundle = Platform.getBundle("org.eclipse.fordiac.ide.test.deployment"); //$NON-NLS-1$
 		final Path projectPath = new Path("data/ResourceDeploymentTest"); //$NON-NLS-1$
 		final FordiacProjectLoader loader = new FordiacProjectLoader(bundle, projectPath);
 		return loader.getAutomationSystem("ResourceDeploymentTest").getDeviceNamed("FORTE_PC").getResource(); //$NON-NLS-1$
