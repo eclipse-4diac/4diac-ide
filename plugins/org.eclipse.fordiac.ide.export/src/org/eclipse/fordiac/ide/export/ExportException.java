5f21915ed61ff3064cbbd99aaecc4ccf4909d9f7
Ernst Blecha <ernst.blecha@primetals.com>
2024-08-28 17:23:56 +0200

    add buttons for "Cancel All" and "Overwrite All" to the export

--- plugins/org.eclipse.fordiac.ide.export/src/org/eclipse/fordiac/ide/export/ExportException.java
+++ plugins/org.eclipse.fordiac.ide.export/src/org/eclipse/fordiac/ide/export/ExportException.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2009 Profactor GmbH
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -23,11 +23,41 @@ public class ExportException extends Exception {
 
 	/**
 	 * Instantiates a new export exception.
-	 * 
+	 *
 	 * @param message the message
 	 */
 	public ExportException(final String message) {
 		super(message);
+	}
+
+	public static class UserInteraction extends ExportException {
+
+		private static final long serialVersionUID = 1L;
+
+		public UserInteraction(final String message) {
+			super(message);
+		}
+
+	}
+
+	public static class OverwriteAll extends UserInteraction {
+
+		private static final long serialVersionUID = 1L;
+
+		public OverwriteAll() {
+			super(Messages.TemplateExportFilter_OVERWRITE_ALL_LABEL_STRING);
+		}
+
+	}
+
+	public static class CancelAll extends UserInteraction {
+
+		private static final long serialVersionUID = 1L;
+
+		public CancelAll() {
+			super(Messages.TemplateExportFilter_OVERWRITE_ALL_LABEL_STRING);
+		}
+
 	}
 
 }
