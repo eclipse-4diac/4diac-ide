ff431ffa9a03740b1d22ec76fbe166fd72b80390
Martin Jobst <martin.jobst@posteo.de>
2024-08-30 18:17:43 +0200

    Add support for nested structs and arrays to value converters

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/value/ValueConverterFactory.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/value/ValueConverterFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 Martin Erich Jobst
+ * Copyright (c) 2022, 2024 Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,8 +12,10 @@
  */
 package org.eclipse.fordiac.ide.model.value;
 
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.fordiac.ide.model.data.AnyDurationType;
 import org.eclipse.fordiac.ide.model.data.AnyNumType;
+import org.eclipse.fordiac.ide.model.data.ArrayType;
 import org.eclipse.fordiac.ide.model.data.BoolType;
 import org.eclipse.fordiac.ide.model.data.ByteType;
 import org.eclipse.fordiac.ide.model.data.CharType;
@@ -26,39 +28,57 @@ import org.eclipse.fordiac.ide.model.data.LdtType;
 import org.eclipse.fordiac.ide.model.data.LtodType;
 import org.eclipse.fordiac.ide.model.data.LwordType;
 import org.eclipse.fordiac.ide.model.data.StringType;
+import org.eclipse.fordiac.ide.model.data.StructuredType;
 import org.eclipse.fordiac.ide.model.data.TimeOfDayType;
 import org.eclipse.fordiac.ide.model.data.WcharType;
 import org.eclipse.fordiac.ide.model.data.WordType;
 import org.eclipse.fordiac.ide.model.data.WstringType;
+import org.eclipse.fordiac.ide.model.datatype.helper.TypeDeclarationParser;
+import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 
 public final class ValueConverterFactory {
 	public static ValueConverter<?> createValueConverter(final DataType type) {
-		if (type instanceof BoolType) {
-			return BoolValueConverter.INSTANCE;
-		} else if (type instanceof AnyNumType) {
-			return NumericValueConverter.INSTANCE;
-		} else if (type instanceof ByteType) {
-			return NumericValueConverter.INSTANCE_BYTE;
-		} else if (type instanceof WordType) {
-			return NumericValueConverter.INSTANCE_WORD;
-		} else if (type instanceof DwordType) {
-			return NumericValueConverter.INSTANCE_DWORD;
-		} else if (type instanceof LwordType) {
-			return NumericValueConverter.INSTANCE_LWORD;
-		} else if (type instanceof AnyDurationType) {
-			return TimeValueConverter.INSTANCE;
-		} else if (type instanceof CharType || type instanceof StringType) {
-			return StringValueConverter.INSTANCE;
-		} else if (type instanceof WcharType || type instanceof WstringType) {
-			return WStringValueConverter.INSTANCE;
-		} else if (type instanceof DateType || type instanceof LdateType) {
-			return DateValueConverter.INSTANCE;
-		} else if (type instanceof TimeOfDayType || type instanceof LtodType) {
-			return TimeOfDayValueConverter.INSTANCE;
-		} else if (type instanceof DateAndTimeType || type instanceof LdtType) {
-			return DateAndTimeValueConverter.INSTANCE;
+		return switch (type) {
+		case final BoolType unused -> BoolValueConverter.INSTANCE;
+		case final AnyNumType unused -> NumericValueConverter.INSTANCE;
+		case final ByteType unused -> NumericValueConverter.INSTANCE_BYTE;
+		case final WordType unused -> NumericValueConverter.INSTANCE_WORD;
+		case final DwordType unused -> NumericValueConverter.INSTANCE_DWORD;
+		case final LwordType unused -> NumericValueConverter.INSTANCE_LWORD;
+		case final AnyDurationType unused -> TimeValueConverter.INSTANCE;
+		case final CharType unused -> StringValueConverter.INSTANCE;
+		case final StringType unused -> StringValueConverter.INSTANCE;
+		case final WcharType unused -> WStringValueConverter.INSTANCE;
+		case final WstringType unused -> WStringValueConverter.INSTANCE;
+		case final DateType unused -> DateValueConverter.INSTANCE;
+		case final LdateType unused -> DateValueConverter.INSTANCE;
+		case final TimeOfDayType unused -> TimeOfDayValueConverter.INSTANCE;
+		case final LtodType unused -> TimeOfDayValueConverter.INSTANCE;
+		case final DateAndTimeType unused -> DateAndTimeValueConverter.INSTANCE;
+		case final LdtType unused -> DateAndTimeValueConverter.INSTANCE;
+		case final ArrayType arrayType -> new ArrayValueConverter<>(createValueConverter(getElementType(arrayType)));
+		case final StructuredType structuredType ->
+			new StructValueConverter(name -> createValueConverter(getMemberType(structuredType, name)));
+		case null, default -> null;
+		};
+	}
+
+	private static DataType getElementType(final ArrayType type) {
+		if (type.getSubranges().size() > 1) {
+			final ArrayType result = EcoreUtil.copy(type);
+			result.getSubranges().removeFirst();
+			return result;
 		}
-		return null;
+		return type.getBaseType();
+	}
+
+	private static DataType getMemberType(final StructuredType type, final String name) {
+		final VarDeclaration member = type.getMemberVariables().stream().filter(v -> name.equals(v.getName()))
+				.findFirst().orElseThrow();
+		if (member.isArray()) {
+			return TypeDeclarationParser.parseTypeDeclaration(member.getType(), member.getArraySize().getValue());
+		}
+		return member.getType();
 	}
 
 	private ValueConverterFactory() {
