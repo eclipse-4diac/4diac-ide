4c0a1c4ef2dda33bff10788226acbcdd8b3a50fd
Andrea Zoitl <andrea.f.zoitl@gmail.com>
2024-09-09 16:04:21 +0200

    Creation of supporting SWTBot class SWTBotECC #377
    - Creation of supporting SWTBot class SWTBotECC and related methods are
    moved here from Abstract4diacUITests
    - ECCEditorTests are adapted to this changes and are running again
    
    Addresses https://github.com/eclipse-4diac/4diac-ide/issues/377

--- tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/Abstract4diacUITests.java
+++ tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/Abstract4diacUITests.java
@@ -20,19 +20,12 @@ import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.List;
-
-import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.fordiac.ide.test.ui.helpers.SWTBotPropertySheet;
 import org.eclipse.fordiac.ide.test.ui.helpers.SWTBotSystemExplorer;
 import org.eclipse.fordiac.ide.test.ui.helpers.UITestNamesHelper;
 import org.eclipse.fordiac.ide.test.ui.swtbot.SWT4diacGefBot;
-import org.eclipse.fordiac.ide.test.ui.swtbot.SWTBot4diacGefEditor;
 import org.eclipse.fordiac.ide.test.ui.swtbot.SWTBot4diacGefViewer;
 import org.eclipse.fordiac.ide.test.ui.swtbot.SWTBot4diacNatTable;
-import org.eclipse.gef.GraphicalEditPart;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swtbot.eclipse.finder.matchers.WidgetMatcherFactory;
@@ -192,71 +185,6 @@ public class Abstract4diacUITests {
 		dataTypeTableBot.setCellDataValueByPosition(row, col, newValue);
 		dataTypeTableBot.doubleclick(row, col);
 		assertEquals(dataTypeTableBot.getCellDataValueByPosition(row, col), newValue);
-	}
-
-	/**
-	 * Change the algorithm or event value of an EC state action.
-	 *
-	 * @param editor    The ECC editor.
-	 * @param stateName The name of the EC state.
-	 * @param value     The value to assign (algorithm or event).
-	 * @param index     The index of the item box to modify.
-	 */
-	protected static void changeAlgorithmAndEventValue(final SWTBot4diacGefEditor editor, final String partLabel,
-			final String newValue, final int boxIndex) {
-		final SWTBotGefEditPart box = getChildrenPart(editor.getEditPart(partLabel), boxIndex);
-		box.doubleClick().select();
-		editor.bot().ccomboBox().setSelection(newValue);
-		box.doubleClick().select();
-		assertEquals(editor.bot().ccomboBox().selection(), newValue);
-	}
-
-	/**
-	 * Retrieves the center point of a graphical part within the editor.
-	 *
-	 *
-	 * @param editPart The graphical part for which to get the center point.
-	 * @return A Point object representing the center coordinates of the part.
-	 */
-	protected static Point getPoint(final SWTBotGefEditPart editPart) {
-		final IFigure figure = ((GraphicalEditPart) editPart.part()).getFigure();
-		assertNotNull(figure);
-		final Rectangle statePartBounds = ((GraphicalEditPart) editPart.part()).getFigure().getBounds().getCopy();
-		assertNotNull(statePartBounds);
-		figure.translateToAbsolute(statePartBounds);
-		return new Point(statePartBounds.getCenter().x, statePartBounds.getCenter().y);
-
-	}
-
-	/**
-	 * Retrieves a child part of a given parent part at a specified index.
-	 *
-	 * @param parentPart The parent part from which to retrieve the child.
-	 * @param index      The index of the child part to retrieve. Use 0 for the
-	 *                   first child, -1 for the last child, or any other index.
-	 * @return The SWTBotGefEditPart corresponding to the specified child.
-	 */
-	protected static SWTBotGefEditPart getChildrenPart(final SWTBotGefEditPart parentPart, final int index) {
-		final List<SWTBotGefEditPart> childParts = parentPart.children();
-		assertNotNull(childParts);
-		return switch (index) {
-		case 0 -> {
-			final SWTBotGefEditPart childPart = childParts.getFirst();
-			assertNotNull(childPart);
-			yield childPart;
-		}
-		case -1 -> {
-			final SWTBotGefEditPart childPart = childParts.getLast();
-			assertNotNull(childPart);
-			yield childPart;
-		}
-		default -> {
-			final SWTBotGefEditPart childPart = childParts.get(index);
-			assertNotNull(childPart);
-			yield childPart;
-		}
-		};
-
 	}
 
 	/**
