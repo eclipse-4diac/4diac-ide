7123c411a436141cea566514b69d9b8b59ea44f5
Andrea Zoitl <andrea.f.zoitl@gmail.com>
2024-09-19 11:38:35 +0200

    Added copyright header to class SWTBotECC #377
    Added missing copyright header to supporting SWTBot class SWTBotECC
    
    Addresses https://github.com/eclipse-4diac/4diac-ide/issues/377

--- tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/helpers/SWTBotECC.java
+++ tests/org.eclipse.fordiac.ide.test.ui/src/org/eclipse/fordiac/ide/test/ui/helpers/SWTBotECC.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Prashantkumar Khatri, Andrea Zoitl
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Prashantkumar Khatri - initial API and implementation and/or initial documentation
+ *   Andrea Zoitl - Creation of a fluid API design for UI SWTBot testing
+ *******************************************************************************/
+
 package org.eclipse.fordiac.ide.test.ui.helpers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
