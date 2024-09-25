f680144f28ccd0f47c70addb19a364dccd41072e
Martin Jobst <martin.jobst@posteo.de>
2024-09-25 10:35:02 +0200

    Fix variable access for internal FBs in evaluator

--- tests/org.eclipse.fordiac.ide.test.model.eval/src/org/eclipse/fordiac/ide/test/model/eval/fb/SimpleFBEvaluatorTest.java
+++ tests/org.eclipse.fordiac.ide.test.model.eval/src/org/eclipse/fordiac/ide/test/model/eval/fb/SimpleFBEvaluatorTest.java
@@ -829,6 +829,34 @@ class SimpleFBEvaluatorTest extends AbstractFBEvaluatorTest {
 						List.of(internalFB)).getVariables().get("DO1").getValue());
 	}
 
+	@Test
+	void testFunctionFBCallVariables() throws EvaluatorException, InterruptedException {
+		final FB internalFB = newFB("FB1",
+				newFunctionFBType("TEST_FUNC",
+						List.of(newVarDeclaration("DI1", ElementaryTypes.DINT, true),
+								newVarDeclaration("DI2", ElementaryTypes.DINT, true),
+								newVarDeclaration("", ElementaryTypes.DINT, false)),
+						"""
+								FUNCTION TEST_FUNC : DINT
+								VAR_INPUT
+									DI1 : DINT;
+									DI2 : DINT;
+								END_VAR
+								TEST_FUNC := DI1 + DI2;
+								END_FUNCTION
+								"""));
+		assertEquals(toDIntValue(42),
+				evaluateSimpleFB(List.of(newSTAlgorithm("""
+						FB1.DI1 := DI1;
+						FB1.DI2 := DI2;
+						DO1 := MUL(FB1(), 2);
+						""", "REQ")), "REQ",
+						Collections.<Variable<?>>unmodifiableList(CollectionLiterals.<Variable<?>>newArrayList(
+								newVariable(toDIntValue(17), "DI1"), newVariable(toDIntValue(4), "DI2"))),
+						newVarDeclaration("DO1", ElementaryTypes.DINT, false), List.of(), List.of(),
+						List.of(internalFB)).getVariables().get("DO1").getValue());
+	}
+
 	FBType newTestSimpleFBType() {
 		final Event inputEvent = newEvent("REQ", true);
 		final Event outputEvent = newEvent("CNF", false);
