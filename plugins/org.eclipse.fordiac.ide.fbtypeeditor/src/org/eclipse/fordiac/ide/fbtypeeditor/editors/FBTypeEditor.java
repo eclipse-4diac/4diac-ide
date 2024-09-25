85edbbb3bfc59aadcd4a66f71871f8fd03be7e69
Ernst Blecha <ernst.blecha@primetals.com>
2024-09-25 21:10:40 +0200

    Update /org.eclipse.fordiac.ide.fbtypeeditor/src/org/eclipse/fordiac/ide/fbtypeeditor/editors/FBTypeEditor.java
    Co-authored-by: Martin Jobst <martin.jobst@posteo.de>

--- plugins/org.eclipse.fordiac.ide.fbtypeeditor/src/org/eclipse/fordiac/ide/fbtypeeditor/editors/FBTypeEditor.java
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor/src/org/eclipse/fordiac/ide/fbtypeeditor/editors/FBTypeEditor.java
@@ -96,7 +96,6 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.IEditorActionBarContributor;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
@@ -515,11 +514,6 @@ public class FBTypeEditor extends AbstractCloseAbleFormEditor implements ISelect
 				i++;
 			}
 		}
-	}
-
-	@SuppressWarnings("static-method")
-	public IEditorActionBarContributor getActionBarContributor() {
-		return null;
 	}
 
 	@Override
