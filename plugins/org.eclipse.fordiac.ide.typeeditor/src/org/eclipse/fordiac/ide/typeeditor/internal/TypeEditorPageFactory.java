3b393d621196ad233cf624aec03a898194b4b46e
Martin Jobst <martin.jobst@posteo.de>
2024-09-26 21:26:40 +0200

    Add enablement to type editor page extension point
    Add enablement to type editor page extension point and remove individual
    editor descriptors.

--- plugins/org.eclipse.fordiac.ide.typeeditor/src/org/eclipse/fordiac/ide/typeeditor/internal/TypeEditorPageFactory.java
+++ plugins/org.eclipse.fordiac.ide.typeeditor/src/org/eclipse/fordiac/ide/typeeditor/internal/TypeEditorPageFactory.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2024 Primetals Technologies Austria GmbH
+ *                    Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,60 +10,41 @@
  *
  * Contributors:
  *   Alois Zoitl - initial API and implementation and/or initial documentation
+ *   Martin Erich Jobst - add enablement element
  *******************************************************************************/
 package org.eclipse.fordiac.ide.typeeditor.internal;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
 import org.eclipse.fordiac.ide.typeeditor.ITypeEditorPage;
-import org.eclipse.fordiac.ide.typeeditor.ITypeEditorPageDescriptor;
-import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
 
 public class TypeEditorPageFactory {
 
 	private static final String TYPE_EDITOR_PAGE_EXTENSION_POINT_ID = "org.eclipse.fordiac.ide.typeeditor.typeEditorPage"; //$NON-NLS-1$
 
-	private final record TypeEditorPageEntry(ITypeEditorPageDescriptor editorPageDescriptor, int sortIndex) {
-
-	}
-
-	private final List<TypeEditorPageEntry> editorPages = loadEditorPages();
+	private final List<TypeEditorPageDescriptor> editorPages = loadEditorPages();
 
 	public Collection<ITypeEditorPage> getEditors(final LibraryElement type) {
 		return editorPages.stream() //
-				.filter(ep -> ep.editorPageDescriptor().handlesType(type)) //
-				.sorted(Comparator.comparingInt(TypeEditorPageEntry::sortIndex)) //
-				.map(ep -> ep.editorPageDescriptor().createEditorPage()) //
-				.toList();
+				.filter(ep -> ep.handlesType(type)) //
+				.sorted(Comparator.comparingInt(TypeEditorPageDescriptor::getSortIndex)) //
+				.map(TypeEditorPageDescriptor::createEditorPage) //
+				.filter(Objects::nonNull).toList();
 	}
 
-	private static List<TypeEditorPageEntry> loadEditorPages() {
+	private static List<TypeEditorPageDescriptor> loadEditorPages() {
 		final IExtensionRegistry registry = Platform.getExtensionRegistry();
 		final IExtensionPoint point = registry.getExtensionPoint(TYPE_EDITOR_PAGE_EXTENSION_POINT_ID);
-		final List<TypeEditorPageEntry> editorPages = new ArrayList<>();
-		for (final IExtension extension : point.getExtensions()) {
-			for (final IConfigurationElement confEl : extension.getConfigurationElements()) {
-				try {
-					if (confEl.createExecutableExtension(
-							"class") instanceof final ITypeEditorPageDescriptor editorPageDescriptor) { //$NON-NLS-1$
-						editorPages.add(new TypeEditorPageEntry(editorPageDescriptor,
-								Integer.parseInt(confEl.getAttribute("sortIndex")))); //$NON-NLS-1$
-					}
-				} catch (final Exception e) {
-					FordiacLogHelper.logError(e.getMessage(), e);
-				}
-			}
-		}
-		return editorPages;
+		return Stream.of(point.getExtensions()).map(IExtension::getConfigurationElements).flatMap(Stream::of)
+				.map(TypeEditorPageDescriptor::new).toList();
 	}
-
 }
