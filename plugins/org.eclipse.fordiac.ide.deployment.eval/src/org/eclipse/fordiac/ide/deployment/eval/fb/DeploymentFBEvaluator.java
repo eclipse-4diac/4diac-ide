0a29e1d3eba6756fbe066bcc26a3f79736ad174b
Martin Jobst <martin.jobst@posteo.de>
2024-10-10 11:46:30 +0200

    Refactor deployment FB evaluator to use new monitoring methods
    Also fix some minor issues in evaluator.

--- plugins/org.eclipse.fordiac.ide.deployment.eval/src/org/eclipse/fordiac/ide/deployment/eval/fb/DeploymentFBEvaluator.java
+++ plugins/org.eclipse.fordiac.ide.deployment.eval/src/org/eclipse/fordiac/ide/deployment/eval/fb/DeploymentFBEvaluator.java
@@ -12,20 +12,21 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.deployment.eval.fb;
 
+import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.eclipse.fordiac.ide.deployment.data.FBDeploymentData;
-import org.eclipse.fordiac.ide.deployment.devResponse.Data;
 import org.eclipse.fordiac.ide.deployment.devResponse.Port;
 import org.eclipse.fordiac.ide.deployment.devResponse.Response;
+import org.eclipse.fordiac.ide.deployment.devResponse.Watches;
 import org.eclipse.fordiac.ide.deployment.eval.DeploymentEvaluatorSharedState;
 import org.eclipse.fordiac.ide.deployment.eval.Messages;
 import org.eclipse.fordiac.ide.deployment.exceptions.DeploymentException;
-import org.eclipse.fordiac.ide.deployment.monitoringbase.MonitoringBaseElement;
 import org.eclipse.fordiac.ide.model.eval.Evaluator;
 import org.eclipse.fordiac.ide.model.eval.EvaluatorException;
 import org.eclipse.fordiac.ide.model.eval.fb.FBEvaluator;
@@ -34,25 +35,20 @@ import org.eclipse.fordiac.ide.model.eval.function.StandardFunctions;
 import org.eclipse.fordiac.ide.model.eval.variable.Variable;
 import org.eclipse.fordiac.ide.model.libraryElement.Event;
 import org.eclipse.fordiac.ide.model.libraryElement.FB;
-import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.fordiac.ide.model.libraryElement.FBType;
 import org.eclipse.fordiac.ide.model.libraryElement.ICallable;
 import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
-import org.eclipse.fordiac.ide.model.libraryElement.INamedElement;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
 import org.eclipse.fordiac.ide.model.libraryElement.Resource;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
-import org.eclipse.fordiac.ide.model.monitoring.MonitoringElement;
-import org.eclipse.fordiac.ide.model.monitoring.MonitoringFactory;
-import org.eclipse.fordiac.ide.monitoring.MonitoringManagerUtils;
 
 public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 
 	private static final String FAKE_TIME_DEV_PARAM_NAME = "FakeTime"; //$NON-NLS-1$
 	private DeploymentEvaluatorSharedState sharedState;
 	private FBDeploymentData deploymentData;
-	private Map<String, MonitoringElement> monitoringElements;
 	private boolean outputEvent;
+	private final Map<Event, Integer> eventCounters = new HashMap<>();
 
 	public DeploymentFBEvaluator(final T type, final Variable<?> context, final Iterable<Variable<?>> variables,
 			final Evaluator parent) {
@@ -64,20 +60,20 @@ public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 		if (sharedState == null) {
 			sharedState = DeploymentEvaluatorSharedState.fromContext(getType().getTypeLibrary());
 			deploymentData = createFBDeploymentData(sharedState.getResource());
-			monitoringElements = createMonitoringElements(deploymentData.getFb());
 			try {
 				sharedState.prepare();
 				sharedState.createFBInstance(deploymentData);
 				sharedState.startFB(deploymentData);
-				for (final MonitoringBaseElement element : monitoringElements.values()) {
-					sharedState.getDeviceManagementInteractor().addWatch(element);
+				for (final IInterfaceElement element : deploymentData.getFb().getInterface()
+						.getAllInterfaceElements()) {
+					sharedState.addWatch(sharedState.getResource(), getResourceRelativeName(element));
 				}
 				writeVariables(getType().getInterfaceList().getInputVars());
 				writeVariables(getType().getInterfaceList().getInOutVars());
 				updateWatches(sharedState.readWatches());
 				outputEvent = false; // make sure output event is set to false after update
 			} catch (final DeploymentException e) {
-				throw new RuntimeException(e);
+				throw new EvaluatorException(e.getMessage(), e, this);
 			}
 		}
 	}
@@ -85,13 +81,14 @@ public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 	public void cleanup() {
 		if (sharedState != null) {
 			try {
-				for (final MonitoringBaseElement element : monitoringElements.values()) {
-					sharedState.getDeviceManagementInteractor().removeWatch(element);
+				for (final IInterfaceElement element : deploymentData.getFb().getInterface()
+						.getAllInterfaceElements()) {
+					sharedState.removeWatch(sharedState.getResource(), getResourceRelativeName(element));
 				}
 				sharedState.deleteFB(deploymentData);
 				sharedState.getResource().getFBNetwork().getNetworkElements().remove(deploymentData.getFb());
 			} catch (final DeploymentException e) {
-				throw new RuntimeException(e);
+				throw new EvaluatorException(e.getMessage(), e, this);
 			} finally {
 				sharedState = null;
 			}
@@ -107,25 +104,20 @@ public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 		return new FBDeploymentData("", fb); //$NON-NLS-1$
 	}
 
-	protected static Map<String, MonitoringElement> createMonitoringElements(final FBNetworkElement fbne) {
-		return fbne.getInterface().getAllInterfaceElements().stream()
-				.collect(Collectors.toMap(INamedElement::getName, DeploymentFBEvaluator::createMonitoringElement));
-	}
-
-	protected static MonitoringElement createMonitoringElement(final IInterfaceElement interfaceElement) {
-		final MonitoringElement monitoringElement = MonitoringFactory.eINSTANCE.createMonitoringElement();
-		monitoringElement.setPort(MonitoringManagerUtils.createPortElement(interfaceElement));
-		return monitoringElement;
-	}
-
 	@Override
-	public void evaluate(final Event event) throws EvaluatorException {
+	public void evaluate(final Event event) throws EvaluatorException, InterruptedException {
+		final Event instanceEvent = deploymentData.getFb().getInterface().getEvent(event.getName());
+		if (instanceEvent == null) {
+			throw new EvaluatorException(
+					MessageFormat.format(Messages.DeploymentFBEvaluator_NoSuchInstanceEvent, event.getQualifiedName()),
+					this);
+		}
 		prepare();
 		try {
 			writeVariables(getType().getInterfaceList().getInputVars());
 			writeVariables(getType().getInterfaceList().getInOutVars());
 			sharedState.writeDeviceParameter(FAKE_TIME_DEV_PARAM_NAME, StandardFunctions.NOW_MONOTONIC().toString());
-			sharedState.triggerEvent(monitoringElements.get(event.getName()));
+			sharedState.triggerEvent(sharedState.getResource(), getResourceRelativeName(instanceEvent));
 			pollWatches();
 			update(getVariables().values());
 		} catch (final DeploymentException e) {
@@ -140,15 +132,18 @@ public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 		}
 	}
 
-	protected void pollWatches() throws DeploymentException {
+	protected void pollWatches() throws DeploymentException, InterruptedException {
 		do {
+			if (Thread.interrupted()) {
+				throw new InterruptedException();
+			}
 			updateWatches(sharedState.readWatches());
 		} while (!outputEvent);
 		outputEvent = false;
 	}
 
 	protected void updateWatches(final Response response) {
-		response.getWatches().getResources().stream()
+		Optional.ofNullable(response.getWatches()).map(Watches::getResources).stream().flatMap(Collection::stream)
 				.filter(resource -> sharedState.getResource().getName().equals(resource.getName()))
 				.map(org.eclipse.fordiac.ide.deployment.devResponse.Resource::getFbs).flatMap(Collection::stream)
 				.filter(fb -> deploymentData.getFb().getName().equals(fb.getName()))
@@ -157,20 +152,17 @@ public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 	}
 
 	protected void updateWatch(final Port port) {
-		final MonitoringElement monitoringElement = monitoringElements.get(port.getName());
-		if (monitoringElement != null) {
-			// update monitoring element
-			final String oldValue = monitoringElement.getCurrentValue();
-			port.getDataValues().stream().map(Data::getValue).forEachOrdered(monitoringElement::setCurrentValue);
-			final String newValue = monitoringElement.getCurrentValue();
-			// update interface element
-			switch (monitoringElement.getPort().getInterfaceElement()) {
-			case final VarDeclaration varDeclaration -> updateDataWatch(varDeclaration, newValue);
-			case final Event event when !oldValue.equals(newValue) -> updateEventWatch(event, newValue);
-			case null, default -> {
-				// do nothing
-			}
-			}
+		if (port.getDataValues().isEmpty()) {
+			return;
+		}
+		final String newValue = port.getDataValues().getLast().getValue();
+		final IInterfaceElement element = getType().getInterfaceList().getInterfaceElement(port.getName());
+		switch (element) {
+		case final VarDeclaration varDeclaration -> updateDataWatch(varDeclaration, newValue);
+		case final Event event -> updateEventWatch(event, newValue);
+		case null, default -> {
+			// do nothing
+		}
 		}
 	}
 
@@ -183,20 +175,39 @@ public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 	}
 
 	protected void updateEventWatch(final Event event, final String newValue) {
+		// parse event count
+		final Integer eventCount;
+		try {
+			eventCount = Integer.valueOf(newValue);
+		} catch (final NumberFormatException e) {
+			error(Messages.DeploymentFBEvaluator_InvalidEventCounter, e);
+			return;
+		}
+		// has changed?
+		if (eventCount.equals(eventCounters.put(event, eventCount))) {
+			return;
+		}
+		// update queue
 		if (getEventQueue() instanceof final FBEvaluatorCountingEventQueue countingEventQueue) {
-			final Event typeEvent = getType().getInterfaceList().getEvent(event.getName());
-			if (typeEvent != null) {
-				try {
-					final int eventCount = Integer.parseInt(newValue);
-					countingEventQueue.getCount(typeEvent).set(eventCount);
-				} catch (final NumberFormatException e) {
-					error(Messages.DeploymentFBEvaluator_InvalidEventCounter, e);
-				}
+			try {
+				countingEventQueue.getCount(event).set(eventCount.intValue());
+			} catch (final NumberFormatException e) {
+				error(Messages.DeploymentFBEvaluator_InvalidEventCounter, e);
 			}
 		}
+		// is output event?
 		if (!event.isIsInput()) {
 			outputEvent = true;
 		}
+	}
+
+	protected String getResourceRelativeName(final IInterfaceElement element) {
+		final String qualifiedName = element.getQualifiedName();
+		final String resourceName = sharedState.getResource().getName();
+		if (qualifiedName.startsWith(resourceName)) {
+			return qualifiedName.substring(resourceName.length() + 1);
+		}
+		return qualifiedName;
 	}
 
 	protected DeploymentEvaluatorSharedState getSharedState() {
@@ -205,14 +216,6 @@ public class DeploymentFBEvaluator<T extends FBType> extends FBEvaluator<T> {
 
 	protected FBDeploymentData getDeploymentData() {
 		return deploymentData;
-	}
-
-	protected Map<String, MonitoringElement> getMonitoringElements() {
-		return monitoringElements;
-	}
-
-	protected void setMonitoringElements(final Map<String, MonitoringElement> monitoringElements) {
-		this.monitoringElements = monitoringElements;
 	}
 
 	@Override
