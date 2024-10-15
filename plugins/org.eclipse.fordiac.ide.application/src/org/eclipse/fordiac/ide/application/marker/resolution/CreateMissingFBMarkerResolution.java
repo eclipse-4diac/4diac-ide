10ea7d3bdea368b31322f2eb0718728df7cd8fce
Martin Jobst <martin.jobst@posteo.de>
2024-10-15 18:40:16 +0200

    Refactor marker resolutions
    Refactor marker resolutions to use proper locking, combine commands for
    the same type, and save only if necessary after applying changes.

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/marker/resolution/CreateMissingFBMarkerResolution.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/marker/resolution/CreateMissingFBMarkerResolution.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2024 Primetals Technologies Austria GmbH
+ *                    Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -10,6 +11,8 @@
  * Contributors:
  *   Sebastian Hollersbacher
  *    - initial API and implementation and/or initial documentation
+ *   Martin Erich Jobst
+ *    - refactor marker resolutions
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.marker.resolution;
 
@@ -17,72 +20,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.fordiac.ide.model.commands.change.UpdateFBTypeCommand;
 import org.eclipse.fordiac.ide.model.commands.change.UpdateInternalFBCommand;
 import org.eclipse.fordiac.ide.model.errormarker.FordiacErrorMarker;
-import org.eclipse.fordiac.ide.model.libraryElement.BaseFBType;
-import org.eclipse.fordiac.ide.model.libraryElement.ErrorMarkerFBNElement;
 import org.eclipse.fordiac.ide.model.libraryElement.FB;
-import org.eclipse.fordiac.ide.model.search.AbstractLiveSearchContext;
+import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
+import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementPackage;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeEntry;
-import org.eclipse.fordiac.ide.model.typelibrary.impl.ErrorFBTypeEntryImpl;
 import org.eclipse.fordiac.ide.typemanagement.wizards.RestrictedNewTypeWizard;
 import org.eclipse.fordiac.ide.ui.FordiacMessages;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.PlatformUI;
 
-public class CreateMissingFBMarkerResolution extends AbstractErrorMarkerResolution {
+public class CreateMissingFBMarkerResolution extends AbstractCommandMarkerResolution<FBNetworkElement> {
 
 	private TypeEntry newEntry;
 	List<EObject> targets = new ArrayList<>();
 
 	public CreateMissingFBMarkerResolution(final IMarker marker) {
-		super(FordiacMessages.Repair_Dialog_New_FB, marker);
+		super(marker, FBNetworkElement.class);
 	}
 
 	@Override
-	public void run(final IMarker marker) {
-		if (!marker.exists()) {
-			return;
-		}
-		run(new IMarker[] { marker }, new NullProgressMonitor());
-	}
-
-	private void updateTarget(final EObject target) {
-		if (target instanceof final ErrorMarkerFBNElement err) {
-			AbstractLiveSearchContext.executeAndSave(new UpdateFBTypeCommand(err, newEntry), err,
-					new NullProgressMonitor());
-		}
-
-		if (target instanceof final FB fb && fb.getTypeEntry() instanceof ErrorFBTypeEntryImpl
-				&& fb.eContainer() instanceof final BaseFBType base && base.getInternalFbs().contains(fb)) {
-			AbstractLiveSearchContext.executeAndSave(new UpdateInternalFBCommand(fb, newEntry), fb,
-					new NullProgressMonitor());
-		}
-	}
-
-	@Override
-	public void run(final IMarker[] markers, final IProgressMonitor monitor) {
-		// save Error-Objects for new Type has same name
-		for (final IMarker marker : markers) {
-			targets.add(getTargetElement(marker));
-		}
-
-		final String errorName = FordiacErrorMarker.getData(this.marker)[0];
-		createNewEntry(errorName);
-
-		if (newEntry == null) {
-			return;
-		}
-
-		targets.forEach(this::updateTarget);
-	}
-
-	private void createNewEntry(final String defaultName) {
+	protected boolean prepare(final IMarker[] markers, final IProgressMonitor monitor) throws CoreException {
+		final String defaultName = FordiacErrorMarker.getData(markers[0])[0];
 		newEntry = RestrictedNewTypeWizard.showRestrictedNewTypeWizard(
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), defaultName, ".fbt", //$NON-NLS-1$
-				marker.getResource().getProject().getName());
+				markers[0].getResource().getProject().getName());
+		return newEntry != null;
+	}
+
+	@Override
+	protected Command createCommand(final FBNetworkElement element, final IProgressMonitor monitor)
+			throws CoreException {
+		return switch (element) {
+		case final FB fb when fb.eContainmentFeature() == LibraryElementPackage.Literals.BASE_FB_TYPE__INTERNAL_FBS ->
+			new UpdateInternalFBCommand(fb, newEntry);
+		default -> new UpdateFBTypeCommand(element, newEntry);
+		};
+	}
+
+	@Override
+	public String getLabel() {
+		return FordiacMessages.Repair_Dialog_New_FB;
+	}
+
+	@Override
+	public String getDescription() {
+		return FordiacMessages.Repair_Dialog_New_FB;
+	}
+
+	@Override
+	public Image getImage() {
+		return null;
 	}
 }
