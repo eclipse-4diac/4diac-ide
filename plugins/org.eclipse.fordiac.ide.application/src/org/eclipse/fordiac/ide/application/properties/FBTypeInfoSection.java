acc074aeb25181b8e08c134336cd8fa70dba4638
Alois Zoitl <alois.zoitl@gmx.at>
2024-10-20 22:54:29 +0200

    [Fix] Type Info property sheets showed instance information

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/FBTypeInfoSection.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/properties/FBTypeInfoSection.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2014, 2016 fortiss GmbH
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -13,26 +13,29 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.properties;
 
-import org.eclipse.fordiac.ide.application.editparts.FBEditPart;
 import org.eclipse.fordiac.ide.gef.properties.TypeInfoSection;
 import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.ui.IWorkbenchPart;
 
 public class FBTypeInfoSection extends TypeInfoSection {
 
 	@Override
-	protected CommandStack getCommandStack(IWorkbenchPart part, Object input) {
+	protected CommandStack getCommandStack(final IWorkbenchPart part, final Object input) {
 		return null;
 	}
 
 	@Override
-	protected LibraryElement getInputType(Object input) {
-		if (input instanceof FBEditPart) {
-			return ((FBEditPart) input).getModel().getType();
-		} else if (input instanceof FBNetworkElement) {
-			return ((FBNetworkElement) input).getType();
+	protected LibraryElement getInputType(final Object input) {
+		Object elementToCheck = input;
+
+		if (input instanceof final EditPart ep) {
+			elementToCheck = ep.getModel();
+		}
+		if (elementToCheck instanceof final FBNetworkElement fbnEl) {
+			return fbnEl.getType();
 		}
 		return null;
 	}
