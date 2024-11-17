36959c099eb0fd234ce14fae033d0816f872fb5a
Martin Jobst <martin.jobst@posteo.de>
2024-11-17 17:07:16 +0100

    Avoid deprecated type editable in ST editor

--- plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/document/STCoreDocumentProvider.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/document/STCoreDocumentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Martin Erich Jobst
+ * Copyright (c) 2023, 2024 Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -19,7 +19,6 @@ import org.eclipse.fordiac.ide.structuredtextcore.util.STCoreReconciler;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IFileEditorInput;
-import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.xtext.ui.editor.model.XtextDocument;
 
 import com.google.inject.Inject;
@@ -47,30 +46,10 @@ public class STCoreDocumentProvider extends LibraryElementXtextDocumentProvider 
 			monitor.worked(1);
 			monitor.subTask("Reconciling"); //$NON-NLS-1$
 			if (partition.isPresent()) {
-				Display.getDefault().syncExec(() -> {
-					reconciler.reconcile(element, partition);
-					// save type if opened directly from a file and not in an FB type editor,
-					// indicated by a FileEditorInput instead of a TypeEditorInput
-					if (fileEditorInput instanceof FileEditorInput) {
-						try {
-							element.getTypeEntry().save(element, monitor);
-						} catch (final CoreException e) {
-							throw new RuntimeException(e);
-						}
-					}
-				});
+				Display.getDefault().syncExec(() -> reconciler.reconcile(element, partition));
 			}
 		} finally {
 			monitor.done();
-		}
-	}
-
-	@Override
-	protected void handleElementContentChanged(final IFileEditorInput fileEditorInput) {
-		// update if opened directly from a file and not in an FB type editor,
-		// indicated by a FileEditorInput instead of a TypeEditorInput
-		if (fileEditorInput instanceof FileEditorInput) {
-			super.handleElementContentChanged(fileEditorInput);
 		}
 	}
 }
