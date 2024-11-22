993f59de977622faf8c10785259f3d950db40c54
Daniel Lindhuber <lindhuber.daniel@gmail.com>
2024-11-22 11:24:22 +0100

    layout class cleanup

--- plugins/org.eclipse.fordiac.ide.elk/src/org/eclipse/fordiac/ide/elk/FordiacLayout.java
+++ plugins/org.eclipse.fordiac.ide.elk/src/org/eclipse/fordiac/ide/elk/FordiacLayout.java
@@ -14,7 +14,6 @@ package org.eclipse.fordiac.ide.elk;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.draw2d.geometry.Dimension;
@@ -28,6 +27,7 @@ import org.eclipse.elk.alg.layered.options.LayeringStrategy;
 import org.eclipse.elk.alg.layered.options.NodePlacementStrategy;
 import org.eclipse.elk.alg.layered.options.NodePromotionStrategy;
 import org.eclipse.elk.alg.layered.options.ValidifyStrategy;
+import org.eclipse.elk.alg.libavoid.options.LibavoidMetaDataProvider;
 import org.eclipse.elk.alg.libavoid.server.LibavoidServerException;
 import org.eclipse.elk.core.RecursiveGraphLayoutEngine;
 import org.eclipse.elk.core.math.ElkPadding;
@@ -60,33 +60,38 @@ import org.eclipse.ui.PlatformUI;
 public class FordiacLayout {
 
 	public static void blockLayout(final IEditorPart part, final AbstractFBNetworkEditPart ep) {
-		part.getAdapter(CommandStack.class).execute(layout(part, ep, true));
+		executeLayout(part, ep, true);
 	}
 
 	public static void connectionLayout(final IEditorPart part, final AbstractFBNetworkEditPart ep) {
-		part.getAdapter(CommandStack.class).execute(layout(part, ep, false));
-		getConnectionLayoutCommand(part);
+		executeLayout(part, ep, false);
 	}
 
 	public static Command getConnectionLayoutCommand(final IEditorPart part) {
-		final var networkEP = findRootEditPart(part);
-		return layout(part, networkEP, false);
+		return layout(part, findRootEditPart(part), false);
+	}
+
+	private static void executeLayout(final IEditorPart part, final AbstractFBNetworkEditPart ep,
+			final boolean isBlockLayout) {
+		final Command command = layout(part, ep, isBlockLayout);
+		part.getAdapter(CommandStack.class).execute(command);
 	}
 
 	private static Command layout(final IEditorPart part, final AbstractFBNetworkEditPart ep,
 			final boolean isBlockLayout) {
-		final var engine = new RecursiveGraphLayoutEngine();
-		final var cmd = new CompoundCommand();
-		cmd.add(performLayoutRun(part, ep, engine, isBlockLayout)); // initial run, e.g. application
+		final RecursiveGraphLayoutEngine engine = new RecursiveGraphLayoutEngine();
+		final CompoundCommand command = new CompoundCommand();
 
-		if (ep instanceof UnfoldedSubappContentEditPart) { // have to look for further subapps
-			final List<UnfoldedSubappContentEditPart> subapps = new ArrayList<>();
-			collectSubapps(ep, subapps);
-			Collections.reverse(subapps);
-			subapps.stream().forEach(subapp -> cmd.add(performLayoutRun(part, subapp, engine, isBlockLayout)));
+		// perform initial layout
+		command.add(performLayoutRun(part, ep, engine, isBlockLayout));
+
+		// handle subapps if applicable
+		if (ep instanceof UnfoldedSubappContentEditPart) {
+			final List<UnfoldedSubappContentEditPart> subapps = collectSubapps(ep);
+			subapps.forEach(subapp -> command.add(performLayoutRun(part, subapp, engine, isBlockLayout)));
 		}
 
-		return cmd;
+		return command;
 	}
 
 	private static Command performLayoutRun(final IEditorPart part, final AbstractFBNetworkEditPart ep,
@@ -96,43 +101,58 @@ public class FordiacLayout {
 		FordiacGraphBuilder.build(mapping);
 
 		if (isBlockLayout) {
-			configureBlockLayoutGraph(mapping.getLayoutGraph());
-			engine.layout(mapping.getLayoutGraph(), new NullElkProgressMonitor());
-
-			if (Activator.getDefault().getPreferenceStore().getBoolean(DiagramPreferences.SNAP_TO_GRID)) {
-				snapToGrid(mapping);
-			}
+			performBlockLayout(mapping, engine);
 		}
 
+		performConnectionLayout(mapping, engine);
+
+		FordiacGraphDataHelper.calculate(mapping);
+
+		return createCompoundCommand(mapping, isBlockLayout);
+	}
+
+	private static void performBlockLayout(final FordiacLayoutMapping mapping,
+			final RecursiveGraphLayoutEngine engine) {
+		configureBlockLayoutGraph(mapping.getLayoutGraph());
+		engine.layout(mapping.getLayoutGraph(), new NullElkProgressMonitor());
+		applySnapToGridIfEnabled(mapping);
+	}
+
+	private static void performConnectionLayout(final FordiacLayoutMapping mapping,
+			final RecursiveGraphLayoutEngine engine) {
 		configureConnectionLayoutGraph(mapping.getLayoutGraph());
 		try {
 			engine.layout(mapping.getLayoutGraph(), new NullElkProgressMonitor());
 		} catch (final LibavoidServerException e) {
-			MessageDialog.openWarning(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-					Messages.ConnectionLayout_TimeoutTitle,
-					MessageFormat.format(Messages.ConnectionLayout_TimeoutMessage, e.getMessage()));
+			showConnectionLayoutWarning(e);
 		}
+	}
 
-		FordiacGraphDataHelper.calculate(mapping);
-
-		final var cmd = new CompoundCommand();
-		if (isBlockLayout) {
-			cmd.add(new BlockLayoutCommand(mapping.getLayoutData()));
+	private static void applySnapToGridIfEnabled(final FordiacLayoutMapping mapping) {
+		if (Activator.getDefault().getPreferenceStore().getBoolean(DiagramPreferences.SNAP_TO_GRID)) {
+			snapToGrid(mapping);
 		}
-		cmd.add(new ConnectionLayoutCommand(mapping.getLayoutData()));
-		return cmd;
 	}
 
 	private static void snapToGrid(final FordiacLayoutMapping mapping) {
-		final var viewer = mapping.getWorkbenchPart().getAdapter(GraphicalViewer.class);
-		final var dim = (Dimension) viewer.getProperty(SnapToGrid.PROPERTY_GRID_SPACING);
-		final var gridSize = dim.preciseHeight();
+		final GraphicalViewer viewer = mapping.getWorkbenchPart().getAdapter(GraphicalViewer.class);
+		final Dimension gridSpacing = (Dimension) viewer.getProperty(SnapToGrid.PROPERTY_GRID_SPACING);
+		final double gridSize = gridSpacing.preciseHeight();
 
 		mapping.getLayoutGraph().getChildren().forEach(node -> {
-			final double x = Math.round((node.getX()) / gridSize) * gridSize;
-			final double y = Math.round((node.getY()) / gridSize) * gridSize;
+			final double x = Math.round(node.getX() / gridSize) * gridSize;
+			final double y = Math.round(node.getY() / gridSize) * gridSize;
 			node.setLocation(x, y);
 		});
+	}
+
+	private static Command createCompoundCommand(final FordiacLayoutMapping mapping, final boolean isBlockLayout) {
+		final CompoundCommand command = new CompoundCommand();
+		if (isBlockLayout) {
+			command.add(new BlockLayoutCommand(mapping.getLayoutData()));
+		}
+		command.add(new ConnectionLayoutCommand(mapping.getLayoutData()));
+		return command;
 	}
 
 	private static void configureBlockLayoutGraph(final ElkNode graph) {
@@ -166,44 +186,40 @@ public class FordiacLayout {
 
 	private static void configureConnectionLayoutGraph(final ElkGraphElement graph) {
 		graph.setProperty(CoreOptions.ALGORITHM, "org.eclipse.elk.alg.libavoid") //$NON-NLS-1$
+				.setProperty(LibavoidMetaDataProvider.SHAPE_BUFFER_DISTANCE, Double.valueOf(10))
+				.setProperty(LibavoidMetaDataProvider.IDEAL_NUDGING_DISTANCE, Double.valueOf(5))
+				.setProperty(LibavoidMetaDataProvider.NUDGE_SHARED_PATHS_WITH_COMMON_END_POINT, Boolean.FALSE)
+				.setProperty(LibavoidMetaDataProvider.ENABLE_HYPEREDGES_FROM_COMMON_SOURCE, Boolean.TRUE)
+				.setProperty(LibavoidMetaDataProvider.IMPROVE_HYPEREDGE_ROUTES_MOVING_ADDING_AND_DELETING_JUNCTIONS,
+						Boolean.TRUE)
 				.setProperty(CoreOptions.PORT_CONSTRAINTS, PortConstraints.FIXED_POS)
-				.setProperty(CoreOptions.DEBUG_MODE, Boolean.TRUE).setProperty(CoreOptions.DIRECTION, Direction.RIGHT);
-
-//				.setProperty(LibavoidMetaDataProvider.SHAPE_BUFFER_DISTANCE, Double.valueOf(10))
-//				.setProperty(LibavoidMetaDataProvider.IDEAL_NUDGING_DISTANCE, Double.valueOf(5))
-//				.setProperty(LibavoidMetaDataProvider.CLUSTER_CROSSING_PENALTY, Double.valueOf(10));
-//				.setProperty(LibavoidMetaDataProvider.NUDGE_SHARED_PATHS_WITH_COMMON_END_POINT, Boolean.FALSE)
-//				.setProperty(LibavoidMetaDataProvider.ENABLE_HYPEREDGES_FROM_COMMON_SOURCE, Boolean.TRUE)
-//				.setProperty(LibavoidMetaDataProvider.IMPROVE_HYPEREDGE_ROUTES_MOVING_ADDING_AND_DELETING_JUNCTIONS,
-//						Boolean.TRUE);
+				.setProperty(CoreOptions.DIRECTION, Direction.RIGHT);
 	}
 
-	private static void collectSubapps(final AbstractFBNetworkEditPart ep,
-			final List<UnfoldedSubappContentEditPart> subapps) {
-		// @formatter:off
-		final var children = ep.getChildren().stream()
-				.filter(SubAppForFBNetworkEditPart.class::isInstance)
-				.map(SubAppForFBNetworkEditPart.class::cast)
-				.filter(sub -> sub.getModel().isUnfolded())
-				.map(SubAppForFBNetworkEditPart::getContentEP)
-				.map(UnfoldedSubappContentEditPart.class::cast)
-				.toList();
-		// @formatter:on
-		subapps.addAll(children);
-		children.forEach(sub -> collectSubapps(sub, subapps));
+	private static void showConnectionLayoutWarning(final LibavoidServerException e) {
+		MessageDialog.openWarning(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+				Messages.ConnectionLayout_TimeoutTitle,
+				MessageFormat.format(Messages.ConnectionLayout_TimeoutMessage, e.getMessage()));
+	}
+
+	private static List<UnfoldedSubappContentEditPart> collectSubapps(final AbstractFBNetworkEditPart ep) {
+		final List<UnfoldedSubappContentEditPart> subapps = new ArrayList<>();
+		ep.getChildren().stream().filter(SubAppForFBNetworkEditPart.class::isInstance)
+				.map(SubAppForFBNetworkEditPart.class::cast).filter(sub -> sub.getModel().isUnfolded())
+				.map(SubAppForFBNetworkEditPart::getContentEP).map(UnfoldedSubappContentEditPart.class::cast)
+				.forEach(subapps::add);
+		return subapps;
 	}
 
 	private static AbstractFBNetworkEditPart findRootEditPart(final IWorkbenchPart workbenchPart) {
-		final var networkEP = (AbstractFBNetworkEditPart) workbenchPart.getAdapter(GraphicalViewer.class)
-				.getRootEditPart().getChildren().get(0);
-
 		final GraphicalViewer viewer = workbenchPart.getAdapter(GraphicalViewer.class);
-		final Point pt = ((FBNetworkContextMenuProvider) viewer.getContextMenu()).getTranslatedAndZoomedPoint();
+		final AbstractFBNetworkEditPart networkEP = (AbstractFBNetworkEditPart) viewer.getRootEditPart().getChildren()
+				.get(0);
+		final Point point = ((FBNetworkContextMenuProvider) viewer.getContextMenu()).getTranslatedAndZoomedPoint();
 
 		final AbstractContainerContentEditPart containerEP = GetEditPartFromGraficalViewerHelper
-				.findAbstractContainerContentEditPartAtPosition((IEditorPart) workbenchPart, pt, networkEP.getModel());
-
+				.findAbstractContainerContentEditPartAtPosition((IEditorPart) workbenchPart, point,
+						networkEP.getModel());
 		return (containerEP != null) ? containerEP : networkEP;
 	}
-
 }
