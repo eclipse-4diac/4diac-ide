16017e404962da01320be1e5e05965c933df50b5
Alois Zoitl <alois.zoitl@gmx.at>
2025-01-28 00:14:53 +0100

    Improve connection feedback when crossing viewer borders

--- plugins/org.eclipse.fordiac.ide.gef/src/org/eclipse/fordiac/ide/gef/tools/ScrollingConnectionEndpointTracker.java
+++ plugins/org.eclipse.fordiac.ide.gef/src/org/eclipse/fordiac/ide/gef/tools/ScrollingConnectionEndpointTracker.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2022 Johannes Kepler University Linz,
- *                          Primetals Technologies Austria
+ * Copyright (c) 2019, 2025 Johannes Kepler University Linz,
+ *                          Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -19,19 +19,14 @@ package org.eclipse.fordiac.ide.gef.tools;
 
 import java.util.Map;
 
-import org.eclipse.draw2d.geometry.Dimension;
-import org.eclipse.draw2d.geometry.Insets;
 import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.fordiac.ide.gef.figures.HideableConnection;
-import org.eclipse.fordiac.ide.gef.router.MoveableRouter;
-import org.eclipse.fordiac.ide.model.ui.editors.AdvancedScrollingGraphicalViewer;
-import org.eclipse.fordiac.ide.ui.preferences.ConnectionPreferenceValues;
 import org.eclipse.gef.ConnectionEditPart;
 import org.eclipse.gef.EditDomain;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPartViewer;
 import org.eclipse.gef.RequestConstants;
 import org.eclipse.gef.requests.ReconnectRequest;
+import org.eclipse.gef.tools.ConnectionDragCreationTool;
 import org.eclipse.gef.tools.ConnectionEndpointTracker;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyEvent;
@@ -39,14 +34,7 @@ import org.eclipse.swt.events.MouseEvent;
 
 public class ScrollingConnectionEndpointTracker extends ConnectionEndpointTracker {
 
-	// Safety border around the canvas to ensure that during dragging connections
-	// the canvas is not growing
-	private static final Insets CONNECTION_CANVAS_BORDER = new Insets(ConnectionPreferenceValues.HANDLE_SIZE,
-			MoveableRouter.MIN_CONNECTION_FB_DISTANCE_SCREEN + HideableConnection.BEND_POINT_BEVEL_SIZE
-					+ ConnectionPreferenceValues.HANDLE_SIZE,
-			ConnectionPreferenceValues.HANDLE_SIZE, ConnectionPreferenceValues.HANDLE_SIZE);
-
-	private InlineConnectionCreationTool conCreationTool = null;
+	private ConnectionDragCreationTool conCreationTool = null;
 
 	public ScrollingConnectionEndpointTracker(final ConnectionEditPart cep) {
 		super(cep);
@@ -54,19 +42,6 @@ public class ScrollingConnectionEndpointTracker extends ConnectionEndpointTracke
 
 	@Override
 	public void mouseDrag(final MouseEvent me, final EditPartViewer viewer) {
-		if (isActive() && (viewer instanceof final AdvancedScrollingGraphicalViewer advViewer)) {
-			final Point oldViewPort = advViewer.getViewLocation();
-			advViewer.checkScrollPositionDuringDragBounded(me,
-					new Point(MoveableRouter.MIN_CONNECTION_FB_DISTANCE_SCREEN
-							+ HideableConnection.BEND_POINT_BEVEL_SIZE + ConnectionPreferenceValues.HANDLE_SIZE,
-							ConnectionPreferenceValues.HANDLE_SIZE));
-			final Dimension delta = oldViewPort
-					.getDifference(((AdvancedScrollingGraphicalViewer) viewer).getViewLocation());
-			// Compensate the moved scrolling in the start position for correct dropping of
-			// moved parts
-			setStartLocation(getStartLocation().getTranslated(delta));
-			CanvasHelper.bindToContentPane(me, (AdvancedScrollingGraphicalViewer) viewer, getCanvasBorder());
-		}
 		if (null != conCreationTool) {
 			getCurrentInput().setInput(me);
 			conCreationTool.mouseDrag(me, viewer);
@@ -75,11 +50,6 @@ public class ScrollingConnectionEndpointTracker extends ConnectionEndpointTracke
 		} else {
 			super.mouseDrag(me, viewer);
 		}
-	}
-
-	@SuppressWarnings("static-method") // allow sub-classes to override the border calculation
-	protected Insets getCanvasBorder() {
-		return CONNECTION_CANVAS_BORDER;
 	}
 
 	@Override
@@ -96,9 +66,13 @@ public class ScrollingConnectionEndpointTracker extends ConnectionEndpointTracke
 		final EditPart target = (getCommandName().equals(RequestConstants.REQ_RECONNECT_SOURCE))
 				? getConnectionEditPart().getTarget()
 				: getConnectionEditPart().getSource();
-		conCreationTool = InlineConnectionCreationTool.createInlineConnCreationTool(target, getDomain(),
-				getCurrentViewer(), getLocation());
+		conCreationTool = createConnectionCreationTool(target);
 		updateTarget(getStartLocation());
+	}
+
+	protected ConnectionDragCreationTool createConnectionCreationTool(final EditPart target) {
+		return InlineConnectionCreationTool.createInlineConnCreationTool(target, getDomain(), getCurrentViewer(),
+				getLocation());
 	}
 
 	private void updateTarget(final Point p) {
