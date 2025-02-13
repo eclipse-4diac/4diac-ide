5f77e7347a74290fda09f17e104d5fc590533ac8
Martin Jobst <martin.jobst@posteo.de>
2025-02-13 06:49:48 +0100

    Add plant hierarchy build configuration

---
+++ plugins/org.eclipse.fordiac.ide.hierarchymanager.build/src/org/eclipse/fordiac/ide/hierarchymanager/build/HierarchyLanguageConstants.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Martin Erich Jobst
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Martin Jobst - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.hierarchymanager.build;
+
+public class HierarchyLanguageConstants {
+
+	public static final String ORG_ECLIPSE_FORDIAC_IDE_HIERARCHYMANAGER_HIERARCHY = "org.eclipse.fordiac.ide.hierarchymanager.hierarchy"; //$NON-NLS-1$
+
+	private HierarchyLanguageConstants() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ plugins/org.eclipse.fordiac.ide.hierarchymanager.build/src/org/eclipse/fordiac/ide/hierarchymanager/build/HierarchyRuntimeModule.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Martin Erich Jobst
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Martin Jobst - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.hierarchymanager.build;
+
+import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.resource.generic.AbstractGenericResourceRuntimeModule;
+
+public class HierarchyRuntimeModule extends AbstractGenericResourceRuntimeModule {
+
+	@Override
+	protected String getLanguageName() {
+		return HierarchyLanguageConstants.ORG_ECLIPSE_FORDIAC_IDE_HIERARCHYMANAGER_HIERARCHY;
+	}
+
+	@Override
+	protected String getFileExtensions() {
+		return "hier"; //$NON-NLS-1$
+	}
+
+	@Override
+	public Class<? extends IQualifiedNameProvider> bindIQualifiedNameProvider() {
+		return DefaultDeclarativeQualifiedNameProvider.class;
+	}
+}
