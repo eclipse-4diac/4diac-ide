affab853fc129fda6727a8bf0a6b5415c74b4570
anuragsisodiya <sisodiyaanurag2121@gmail.com>
2025-02-28 20:24:03 +0530

    Fixed resource naming bug

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/impl/ResourceTypeEntryImpl.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/impl/ResourceTypeEntryImpl.java
@@ -15,8 +15,14 @@
  ******************************************************************************/
 package org.eclipse.fordiac.ide.model.typelibrary.impl;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.fordiac.ide.model.dataexport.AbstractTypeExporter;
 import org.eclipse.fordiac.ide.model.dataimport.CommonElementImporter;
@@ -35,6 +41,23 @@ public class ResourceTypeEntryImpl extends AbstractCheckedTypeEntryImpl<Resource
 	@Override
 	public void save(final LibraryElement toSave, final IProgressMonitor monitor) throws CoreException {
 		// currently we can not save resources, but we also have no editor for it
+
+		// Read the existing XML file
+		final Path filePath = getFile().getLocation().toFile().toPath();
+		try {
+			// Read the entire file as a string
+			String xmlContent = new String(Files.readAllBytes(filePath));
+
+			// Update the Name attribute using a regular expression
+			xmlContent = xmlContent.replaceFirst("(<ResourceType[^>]* Name=\")[^\"]*(\"[^>]*>)",
+					"$1" + toSave.getName() + "$2");
+
+			// Write the updated XML back to the file
+			Files.write(filePath, xmlContent.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+		} catch (final Exception e) {
+			throw new CoreException(
+					new Status(IStatus.ERROR, "org.eclipse.fordiac.ide.model", "Error saving LibraryElement", e));
+		}
 	}
 
 	@Override
