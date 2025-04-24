538c2819e1b27583e5616f40442622cc2028a105
Qemal Alliu <qemal.alliu@yahoo.com>
2025-04-24 14:29:43 +0200

    Global Constants: Refactoring and fixes

--- plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/handlers/STCoreFindReferencesHandler.java
+++ plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/handlers/STCoreFindReferencesHandler.java
@@ -11,29 +11,36 @@
  *   Daniel Lindhuber
  *    - initial API and implementation and/or initial documentation
  *******************************************************************************/
-package org.eclipse.fordiac.ide.globalconstantseditor.ui.handlers;
+package org.eclipse.fordiac.ide.structuredtextcore.ui.handlers;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.fordiac.ide.globalconstantseditor.globalConstants.STVarGlobalDeclarationBlock;
 import org.eclipse.fordiac.ide.model.search.ModelQuerySpec;
 import org.eclipse.fordiac.ide.model.search.ModelQuerySpec.SearchScope;
 import org.eclipse.fordiac.ide.model.search.ModelSearchQuery;
 import org.eclipse.fordiac.ide.structuredtextcore.stcore.STVarDeclaration;
 import org.eclipse.search.ui.NewSearchUI;
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.ui.editor.findrefs.FindReferencesHandler;
 
+import com.google.inject.Inject;
+
 @SuppressWarnings("restriction")
-public class GlobalConstantsFindReferencesHandler extends FindReferencesHandler {
+public class STCoreFindReferencesHandler extends FindReferencesHandler {
+	@Inject
+	private IQualifiedNameProvider nameProvider;
 
 	@Override
 	protected void findReferences(final EObject target) {
-		if (target instanceof final STVarDeclaration varDec) {
+		if (target instanceof final STVarDeclaration varDec
+				&& varDec.eContainer() instanceof STVarGlobalDeclarationBlock) {
 			// @formatter:off
 			final ModelQuerySpec searchSpec = new ModelQuerySpec(
-					varDec.getName(),
+					nameProvider.getFullyQualifiedName(varDec).toString("::"), //$NON-NLS-1$
 					false,
 					false,
 					true,
