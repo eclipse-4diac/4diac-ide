0230234afb68bc0ab4cb05710354c15535106bbe
Martin Jobst <martin.jobst@posteo.de>
2025-05-13 20:31:13 +0200

    Rename launch configs to better correspond to launch shortcuts
    Also change to NLS to plugin localization.

--- plugins/org.eclipse.fordiac.ide.deployment.debug/src/org/eclipse/fordiac/ide/deployment/debug/Messages.java
+++ plugins/org.eclipse.fordiac.ide.deployment.debug/src/org/eclipse/fordiac/ide/deployment/debug/Messages.java
@@ -16,7 +16,7 @@ import org.eclipse.osgi.util.NLS;
 
 @SuppressWarnings("squid:S3008") // tell sonar the java naming convention does not make sense for this class
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.fordiac.ide.deployment.debug.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "plugin"; //$NON-NLS-1$
 	public static String AbstractRuntimeWatch_AddWatch;
 	public static String AbstractRuntimeWatch_RemoveWatch;
 	public static String AbstractVariableWatch_Disconnected;
