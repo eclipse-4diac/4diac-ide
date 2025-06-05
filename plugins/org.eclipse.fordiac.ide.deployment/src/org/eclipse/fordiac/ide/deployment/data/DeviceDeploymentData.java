328a68b0b8317ea4601f4b53d33073fef8d3bfbf
Martin Jobst <martin.jobst@posteo.de>
2025-06-05 19:16:38 +0200

    Add query for FB and data types in deployment

--- plugins/org.eclipse.fordiac.ide.deployment/src/org/eclipse/fordiac/ide/deployment/data/DeviceDeploymentData.java
+++ plugins/org.eclipse.fordiac.ide.deployment/src/org/eclipse/fordiac/ide/deployment/data/DeviceDeploymentData.java
@@ -13,20 +13,28 @@ package org.eclipse.fordiac.ide.deployment.data;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.SequencedSet;
 
 import org.eclipse.fordiac.ide.model.libraryElement.Device;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
+import org.eclipse.fordiac.ide.model.typelibrary.DataTypeEntry;
+import org.eclipse.fordiac.ide.model.typelibrary.FBTypeEntry;
 
 public class DeviceDeploymentData {
 
 	private final Device dev;
 
-	private List<ResourceDeploymentData> resData = new ArrayList<>();
+	private final List<ResourceDeploymentData> resData = new ArrayList<>();
 
 	private List<VarDeclaration> selectedDevParams = Collections.emptyList();
 
-	public DeviceDeploymentData(Device device) {
+	private final SequencedSet<FBTypeEntry> fbTypes = new LinkedHashSet<>();
+
+	private final SequencedSet<DataTypeEntry> dataTypes = new LinkedHashSet<>();
+
+	public DeviceDeploymentData(final Device device) {
 		dev = device;
 	}
 
@@ -34,15 +42,17 @@ public class DeviceDeploymentData {
 		return dev;
 	}
 
-	public void addResourceData(ResourceDeploymentData data) {
+	public void addResourceData(final ResourceDeploymentData data) {
 		resData.add(data);
+		fbTypes.addAll(data.getFbTypes());
+		dataTypes.addAll(data.getDataTypes());
 	}
 
 	public List<ResourceDeploymentData> getResData() {
 		return resData;
 	}
 
-	public void setSeltectedDevParams(List<VarDeclaration> selParams) {
+	public void setSeltectedDevParams(final List<VarDeclaration> selParams) {
 		selectedDevParams = Collections.unmodifiableList(new ArrayList<>(selParams));
 	}
 
@@ -54,4 +64,11 @@ public class DeviceDeploymentData {
 		return selectedDevParams;
 	}
 
+	public SequencedSet<FBTypeEntry> getFbTypes() {
+		return fbTypes;
+	}
+
+	public SequencedSet<DataTypeEntry> getDataTypes() {
+		return dataTypes;
+	}
 }
