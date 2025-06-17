15738e25e1bb39b1d9d90910c7cf64a64930b96c
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2025-06-17 14:30:20 +0200

    Added Option to Open BulkEditor with pre-selected SubApps as scope

--- plugins/org.eclipse.fordiac.ide.bulkeditor/src/org/eclipse/fordiac/ide/bulkeditor/editors/OpenBulkEditorHandler.java
+++ plugins/org.eclipse.fordiac.ide.bulkeditor/src/org/eclipse/fordiac/ide/bulkeditor/editors/OpenBulkEditorHandler.java
@@ -12,11 +12,23 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.bulkeditor.editors;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.fordiac.ide.model.libraryElement.UntypedSubApp;
+import org.eclipse.fordiac.ide.model.typelibrary.SubAppTypeEntry;
+import org.eclipse.fordiac.ide.model.typelibrary.TypeLibraryManager;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -24,20 +36,32 @@ import org.eclipse.ui.handlers.HandlerUtil;
 public class OpenBulkEditorHandler extends AbstractHandler {
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
-		final IStructuredSelection selection = HandlerUtil.getCurrentStructuredSelection(event);
-		final Object firstElement = selection.getFirstElement();
+		if (HandlerUtil.getCurrentStructuredSelection(event) instanceof final TreeSelection treeSelection) {
+			final Map<IProject, List<URI>> projectMap = new HashMap<>();
+			Arrays.stream(treeSelection.getPaths()).filter(
+					treePath -> treePath.getFirstSegment() instanceof final IProject project && project.isOpen())
+					.forEach(treePath -> {
+						final IProject project = ((IProject) treePath.getFirstSegment());
+						projectMap.computeIfAbsent(project, key -> new ArrayList<URI>());
 
-		if (firstElement instanceof final IProject project && project.isOpen()) {
-			final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-			try {
-				window.getActivePage().openEditor(new BulkEditorInput(project),
-						"org.eclipse.fordiac.ide.bulkeditor.BulkEditor"); //$NON-NLS-1$
-			} catch (final Exception e) {
-				e.printStackTrace();
-			}
+						if (treePath.getLastSegment() instanceof final UntypedSubApp untypedSubapp) {
+							projectMap.get(project).add(EcoreUtil.getURI(untypedSubapp));
+						} else if (treePath.getLastSegment() instanceof final IFile file && TypeLibraryManager.INSTANCE
+								.getTypeEntryForFile(file) instanceof final SubAppTypeEntry typeEntry) {
+							projectMap.get(project).add(EcoreUtil.getURI(typeEntry.getType()));
+						}
+					});
+
+			projectMap.forEach((project, subAppList) -> {
+				final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+				try {
+					window.getActivePage().openEditor(new BulkEditorInput(project, subAppList),
+							"org.eclipse.fordiac.ide.bulkeditor.BulkEditor"); //$NON-NLS-1$
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+			});
 		}
 		return null;
 	}
-
-	// TODO: disable for closed Projects
 }
