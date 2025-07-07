38d41cfa8fd281b2dadb678691b1fd60ac635e9c
Alois Zoitl <alois.zoitl@gmx.at>
2025-07-07 21:42:34 +0200

    Correctly refresh the system explorer on marker changes

--- plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/navigator/FBTypeContentProvider.java
+++ plugins/org.eclipse.fordiac.ide.typemanagement/src/org/eclipse/fordiac/ide/typemanagement/navigator/FBTypeContentProvider.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2024 TU Wien ACIN, fortiss GmbH
+ * Copyright (c) 2011, 2025 TU Wien ACIN, fortiss GmbH,
+ *                          Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -15,12 +16,26 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.typemanagement.navigator;
 
-import org.eclipse.core.resources.IFile;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FBTypeContentProvider extends LibraryElementContentProvider {
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarkerDelta;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.navigator.CommonViewer;
+
+public class FBTypeContentProvider extends LibraryElementContentProvider implements IResourceChangeListener {
 
 	public FBTypeContentProvider() {
 		super(FBTypeComposedAdapterFactory.getAdapterFactory());
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this,
+				IResourceChangeEvent.POST_CHANGE | IResourceChangeEvent.POST_BUILD);
 	}
 
 	@Override
@@ -37,5 +52,41 @@ public class FBTypeContentProvider extends LibraryElementContentProvider {
 			return true;
 		}
 		return super.hasChildren(element);
+	}
+
+	@Override
+	public void dispose() {
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+		super.dispose();
+	}
+
+	@Override
+	public void resourceChanged(final IResourceChangeEvent event) {
+		try {
+			final List<IResource> list = new ArrayList<>();
+			event.getDelta().accept(delta -> {
+				for (final IMarkerDelta markerDelta : delta.getMarkerDeltas()) {
+					IResource resource = markerDelta.getResource();
+					while (resource != null) {
+						list.add(resource);
+						resource = resource.getParent();
+					}
+				}
+				return true;
+			});
+			if (!list.isEmpty()) {
+				performViewerRefresh(list);
+			}
+		} catch (final CoreException e) {
+			FordiacLogHelper.logError("Couldn't refresh markers", e); //$NON-NLS-1$
+		}
+	}
+
+	private void performViewerRefresh(final List<IResource> list) {
+		Display.getDefault().asyncExec(() -> {
+			if (viewer instanceof final CommonViewer cViewer && !viewer.getControl().isDisposed()) {
+				cViewer.update(list.toArray(), null);
+			}
+		});
 	}
 }
