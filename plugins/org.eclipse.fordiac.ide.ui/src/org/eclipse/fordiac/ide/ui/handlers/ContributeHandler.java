40ecd64b18a2241cee5bb17ec09faa7fe2219520
Alois Zoitl <alois.zoitl@gmx.at>
2025-07-08 14:41:38 +0200

    Prefill Github issue on report bug
    The error dialog allows to report an issue to github with this commit it
    prefills the title and adds the exception with stacktrace to the issue
    body.
    
    Also the link to the contribute page is fixed.

--- plugins/org.eclipse.fordiac.ide.ui/src/org/eclipse/fordiac/ide/ui/handlers/ContributeHandler.java
+++ plugins/org.eclipse.fordiac.ide.ui/src/org/eclipse/fordiac/ide/ui/handlers/ContributeHandler.java
@@ -19,7 +19,6 @@ import java.net.URISyntaxException;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
@@ -27,21 +26,21 @@ import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ContributeHandler extends AbstractHandler {
-	private static final String FORDIAC_CONTRIBUTION_URL = "https://eclipse.dev/4diac/en_contribute.php"; //$NON-NLS-1$
+	private static final String FORDIAC_CONTRIBUTION_URL = "https://eclipse.dev/4diac/contribute/"; //$NON-NLS-1$
 
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		final IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
 		if (null != activeWorkbenchWindow) {
-			IWebBrowser browser;
 			try {
-				browser = activeWorkbenchWindow.getWorkbench().getBrowserSupport().createBrowser(getClass().getName());
+				final IWebBrowser browser = activeWorkbenchWindow.getWorkbench().getBrowserSupport()
+						.createBrowser(getClass().getName());
 				browser.openURL(new URI(FORDIAC_CONTRIBUTION_URL).toURL());
 			} catch (PartInitException | MalformedURLException | URISyntaxException e) {
-				FordiacLogHelper.logError("Error in opening the 4diac contribution web-page!", e); //$NON-NLS-1$
+				FordiacLogHelper.logError("Error in opening the Eclipse 4diac contribution web-page!", e); //$NON-NLS-1$
 			}
 		}
 
-		return Status.OK_STATUS;
+		return null;
 	}
 }
