db2f98e2ecf97e678484827926ff28d2d0dde0c3
Martin Jobst <martin.jobst@posteo.de>
2025-08-22 14:55:07 +0200

    Fix unnecessary editor reload during ST refactoring

---
+++ plugins/org.eclipse.fordiac.ide.structuredtextcore.ui/src/org/eclipse/fordiac/ide/structuredtextcore/ui/refactoring/EditorDocumentChange.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Martin Erich Jobst
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Martin Jobst - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.structuredtextcore.ui.refactoring;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.fordiac.ide.structuredtextcore.ui.editor.STCoreNestedEditor;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.text.edits.UndoEdit;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+
+public class EditorDocumentChange extends ProviderDocumentChange {
+
+	private final IEditorPart editorPart;
+
+	public EditorDocumentChange(final String name, final IFileEditorInput editorInput,
+			final IDocumentProvider documentProvider, final IEditorPart editorPart) {
+		super(name, editorInput, documentProvider);
+		this.editorPart = editorPart;
+	}
+
+	public EditorDocumentChange(final String name, final IFileEditorInput editorInput,
+			final IDocumentProvider documentProvider, final IEditorPart editorPart, final boolean doSave) {
+		super(name, editorInput, documentProvider, doSave);
+		this.editorPart = editorPart;
+	}
+
+	@Override
+	protected void commit(final IDocument document, final IProgressMonitor pm) throws CoreException {
+		try {
+			if (editorPart instanceof final STCoreNestedEditor nestedEditor) {
+				nestedEditor.setBlockUpdates(true);
+			}
+			super.commit(document, pm);
+		} finally {
+			if (editorPart instanceof final STCoreNestedEditor nestedEditor) {
+				nestedEditor.setBlockUpdates(false);
+			}
+		}
+	}
+
+	@Override
+	protected Change createUndoChange(final UndoEdit edit) {
+		return new EditorDocumentUndoChange(getName(), getEditorInput(), getDocumentProvider(), edit, editorPart,
+				isDoSave());
+	}
+}
