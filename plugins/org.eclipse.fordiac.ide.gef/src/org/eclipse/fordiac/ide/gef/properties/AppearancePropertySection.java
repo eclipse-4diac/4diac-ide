66795aae92cc493764d11f8d2b221dba3b9feeba
Alois Zoitl <alois.zoitl@gmx.at>
2025-09-02 17:16:46 +0200

    Turned AppearenceProperty Sheet into a single selection sheet

--- plugins/org.eclipse.fordiac.ide.gef/src/org/eclipse/fordiac/ide/gef/properties/AppearancePropertySection.java
+++ plugins/org.eclipse.fordiac.ide.gef/src/org/eclipse/fordiac/ide/gef/properties/AppearancePropertySection.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009, 2011 - 2017 Profactor GmbH, TU Wien ACIN, fortiss GmbH
+ * Copyright (c) 2008, 2025 Profactor GmbH, TU Wien ACIN, fortiss GmbH,
+ *                          Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -8,13 +9,10 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *   Gerahrd Ebenhofer, Alois Zoitl
+ *   Gerhard Ebenhofer, Alois Zoitl
  *     - initial API and implementation and/or initial documentation
  *******************************************************************************/
 package org.eclipse.fordiac.ide.gef.properties;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.fordiac.ide.gef.Messages;
@@ -23,7 +21,6 @@ import org.eclipse.fordiac.ide.model.commands.change.ChangeBackgroundcolorComman
 import org.eclipse.fordiac.ide.model.libraryElement.ColorizableElement;
 import org.eclipse.fordiac.ide.model.libraryElement.SystemConfiguration;
 import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CompoundCommand;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
@@ -45,7 +42,6 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
 public class AppearancePropertySection extends AbstractPropertySection {
 	private ColorizableElement colorizableElement;
-	private final List<ColorizableElement> selectedViews = new ArrayList<>();
 	private Color color;
 	private Label colorLabel;
 	private Group colorsGroup;
@@ -67,20 +63,8 @@ public class AppearancePropertySection extends AbstractPropertySection {
 		if (input instanceof AbstractViewEditPart
 				&& ((AbstractViewEditPart) input).getModel() instanceof ColorizableElement) {
 			colorizableElement = (ColorizableElement) ((AbstractViewEditPart) input).getModel();
-		} else {
-			if (input instanceof ColorizableElement) {
-				colorizableElement = (ColorizableElement) input;
-			}
-		}
-		selectedViews.clear();
-		for (final Object object : ((IStructuredSelection) selection)) {
-			if (object instanceof AbstractViewEditPart
-					&& ((AbstractViewEditPart) object).getModel() instanceof ColorizableElement) {
-				selectedViews.add((ColorizableElement) ((AbstractViewEditPart) object).getModel());
-			}
-		}
-		if (selectedViews.size() <= 1) {
-			selectedViews.clear();
+		} else if (input instanceof ColorizableElement) {
+			colorizableElement = (ColorizableElement) input;
 		}
 	}
 
@@ -88,15 +72,12 @@ public class AppearancePropertySection extends AbstractPropertySection {
 	public void refresh() {
 		if (colorizableElement != null && colorizableElement.getColor() != null) {
 			final org.eclipse.fordiac.ide.model.libraryElement.Color col = colorizableElement.getColor();
-			color.dispose();
-			color = new Color(null, new RGB(col.getRed(), col.getGreen(), col.getBlue()));
+			color = new Color(new RGB(col.getRed(), col.getGreen(), col.getBlue()));
 			colorLabel.setBackground(this.color);
 		} else {
-			color.dispose();
-			color = new Color(null, new RGB(255, 255, 255));
+			color = new Color(new RGB(255, 255, 255));
 			colorLabel.setBackground(this.color);
 		}
-
 		super.refresh();
 	}
 
@@ -105,14 +86,13 @@ public class AppearancePropertySection extends AbstractPropertySection {
 	}
 
 	protected void createColorsGroup(final Composite parent) {
-		colorsGroup = getWidgetFactory().createGroup(parent, Messages.AppearancePropertySection_LABLE_Color);
+		colorsGroup = getWidgetFactory().createGroup(parent, Messages.AppearancePropertySection_LABEL_Color);
 		final GridLayout layout = new GridLayout(1, false);
 		colorsGroup.setLayout(layout);
 		// Start with Celtics green
 		color = new Color(null, new RGB(255, 255, 255));
 		// Use a label full of spaces to show the color
-		colorLabel = getWidgetFactory().createLabel(colorsGroup, ""); //$NON-NLS-1$
-		colorLabel.setText("          "); //$NON-NLS-1$
+		colorLabel = getWidgetFactory().createLabel(colorsGroup, "          "); //$NON-NLS-1$
 		colorLabel.setBackground(color);
 		final GridData gd = new GridData();
 		gd.grabExcessHorizontalSpace = true;
@@ -126,29 +106,17 @@ public class AppearancePropertySection extends AbstractPropertySection {
 				final ColorDialog dlg = new ColorDialog(colorsGroup.getShell());
 				// Set the selected color in the dialog from
 				// user's selected color
-				dlg.setRGB(colorLabel.getBackground().getRGB());
+				dlg.setRGB(color.getRGB());
 				// Change the title bar text
 				dlg.setText(Messages.AppearancePropertySection_LABEL_ChooseColor);
 				// Open the dialog and retrieve the selected color
 				final RGB rgb = dlg.open();
 				if (rgb != null) {
-					// Dispose the old color, create the
-					// new one, and set into the label
-					Command cmd;
-					if (!selectedViews.isEmpty()) {
-						cmd = new CompoundCommand(Messages.AppearancePropertySection_ChangeBackgroundColor);
-						for (final ColorizableElement ce : selectedViews) {
-							final ChangeBackgroundcolorCommand tmp = new ChangeBackgroundcolorCommand(ce, rgb);
-							((CompoundCommand) cmd).add(tmp);
-						}
-					} else {
-						cmd = new ChangeBackgroundcolorCommand(colorizableElement, rgb);
-					}
+					final Command cmd = new ChangeBackgroundcolorCommand(colorizableElement, rgb);
 					if (cmd.canExecute()) {
 						((SystemConfiguration) colorizableElement.eContainer()).getAutomationSystem().getCommandStack()
-						.execute(cmd);
-						color.dispose();
-						color = new Color(null, rgb);
+								.execute(cmd);
+						color = new Color(rgb);
 						colorLabel.setBackground(color);
 					}
 				}
