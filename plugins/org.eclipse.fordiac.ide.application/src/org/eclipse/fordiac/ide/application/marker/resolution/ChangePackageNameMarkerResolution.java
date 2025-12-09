8be059a998f8c19b48a191d9c24b178761595f88
Felix Schmid <50250674+Felix-Schmid@users.noreply.github.com>
2025-12-09 09:40:00 +0100

    Offer quickfix for package validation issue

---
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/marker/resolution/ChangePackageNameMarkerResolution.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Felix Schmid
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Felix Schmid - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.application.marker.resolution;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.fordiac.ide.application.Messages;
+import org.eclipse.fordiac.ide.model.helpers.PackageNameHelper;
+import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
+import org.eclipse.fordiac.ide.model.typelibrary.TypeEntry;
+import org.eclipse.fordiac.ide.model.typelibrary.TypeLibraryManager;
+
+public class ChangePackageNameMarkerResolution extends ChangeNameMarkerResolution {
+
+	protected ChangePackageNameMarkerResolution(final IMarker marker) {
+		super(marker);
+	}
+
+	@Override
+	public String getDescription() {
+		return Messages.ChangePackageName_QuickfixLabel;
+	}
+
+	@Override
+	public String getLabel() {
+		return Messages.ChangePackageName_QuickfixLabel;
+	}
+
+	@Override
+	protected void runInWorkspace(final IMarker marker) throws CoreException {
+		if (!(marker.getResource() instanceof final IFile file)) {
+			throw createExceptionForMarker(Messages.ChangeName_NoFileError, marker);
+		}
+		final TypeEntry typeEntry = TypeLibraryManager.INSTANCE.getTypeEntryForFile(file);
+		if (typeEntry == null) {
+			throw createExceptionForMarker(Messages.ChangeName_NoTypeEntryError, marker);
+		}
+		final LibraryElement type = typeEntry.getType();
+		if (type == null) {
+			throw createExceptionForMarker(Messages.ChangeName_NoTypeError, marker);
+		}
+		PackageNameHelper.setPackageName(type, PackageNameHelper.getPackageNameFromFile(typeEntry.getFile()));
+		typeEntry.save(type);
+	}
+}
