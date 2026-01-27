a31864d577aac654227ce94c47d8c187a3c10ea0
Michael Oberlehner <127855312+oberlehner@users.noreply.github.com>
2026-01-27 10:07:52 +0100

    Fix SystemExplorerPasteAction project paste to restore "Copy Project" behavior

--- plugins/org.eclipse.fordiac.ide.systemmanagement.ui/src/org/eclipse/fordiac/ide/systemmanagement/ui/Messages.java
+++ plugins/org.eclipse.fordiac.ide.systemmanagement.ui/src/org/eclipse/fordiac/ide/systemmanagement/ui/Messages.java
@@ -80,6 +80,17 @@ public final class Messages extends NLS {
 	public static String WrongBuilderOrderMarkerResolution_Label;
 
 	public static String SystemExplorer_CopyPackageNameAction;
+
+	public static String SystemExplorerPasteAction_CopyProject;
+
+	public static String SystemExplorerPasteAction_newProjectName;
+
+	public static String SystemExplorerPasteAction_nonEmptyProject;
+
+	public static String SystemExplorerPasteAction_projectExists;
+
+	public static String SystemExplorerPasteAction_copy;
+
 	public static String CopyActionErrorRetryTitle;
 	public static String CopyActionErrorRetryMessage;
 
