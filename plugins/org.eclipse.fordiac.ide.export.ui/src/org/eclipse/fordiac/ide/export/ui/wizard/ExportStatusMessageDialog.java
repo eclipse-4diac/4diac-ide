6065412ae71a6487f722e14668c3d25c579344c9
Martin Jobst <martin.jobst@posteo.de>
2026-01-27 12:06:54 +0100

    Show infos in export status dialog

--- plugins/org.eclipse.fordiac.ide.export.ui/src/org/eclipse/fordiac/ide/export/ui/wizard/ExportStatusMessageDialog.java
+++ plugins/org.eclipse.fordiac.ide.export.ui/src/org/eclipse/fordiac/ide/export/ui/wizard/ExportStatusMessageDialog.java
@@ -16,6 +16,8 @@ package org.eclipse.fordiac.ide.export.ui.wizard;
 
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -34,19 +36,23 @@ public class ExportStatusMessageDialog extends ErrorDialog {
 
 	private final List<String> warnings;
 	private final List<String> errors;
+	private final List<String> infos;
 
 	private StyledText text;
 
-	public ExportStatusMessageDialog(final Shell parentShell, final List<String> warnings, final List<String> errors) {
+	public ExportStatusMessageDialog(final Shell parentShell, final List<String> warnings, final List<String> errors,
+			final List<String> infos) {
 		super(parentShell, Messages.ExportStatusMessageDialog_4diacIDETypeExportErrors,
 				Messages.ExportStatusMessageDialog_DuringTypeExportTheFollowingIssuesHaveBeenIdentified,
 				new Status(IStatus.INFO, "org.eclipse.fordiac.ide.export", //$NON-NLS-1$
 						MessageFormat.format(Messages.ExportStatusMessageDialog_ExportStatusMessageDialog,
-								Integer.valueOf(errors.size()), Integer.valueOf(warnings.size()))),
+								Integer.valueOf(errors.size()), Integer.valueOf(warnings.size()),
+								Integer.valueOf(infos.size()))),
 				IStatus.OK | IStatus.INFO | IStatus.WARNING | IStatus.ERROR);
 
 		this.warnings = warnings;
 		this.errors = errors;
+		this.infos = infos;
 	}
 
 	@Override
@@ -80,46 +86,26 @@ public class ExportStatusMessageDialog extends ErrorDialog {
 
 	/** writes all the messages ( warnings and errors) in the text field. */
 	public void printMessages() {
-
 		text.setText(""); //$NON-NLS-1$
-		int count = 0;
-
 		if (!errors.isEmpty()) {
-			count += printMessageList(Messages.ExportStatusMessageDialog_ErrorsNotEmpty, errors, count);
+			printMessageList(Messages.ExportStatusMessageDialog_ErrorsNotEmpty, errors);
 		}
-
 		if (!warnings.isEmpty()) {
-			printMessageList(Messages.ExportStatusMessageDialog_WarningsNotEmpty, warnings, count);
+			printMessageList(Messages.ExportStatusMessageDialog_WarningsNotEmpty, warnings);
+		}
+		if (!infos.isEmpty()) {
+			printMessageList(Messages.ExportStatusMessageDialog_InfosNotEmpty, infos);
 		}
 	}
 
-	private int printMessageList(final String title, final List<String> messages, final int startCount) {
-		int count = startCount;
-
+	private void printMessageList(final String title, final List<String> messages) {
+		final StyleRange titleStyle = new StyleRange();
+		titleStyle.start = text.getCharCount();
+		titleStyle.length = title.length();
+		titleStyle.fontStyle = SWT.BOLD;
 		text.append(title);
-		final StyleRange style1 = new StyleRange();
-		style1.start = count;
-		style1.length = title.length();
-		style1.fontStyle = SWT.BOLD;
-		text.setStyleRange(style1);
-
-		count += title.length();
-		count += addLines(messages);
-		text.append("\n"); //$NON-NLS-1$
-		count += "\n".length(); //$NON-NLS-1$
-		return count;
-	}
-
-	private int addLines(final List<String> messages) {
-		int count = 0;
-
-		for (final String string : messages) {
-			if (null != string) {
-				final String line = "    - " + string + "\n"; //$NON-NLS-1$ //$NON-NLS-2$
-				text.append(line);
-				count += line.length();
-			}
-		}
-		return count;
+		text.setStyleRange(titleStyle);
+		text.append(messages.stream().filter(Objects::nonNull).map("    - "::concat) //$NON-NLS-1$
+				.collect(Collectors.joining("\n", "", "\n\n"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 }
