19655f4e6416fe49a3f90bbab3b769bb62b3aa66
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2026-01-29 13:41:40 +0100

    Added CanExecute to TargetLabelReconnect

--- plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/commands/TargetLabelReconnectCommand.java
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/commands/TargetLabelReconnectCommand.java
@@ -12,9 +12,16 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.application.commands;
 
+import java.text.MessageFormat;
+
+import org.eclipse.fordiac.ide.model.Messages;
+import org.eclipse.fordiac.ide.model.data.StructuredType;
 import org.eclipse.fordiac.ide.model.libraryElement.Event;
 import org.eclipse.fordiac.ide.model.libraryElement.IInterfaceElement;
 import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
+import org.eclipse.fordiac.ide.model.validation.LinkConstraints;
+import org.eclipse.fordiac.ide.ui.FordiacMessages;
+import org.eclipse.fordiac.ide.ui.errormessages.ErrorMessenger;
 import org.eclipse.gef.commands.Command;
 
 public class TargetLabelReconnectCommand extends Command {
@@ -28,6 +35,58 @@ public class TargetLabelReconnectCommand extends Command {
 	public TargetLabelReconnectCommand(final IInterfaceElement source, final IInterfaceElement destination) {
 		this.source = source;
 		this.destination = destination;
+	}
+
+	@Override
+	public boolean canExecute() {
+		if (source == null || destination == null) {
+			return false;
+		}
+
+		// equal types for source and dest
+		if (!source.getClass().isAssignableFrom(destination.getClass())
+				&& !destination.getClass().isAssignableFrom(source.getClass())) {
+			ErrorMessenger.popUpErrorMessage(Messages.LinkConstraints_ConnectingIncompatibleInterfaceTypes);
+			return false;
+		}
+
+		// source and dest check
+		if (!LinkConstraints.isValidConnSource(source, source.getBlockFBNetworkElement().getFbNetwork())
+				|| !LinkConstraints.isValidConnDestination(destination,
+						destination.getBlockFBNetworkElement().getFbNetwork())) {
+			ErrorMessenger.popUpErrorMessage(Messages.LinkConstraints_STATUSMessage_IN_IN_OUT_OUT_notAllowed);
+			return false;
+		}
+
+		if (source instanceof Event) {
+			return true;
+		}
+
+		if (source instanceof VarDeclaration) {
+			return dataConnectionChecks();
+		}
+
+		return false;
+	}
+
+	private boolean dataConnectionChecks() {
+		if (!(source.getType() instanceof StructuredType && destination.getType() instanceof StructuredType)
+				&& (!LinkConstraints.typeCheck(source, destination))) {
+			ErrorMessenger.popUpErrorMessage(MessageFormat.format(Messages.LinkConstraints_STATUSMessage_NotCompatible,
+					(null != source.getType()) ? source.getType().getName() : FordiacMessages.NA,
+					(null != destination.getType()) ? destination.getType().getName() : FordiacMessages.NA));
+			return false;
+
+		}
+
+		if (destination instanceof final VarDeclaration targetData && targetData.isInOutVar()
+				&& source instanceof final VarDeclaration sourceData && !sourceData.isInOutVar()) {
+			// a non inout is connected to an inout
+			ErrorMessenger.popUpErrorMessage(Messages.ConnectionValidator_OutputsCannotBeConnectedToVarInOuts);
+			return false;
+		}
+
+		return LinkConstraints.isWithConstraintOK(source) && LinkConstraints.isWithConstraintOK(destination);
 	}
 
 	@Override
