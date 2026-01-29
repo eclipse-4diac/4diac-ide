ce4878f4cc17816cc8d92bf4896469564ecd1113
Jose Cabral <cabralcochi@gmail.com>
2026-01-29 07:53:00 +0100

    Create runtimes recursively

--- plugins/org.eclipse.fordiac.ide.fb.interpreter/src/org/eclipse/fordiac/ide/fb/interpreter/api/RuntimeFactory.java
+++ plugins/org.eclipse.fordiac.ide.fb.interpreter/src/org/eclipse/fordiac/ide/fb/interpreter/api/RuntimeFactory.java
@@ -13,7 +13,10 @@
  *******************************************************************************/
 package org.eclipse.fordiac.ide.fb.interpreter.api;
 
+import java.util.List;
+
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.fordiac.ide.fb.interpreter.OpSem.BasicFBTypeRuntime;
 import org.eclipse.fordiac.ide.fb.interpreter.OpSem.CompositeFBTypeRuntime;
 import org.eclipse.fordiac.ide.fb.interpreter.OpSem.FBNetworkRuntime;
@@ -22,6 +25,7 @@ import org.eclipse.fordiac.ide.fb.interpreter.OpSem.FunctionFBTypeRuntime;
 import org.eclipse.fordiac.ide.fb.interpreter.OpSem.OperationalSemanticsFactory;
 import org.eclipse.fordiac.ide.fb.interpreter.OpSem.ServiceInterfaceFBTypeRuntime;
 import org.eclipse.fordiac.ide.fb.interpreter.OpSem.SimpleFBTypeRuntime;
+import org.eclipse.fordiac.ide.fb.interpreter.mm.VariableUtils;
 import org.eclipse.fordiac.ide.model.libraryElement.BasicFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.CompositeFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.ECState;
@@ -32,6 +36,8 @@ import org.eclipse.fordiac.ide.model.libraryElement.FunctionFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.ServiceInterfaceFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.SimpleFBType;
 import org.eclipse.fordiac.ide.model.libraryElement.SubApp;
+import org.eclipse.fordiac.ide.model.libraryElement.UntypedSubApp;
+import org.eclipse.fordiac.ide.model.libraryElement.VarDeclaration;
 import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
 
 public final class RuntimeFactory {
@@ -110,6 +116,70 @@ public final class RuntimeFactory {
 		final FBNetworkRuntime networkRT = OperationalSemanticsFactory.eINSTANCE.createFBNetworkRuntime();
 		networkRT.setFbnetwork(app);
 		return networkRT;
+	}
+
+	public static FBNetworkRuntime createRecursiveFrom(final FBNetwork network) {
+		final var networkRT = createFrom(network);
+		createInternalRuntimes(networkRT);
+		return networkRT;
+	}
+
+	private static void createInternalRuntimes(final FBNetworkRuntime containerRuntime) {
+		containerRuntime.getFbnetwork().getBlockFBNetworkElements().forEach(networkElement -> {
+
+			if (containerRuntime.getTypeRuntimes().get(networkElement) != null) {
+				return; // runtime already created
+			}
+
+			// handle creation of untyped subapp runtime separately
+			if (networkElement instanceof final UntypedSubApp originalSubApp) {
+				var possibleNetwork = originalSubApp.loadSubAppNetwork();
+				var subAppWithNetowrk = originalSubApp;
+				if (possibleNetwork == null) {
+					// subApps in resources don't contain a network. Only the opposite does
+					subAppWithNetowrk = (UntypedSubApp) originalSubApp.getOpposite();
+					possibleNetwork = subAppWithNetowrk.loadSubAppNetwork();
+
+				}
+				final FBNetworkRuntime networkRuntime = RuntimeFactory.createRecursiveFrom(possibleNetwork);
+				networkRuntime.setOuterNetworkRuntime(containerRuntime);
+				containerRuntime.getTypeRuntimes().put(originalSubApp, networkRuntime);
+
+				// initialize transfer data from input vars (const values in inputs)
+				initializeTransferData(subAppWithNetowrk.getInterface().getInputVars(), networkRuntime);
+
+				createInternalRuntimes(networkRuntime);
+				return;
+			}
+
+			final FBType copiedType = EcoreUtil.copy(networkElement.getType());
+			VariableUtils.initializeFbType(copiedType);
+			final var fbRuntime = RuntimeFactory.createFrom(copiedType);
+			containerRuntime.getTypeRuntimes().put(networkElement, fbRuntime);
+
+			// handle composite FBs
+			if (fbRuntime instanceof final CompositeFBTypeRuntime compositeRuntime) {
+				final var compositeNetworkRuntime = compositeRuntime.getNetworkRuntime();
+				compositeNetworkRuntime.setOuterNetworkRuntime(containerRuntime);
+
+				// initialize transfer data from input vars
+				initializeTransferData(networkElement.getInterface().getInputVars(), compositeNetworkRuntime);
+
+				createInternalRuntimes(compositeNetworkRuntime);
+			}
+
+		});
+	}
+
+	private static void initializeTransferData(final List<VarDeclaration> inputVars,
+			final FBNetworkRuntime networkRuntime) {
+		inputVars.forEach(inputVar -> {
+			final var connections = inputVar.getOutputConnections();
+			for (final var conn : connections) {
+				final var value = inputVar.getValue();
+				networkRuntime.getTransferData().put(conn, EcoreUtil.copy(value));
+			}
+		});
 	}
 
 	public static void setStartState(final FBRuntimeAbstract fbRT, final String startStateName) {
