a59a8d79613a0c0de310f5b513eb27d200fadb65
Martin Jobst <martin.jobst@posteo.de>
2026-02-11 17:25:16 +0100

    Create type entries for a specific type class

--- plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/impl/TypeEntryFactory.java
+++ plugins/org.eclipse.fordiac.ide.model/src/org/eclipse/fordiac/ide/model/typelibrary/impl/TypeEntryFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 Primetals Technologies Austria GmbH
+ * Copyright (c) 2022 Primetals Technologies Austria GmbH, Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *    Alois Zoitl - initial API and implementation and/or initial documentation
+ *    Martin Erich Jobst - add create type entry for classes
  ******************************************************************************/
 package org.eclipse.fordiac.ide.model.typelibrary.impl;
 
@@ -19,6 +20,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.fordiac.ide.model.typelibrary.ITypeEntryCreator;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeEntry;
 import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
@@ -37,14 +39,23 @@ public enum TypeEntryFactory {
 	}
 
 	public TypeEntry createTypeEntry(final IFile file) {
-		TypeEntry entry = null;
 		for (final ITypeEntryCreator in : typeCreators) {
 			if (in.canHandle(file)) {
-				entry = in.createTypeEntry();
+				final TypeEntry entry = in.createTypeEntry();
 				configureTypeEntry(entry, file);
+				return entry;
 			}
 		}
-		return entry;
+		return null;
+	}
+
+	public TypeEntry createTypeEntry(final EClass eClass) {
+		for (final ITypeEntryCreator in : typeCreators) {
+			if (in.canHandle(eClass)) {
+				return in.createTypeEntry();
+			}
+		}
+		return null;
 	}
 
 	private static ITypeEntryCreator[] loadTypeCreators() {
