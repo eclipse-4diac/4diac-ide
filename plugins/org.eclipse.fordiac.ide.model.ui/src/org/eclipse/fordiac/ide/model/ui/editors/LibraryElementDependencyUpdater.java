14b933633f5c23d60c739aad4fdabd772f6607a8
Martin Jobst <martin.jobst@posteo.de>
2026-02-16 17:06:37 +0100

    Reload type entry and handle name changes during dependency update

--- plugins/org.eclipse.fordiac.ide.model.ui/src/org/eclipse/fordiac/ide/model/ui/editors/LibraryElementDependencyUpdater.java
+++ plugins/org.eclipse.fordiac.ide.model.ui/src/org/eclipse/fordiac/ide/model/ui/editors/LibraryElementDependencyUpdater.java
@@ -23,6 +23,7 @@ import org.eclipse.fordiac.ide.model.commands.change.ConfigureFBCommand;
 import org.eclipse.fordiac.ide.model.commands.change.UpdateFBTypeCommand;
 import org.eclipse.fordiac.ide.model.commands.change.UpdateInternalFBCommand;
 import org.eclipse.fordiac.ide.model.data.AnyDerivedType;
+import org.eclipse.fordiac.ide.model.data.DataType;
 import org.eclipse.fordiac.ide.model.libraryElement.AdapterDeclaration;
 import org.eclipse.fordiac.ide.model.libraryElement.AdapterType;
 import org.eclipse.fordiac.ide.model.libraryElement.Attribute;
@@ -37,8 +38,11 @@ import org.eclipse.fordiac.ide.model.libraryElement.StructManipulator;
 import org.eclipse.fordiac.ide.model.typelibrary.AdapterTypeEntry;
 import org.eclipse.fordiac.ide.model.typelibrary.AttributeTypeEntry;
 import org.eclipse.fordiac.ide.model.typelibrary.DataTypeEntry;
+import org.eclipse.fordiac.ide.model.typelibrary.FBTypeEntry;
 import org.eclipse.fordiac.ide.model.typelibrary.InterfaceTypeEntry;
+import org.eclipse.fordiac.ide.model.typelibrary.SubAppTypeEntry;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeEntry;
+import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.swt.widgets.Display;
 
@@ -50,19 +54,30 @@ public class LibraryElementDependencyUpdater extends LibraryElementDependencyTra
 	@Override
 	public void notifyChanged(final Notification notification) {
 		if (notification.getNotifier() instanceof final TypeEntry dependency) {
-			if (getDependencies().contains(notification.getNotifier()) && isRelevant(notification)) {
-				Display.getDefault().execute(() -> updateDependency(dependency));
+			if (getDependencies().contains(dependency)) {
+				// react to TYPE_ENTRY_FILE_CONTENT_FEATURE for changed dependencies
+				// react to TYPE_ENTRY_TYPE_LIBRARY_FEATURE for deleted dependencies
+				if (TypeEntry.TYPE_ENTRY_FILE_CONTENT_FEATURE.equals(notification.getFeature())
+						|| TypeEntry.TYPE_ENTRY_TYPE_LIBRARY_FEATURE.equals(notification.getFeature())) {
+					Display.getDefault().asyncExec(() -> updateDependency(dependency, dependency.getFullTypeName()));
+				}
+				// react to TYPE_ENTRY_FULL_TYPE_NAME_FEATURE for renamed dependencies
+				if (TypeEntry.TYPE_ENTRY_FULL_TYPE_NAME_FEATURE.equals(notification.getFeature())) {
+					Display.getDefault()
+							.asyncExec(() -> updateDependency(dependency, notification.getOldStringValue()));
+				}
 			}
 		} else {
 			super.notifyChanged(notification);
 		}
 	}
 
-	private static boolean isRelevant(final Notification notification) {
-		return TypeEntry.TYPE_ENTRY_FILE_CONTENT_FEATURE.equals(notification.getFeature());
-	}
+	public void updateDependency(final TypeEntry dependency, final String fullTypeName) {
+		final TypeLibrary typeLibrary = libraryElement.getTypeLibrary();
+		if (typeLibrary == null) {
+			return;
+		}
 
-	public void updateDependency(final TypeEntry dependency) {
 		if (updating) {
 			throw new IllegalStateException(
 					"Already updating dependencies for " + libraryElement.getTypeEntry().getFile()); //$NON-NLS-1$
@@ -70,10 +85,14 @@ public class LibraryElementDependencyUpdater extends LibraryElementDependencyTra
 		try {
 			updating = true;
 			switch (dependency) {
-			case final AttributeTypeEntry attributeTypeEntry -> updateAttributeDependency(attributeTypeEntry);
-			case final DataTypeEntry dataTypeEntry -> updateDataTypeDependency(dataTypeEntry);
-			case final AdapterTypeEntry adapterTypeEntry -> updateAdapterDependency(adapterTypeEntry);
-			case final InterfaceTypeEntry interfaceTypeEntry -> updateBlockDependency(interfaceTypeEntry);
+			case final AttributeTypeEntry attributeTypeEntry ->
+				updateAttributeDependency(attributeTypeEntry, fullTypeName, typeLibrary);
+			case final DataTypeEntry dataTypeEntry ->
+				updateDataTypeDependency(dataTypeEntry, fullTypeName, typeLibrary);
+			case final AdapterTypeEntry adapterTypeEntry ->
+				updateAdapterDependency(adapterTypeEntry, fullTypeName, typeLibrary);
+			case final InterfaceTypeEntry interfaceTypeEntry ->
+				updateBlockDependency(interfaceTypeEntry, fullTypeName, typeLibrary);
 			case null, default -> {
 				// do nothing
 			}
@@ -83,16 +102,15 @@ public class LibraryElementDependencyUpdater extends LibraryElementDependencyTra
 		}
 	}
 
-	private void updateAttributeDependency(final AttributeTypeEntry dependency) {
-		final AttributeDeclaration attributeDeclaration = dependency.getType();
-		if (attributeDeclaration == null) {
-			return;
-		}
-		final AnyDerivedType attributeDataType = attributeDeclaration.getType();
-		if (attributeDataType == null) {
-			return;
+	private void updateAttributeDependency(final AttributeTypeEntry dependency, final String fullTypeName,
+			final TypeLibrary typeLibrary) {
+		AttributeTypeEntry entry = typeLibrary.getAttributeTypeEntry(fullTypeName);
+		if (entry == null) {
+			entry = (AttributeTypeEntry) typeLibrary.createErrorTypeEntry(fullTypeName, dependency.getTypeEClass());
 		}
 
+		final AttributeDeclaration attributeDeclaration = entry.getType();
+		final AnyDerivedType attributeDataType = attributeDeclaration.getType();
 		final TreeIterator<EObject> contents = libraryElement.eAllContents();
 		while (contents.hasNext()) {
 			switch (contents.next()) {
@@ -107,19 +125,16 @@ public class LibraryElementDependencyUpdater extends LibraryElementDependencyTra
 		}
 	}
 
-	private void updateDataTypeDependency(final DataTypeEntry dependency) {
-		final AnyDerivedType dataType = dependency.getType();
-		if (dataType == null) {
-			return;
-		}
-
+	private void updateDataTypeDependency(final DataTypeEntry dependency, final String fullTypeName,
+			final TypeLibrary typeLibrary) {
+		final DataType dataType = typeLibrary.getDataTypeLibrary().getType(fullTypeName);
 		final TreeIterator<EObject> contents = libraryElement.eAllContents();
 		while (contents.hasNext()) {
 			switch (contents.next()) {
 			case final Attribute attribute when matches(attribute.getType(), dependency) -> attribute.setType(dataType);
 			case final AttributeDeclaration attributeDeclaration when matches(attributeDeclaration.getType(),
-					dependency) ->
-				attributeDeclaration.setType(dataType);
+					dependency) && dataType instanceof final AnyDerivedType derivedType ->
+				attributeDeclaration.setType(derivedType);
 			case final IInterfaceElement interfaceElement when matches(interfaceElement.getType(), dependency) ->
 				interfaceElement.setType(dataType);
 			case final StructManipulator structManipulator when matches(structManipulator.getDataType(),
@@ -138,12 +153,14 @@ public class LibraryElementDependencyUpdater extends LibraryElementDependencyTra
 		}
 	}
 
-	private void updateAdapterDependency(final AdapterTypeEntry dependency) {
-		final AdapterType adapterType = dependency.getType();
-		if (adapterType == null) {
-			return;
+	private void updateAdapterDependency(final AdapterTypeEntry dependency, final String fullTypeName,
+			final TypeLibrary typeLibrary) {
+		AdapterTypeEntry entry = typeLibrary.getAdapterTypeEntry(fullTypeName);
+		if (entry == null) {
+			entry = (AdapterTypeEntry) typeLibrary.createErrorTypeEntry(fullTypeName, dependency.getTypeEClass());
 		}
 
+		final AdapterType adapterType = entry.getType();
 		final TreeIterator<EObject> contents = libraryElement.eAllContents();
 		while (contents.hasNext()) {
 			switch (contents.next()) {
@@ -158,16 +175,26 @@ public class LibraryElementDependencyUpdater extends LibraryElementDependencyTra
 		}
 	}
 
-	private void updateBlockDependency(final InterfaceTypeEntry dependency) {
+	private void updateBlockDependency(final InterfaceTypeEntry dependency, final String fullTypeName,
+			final TypeLibrary typeLibrary) {
+		TypeEntry entry = switch (dependency) {
+		case final FBTypeEntry fb -> typeLibrary.getFBTypeEntry(fullTypeName);
+		case final SubAppTypeEntry subApp -> typeLibrary.getSubAppTypeEntry(fullTypeName);
+		default -> typeLibrary.find(fullTypeName);
+		};
+		if (entry == null) {
+			entry = typeLibrary.createErrorTypeEntry(fullTypeName, dependency.getTypeEClass());
+		}
+
 		final TreeIterator<EObject> contents = libraryElement.eAllContents();
 		while (contents.hasNext()) {
 			switch (contents.next()) {
 			case final FB fb when matches(fb.getType(), dependency)
 					&& fb.eContainingFeature() == LibraryElementPackage.Literals.BASE_FB_TYPE__INTERNAL_FBS ->
-				executeCommand(new UpdateInternalFBCommand(fb, dependency));
+				executeCommand(new UpdateInternalFBCommand(fb, entry));
 			case final BlockFBNetworkElement element when matches(element.getType(), dependency) -> {
 				if (!element.isMapped() || element.getMapping().getFrom() == element) {
-					executeCommand(new UpdateFBTypeCommand(element, dependency));
+					executeCommand(new UpdateFBTypeCommand(element, entry));
 				}
 				contents.prune(); // contents handled by command or opposite
 			}
