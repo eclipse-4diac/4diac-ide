13ef399a1c2b386c77e7671dafaec39667ac8c2f
Martin Jobst <martin.jobst@posteo.de>
2026-03-04 13:59:22 +0100

    Filter invalid internal FB types in property sheet

--- plugins/org.eclipse.fordiac.ide.model.ui/src/org/eclipse/fordiac/ide/model/ui/nat/FBTypeSelectionTreeContentProvider.java
+++ plugins/org.eclipse.fordiac.ide.model.ui/src/org/eclipse/fordiac/ide/model/ui/nat/FBTypeSelectionTreeContentProvider.java
@@ -14,8 +14,9 @@ package org.eclipse.fordiac.ide.model.ui.nat;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
-import org.eclipse.fordiac.ide.model.typelibrary.FunctionFBTypeEntry;
+import org.eclipse.fordiac.ide.model.typelibrary.FBTypeEntry;
 import org.eclipse.fordiac.ide.model.typelibrary.TypeLibrary;
 import org.eclipse.fordiac.ide.model.ui.Messages;
 
@@ -23,7 +24,14 @@ public class FBTypeSelectionTreeContentProvider extends TypeSelectionTreeContent
 
 	public static final FBTypeSelectionTreeContentProvider INSTANCE = new FBTypeSelectionTreeContentProvider();
 
+	private final Predicate<FBTypeEntry> filter;
+
 	protected FBTypeSelectionTreeContentProvider() {
+		this(unused -> true);
+	}
+
+	public FBTypeSelectionTreeContentProvider(final Predicate<FBTypeEntry> filter) {
+		this.filter = filter;
 	}
 
 	@Override
@@ -33,7 +41,7 @@ public class FBTypeSelectionTreeContentProvider extends TypeSelectionTreeContent
 		}
 
 		final TypeNode fbTypes = new TypeNode(Messages.DataTypeDropdown_FB_Types);
-		addPathSubtree(fbTypes, typeLibrary.getFbTypes().filter(entry -> !(entry instanceof FunctionFBTypeEntry)));
+		addPathSubtree(fbTypes, typeLibrary.getFbTypes().filter(filter));
 		fbTypes.sortChildren();
 
 		return fbTypes.getChildren();
