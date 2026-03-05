55b4e30b6794e3c2038fa619c055d39b2a2e844e
Michael Oberlehner <127855312+oberlehner@users.noreply.github.com>
2026-03-05 15:48:45 +0100

    Adjusted library test cases

--- plugins/org.eclipse.fordiac.ide.library.ui/src/org/eclipse/fordiac/ide/library/ui/wizards/UnifiedLibraryImportWizard.java
+++ plugins/org.eclipse.fordiac.ide.library.ui/src/org/eclipse/fordiac/ide/library/ui/wizards/UnifiedLibraryImportWizard.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Michael Oberlehner - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.fordiac.ide.library.ui.wizards;
 
 import org.eclipse.core.resources.IFolder;
@@ -15,7 +27,7 @@ public class UnifiedLibraryImportWizard extends Wizard implements IImportWizard 
 
 	@Override
 	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
-		setWindowTitle("Library Import");
+		setWindowTitle("Library Import"); //$NON-NLS-1$
 		setNeedsProgressMonitor(true);
 
 		final StructuredSelection sel = new StructuredSelection(selection.toList());
