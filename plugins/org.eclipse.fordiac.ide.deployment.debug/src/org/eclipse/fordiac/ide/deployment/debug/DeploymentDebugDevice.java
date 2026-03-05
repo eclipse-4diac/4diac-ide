6a09ffacc1995beedacf41ba333db31e24a79630
Martin Jobst <martin.jobst@posteo.de>
2026-03-05 18:41:00 +0100

    Add option whether to watch sub-elements in watchpoint

--- plugins/org.eclipse.fordiac.ide.deployment.debug/src/org/eclipse/fordiac/ide/deployment/debug/DeploymentDebugDevice.java
+++ plugins/org.eclipse.fordiac.ide.deployment.debug/src/org/eclipse/fordiac/ide/deployment/debug/DeploymentDebugDevice.java
@@ -40,6 +40,7 @@ import org.eclipse.fordiac.ide.debug.EvaluatorDebugVariable;
 import org.eclipse.fordiac.ide.deployment.debug.DeploymentLaunchConfigurationAttributes.DeploymentLaunchWatchpoint;
 import org.eclipse.fordiac.ide.deployment.debug.breakpoint.DeploymentWatchpoint;
 import org.eclipse.fordiac.ide.deployment.debug.watch.DeploymentDebugWatchData;
+import org.eclipse.fordiac.ide.deployment.debug.watch.ISubContainerWatch;
 import org.eclipse.fordiac.ide.deployment.debug.watch.IVarDeclarationWatch;
 import org.eclipse.fordiac.ide.deployment.debug.watch.IWatch;
 import org.eclipse.fordiac.ide.deployment.debug.watch.IWatch.Source;
@@ -255,6 +256,9 @@ public class DeploymentDebugDevice extends DeploymentDebugElement implements IDe
 				if (watchpoint.isPinnedChanged(delta)) {
 					updatePinned(watchpoint);
 				}
+				if (watchpoint.isWatchSubElementsChanged(delta)) {
+					updateWatchSubElements(watchpoint);
+				}
 			}
 		}
 	}
@@ -266,6 +270,9 @@ public class DeploymentDebugDevice extends DeploymentDebugElement implements IDe
 				final IWatch watch = watches.computeIfAbsent(element.get().getQualifiedName(),
 						name -> IWatch.watchFor(name, element.get(), this));
 				watch.setSource(Source.LAUNCH);
+				if (watch instanceof final ISubContainerWatch subContainerWatch) {
+					subContainerWatch.setWatchSubElements(watchpoint.watchSubElements());
+				}
 				getPrimaryDebugTarget().updateWatches(true);
 				watch.addWatch();
 				if (watchpoint.isForceEnabled() && watch instanceof final IVarDeclarationWatch variableWatch) {
@@ -285,6 +292,9 @@ public class DeploymentDebugDevice extends DeploymentDebugElement implements IDe
 						name -> IWatch.watchFor(name, element.get(), this));
 				watch.setSource(Source.BREAKPOINT);
 				watch.setPinned(watchpoint.isPinned());
+				if (watch instanceof final ISubContainerWatch subContainerWatch) {
+					subContainerWatch.setWatchSubElements(watchpoint.isWatchSubElements());
+				}
 				getPrimaryDebugTarget().updateWatches(true);
 				watch.addWatch();
 				if (watchpoint.isForceEnabled() && watch instanceof final IVarDeclarationWatch variableWatch) {
@@ -333,6 +343,20 @@ public class DeploymentDebugDevice extends DeploymentDebugElement implements IDe
 		if (watch != null) {
 			watch.setPinned(watchpoint.isPinned());
 			getPrimaryDebugTarget().updateWatches(true);
+		}
+	}
+
+	protected void updateWatchSubElements(final DeploymentWatchpoint watchpoint) {
+		final IWatch watch = watches.get(watchpoint.getLocation());
+		if (watch instanceof final ISubContainerWatch subContainerWatch) {
+			try {
+				subContainerWatch.removeWatch();
+				subContainerWatch.setWatchSubElements(watchpoint.isWatchSubElements());
+				subContainerWatch.addWatch();
+				getPrimaryDebugTarget().updateWatches(true);
+			} catch (final DebugException e) {
+				FordiacLogHelper.logWarning("Cannot update watch for watchpoint: " + watchpoint, e); //$NON-NLS-1$
+			}
 		}
 	}
 
