a0cb16b3a2cf82e8ffd6317f3ffe970e86fba4a3
Alois Zoitl <alois.zoitl@gmx.at>
2026-05-29 21:48:26 +0200

    Only show links of fbt macro targets that can be linked to
    Furthermore fixed the wrong copyright header in FbtMacroPatternElement.

--- plugins/org.eclipse.fordiac.ide.fbtypeeditor.asciidoc/src/org/eclipse/fordiac/ide/fbtypeeditor/asciidoc/phrase/FbtMacroPatternElement.java
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.asciidoc/src/org/eclipse/fordiac/ide/fbtypeeditor/asciidoc/phrase/FbtMacroPatternElement.java
@@ -1,17 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2024 David Green and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
+ * Copyright (c) 2026 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
  *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     David Green - initial API and implementation
- *     Alexander Fedorov (ArSysOp) - ongoing support
+ *   Alois Zoitl - initial API and implementation and/or initial documentation
  *******************************************************************************/
-
 package org.eclipse.fordiac.ide.fbtypeeditor.asciidoc.phrase;
 
 import org.eclipse.fordiac.ide.model.libraryElement.LibraryElement;
