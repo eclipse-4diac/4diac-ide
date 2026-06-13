29037c6feda1aa382d4f148351c680ec68a2d328
vikash1703 <vikash07kp@gmail.com>
2026-06-13 13:51:23 +0530

    feat: Add ToggleModelBreakpointHandler for ECC breakpoint support

---
+++ plugins/org.eclipse.fordiac.ide.debug.ui/src/org/eclipse/fordiac/ide/debug/ui/handler/ToggleModelBreakpointHandler.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Vikash Kumar
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Vikash Kumar - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.debug.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.debug.ui.actions.IToggleBreakpointsTarget;
+import org.eclipse.debug.ui.actions.IToggleBreakpointsTargetExtension;
+import org.eclipse.fordiac.ide.ui.FordiacLogHelper;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class ToggleModelBreakpointHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		final IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
+		final ISelection currentSelection = HandlerUtil.getCurrentSelection(event);
+		final IToggleBreakpointsTarget target = DebugUITools.getToggleBreakpointsTargetManager()
+				.getToggleBreakpointsTarget(activePart, currentSelection);
+		try {
+			if (target instanceof final IToggleBreakpointsTargetExtension extension
+					&& extension.canToggleBreakpoints(activePart, currentSelection)) {
+				extension.toggleBreakpoints(activePart, currentSelection);
+			}
+		} catch (final CoreException e) {
+			FordiacLogHelper.logError(e.getLocalizedMessage(), e);
+		}
+		return null;
+	}
+}
