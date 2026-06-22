74ffe642cf5ff5c4379401d4b7c15c4d204842da
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2026-06-22 14:49:01 +0200

    Added MoreAggressiveDelete command

---
+++ plugins/org.eclipse.fordiac.ide.application/src/org/eclipse/fordiac/ide/application/handlers/MoreAggressiveDeleteConnection.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Primetals Technologies Austria GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Sebastian Hollersbacher - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.application.handlers;
+
+import org.eclipse.fordiac.ide.model.libraryElement.FBNetworkElement;
+import org.eclipse.fordiac.ide.model.libraryElement.UntypedSubApp;
+
+public class MoreAggressiveDeleteConnection extends AggressiveDeleteConnection {
+
+	@Override
+	protected boolean canRemove(final FBNetworkElement sourceElement) {
+		return sourceElement instanceof UntypedSubApp;
+	}
+}
