7e2b279ac6efdbc5d2b4cb2aca2964bc8ea2d049
Martin Jobst <martin.jobst@posteo.de>
2026-07-30 15:41:01 +0200

    Add marker for unresolvable dependencies to project
    Add marker for unresolvable dependencies to project instead of the
    manifest file. This makes it easier to find whether the project has such
    a marker in subsequent builders.

--- plugins/org.eclipse.fordiac.ide.library/src/org/eclipse/fordiac/ide/library/LibraryManager.java
+++ plugins/org.eclipse.fordiac.ide.library/src/org/eclipse/fordiac/ide/library/LibraryManager.java
@@ -706,8 +706,11 @@ public enum LibraryManager {
 
 		final int maxSeverity = markerList.stream().mapToInt(ErrorMarkerBuilder::getSeverity).max().orElse(-1);
 		if (maxSeverity >= IMarker.SEVERITY_ERROR) {
-			markerList.add(ErrorMarkerBuilder.createErrorMarkerBuilder(Messages.LibraryManager_UnresolvableDependencies)
-					.setType(FordiacErrorMarker.LIBRARY_MARKER));
+			FordiacMarkerHelper.updateMarkers(project, FordiacErrorMarker.LIBRARY_MARKER,
+					List.of(ErrorMarkerBuilder
+							.createErrorMarkerBuilder(Messages.LibraryManager_UnresolvableDependencies)
+							.setType(FordiacErrorMarker.LIBRARY_MARKER)),
+					true);
 		}
 
 		FordiacMarkerHelper.updateMarkers(project.getFile(MANIFEST), FordiacErrorMarker.LIBRARY_MARKER, markerList,
