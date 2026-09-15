0d5a7ac3b8da8b1a8a3ee58d5644b2f53d9bdc03
vikash1703 <vikash07kp@gmail.com>
2026-09-16 02:01:32 +0530

    Create ECC state on commit of the inline editor
    Double-clicking on the empty ECC canvas opens an inline text editor
    with a template state name. The state is only created when the user
    commits the name, so cancelling the edit leaves the model unchanged.
    
    The root edit part forwards the open request to the ECC edit part,
    which delegates to a direct edit policy that creates the state from
    the entered name and the clicked position.

---
+++ plugins/org.eclipse.fordiac.ide.fbtypeeditor.ecc/src/org/eclipse/fordiac/ide/fbtypeeditor/ecc/policies/ECCCreateStateDirectEditPolicy.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Vikash Kumar Sinha
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Vikash Kumar Sinha - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.fordiac.ide.fbtypeeditor.ecc.policies;
+
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.commands.CreateECStateCommand;
+import org.eclipse.fordiac.ide.fbtypeeditor.ecc.editors.ECCStateCreationDirectEditManager;
+import org.eclipse.fordiac.ide.model.CoordinateConverter;
+import org.eclipse.fordiac.ide.model.libraryElement.ECC;
+import org.eclipse.fordiac.ide.model.libraryElement.ECState;
+import org.eclipse.fordiac.ide.model.libraryElement.LibraryElementFactory;
+import org.eclipse.fordiac.ide.model.libraryElement.Position;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.editpolicies.DirectEditPolicy;
+import org.eclipse.gef.requests.DirectEditRequest;
+import org.eclipse.gef.requests.LocationRequest;
+
+public class ECCCreateStateDirectEditPolicy extends DirectEditPolicy {
+
+	private Point creationLocation;
+
+	public void performDirectEdit(final LocationRequest request) {
+		creationLocation = request.getLocation().getCopy();
+		new ECCStateCreationDirectEditManager(getHost(), creationLocation).show();
+	}
+
+	@Override
+	protected Command getDirectEditCommand(final DirectEditRequest request) {
+		if (creationLocation != null && request.getCellEditor() != null
+				&& request.getCellEditor().getValue() instanceof final String name && !name.isBlank()) {
+			final Point location = creationLocation.getCopy();
+			getHostFigure().translateToRelative(location);
+			final Position pos = CoordinateConverter.INSTANCE.createPosFromScreenCoordinates(location.x, location.y);
+			final ECState newState = LibraryElementFactory.eINSTANCE.createECState();
+			newState.setName(name);
+			return new CreateECStateCommand(newState, pos, (ECC) getHost().getModel());
+		}
+		return null;
+	}
+
+	@Override
+	protected void showCurrentEditValue(final DirectEditRequest request) {
+		// the state does not exist yet, nothing to show
+	}
+}
